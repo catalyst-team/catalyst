42ec64bf9a0d85edc61ff7e330d3d37321fffe13
Aleksandr <belskikh.aleksandr@gmail.com>
2018-09-12 13:49:05 +0300

    albumentations augmentor

--- data/augmentor.py
+++ data/augmentor.py
@@ -51,3 +51,39 @@ class AugmentorKeys(object):
                 for key, value in zip(self.fn_keys, fn_results)}
         dict_ = {**dict_, **fn_results}
         return dict_
+
+
+class AugmentorAlbu:
+    '''Augmentor for albumentations interface'''
+    def __init__(
+            self, dict2fn_keys,  fn_keys, augment_fn):
+        '''
+        :param dict2fn_keys: list(string) - list of dataset dict_ keys.
+               ex: ["image", "target"]
+        :param fn_keys: list(string) - list of keys for augment_fn result
+               to link dict2fn_keys with augment_fn. ex: ["image", "mask"]
+        :param augment_fn: augmentation function
+        '''
+        assert len(dict2fn_keys) == len(fn_keys), \
+            "Parameters dict2fn_keys and fn_keys should have the same length." \
+            f"Parameters: {dict2fn_keys, fn_keys}"
+
+        self.dict2fn_keys = dict2fn_keys
+        self.fn_keys = fn_keys
+        self.augment_fn = augment_fn
+
+    def __call__(self, dict_):
+        '''
+        :param dict_: dict - item form dataset
+        :return dict_: dict - with augmented data
+        '''
+        # link keys from dict_ with augment_fn keys
+        data = {fn_key: dict_[dict_key] for dict_key, fn_key
+                in zip(self.dict2fn_keys, self.fn_keys)}
+
+        augmented = self.augment_fn(**data)
+        # link keys from augment_fn back to dict_ keys
+        results = {dict_key: augmented[fn_key] for dict_key, fn_key
+                   in zip(self.dict2fn_keys, self.fn_keys)}
+
+        return {**dict_, **results}
