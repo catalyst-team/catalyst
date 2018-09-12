7bfb2057bd07467da0872108448e4ddaa833141f
Aleksandr <belskikh.aleksandr@gmail.com>
2018-09-12 14:33:02 +0300

    double quotes

--- data/augmentor.py
+++ data/augmentor.py
@@ -25,25 +25,25 @@ class Augmentor(object):
 
 
 class AugmentorKeys:
-    ''' Augmentation abstraction to match input and augmentations keys'''
+    """ Augmentation abstraction to match input and augmentations keys"""
     def __init__(
             self,
             dict2fn_dict: Dict[str, str],
             augment_fn: Callable):
-        '''
+        """
         :param dict2fn_dict: keys matching dict {input_key: augment_fn_key}
                ex: {"image": "image", "mask": "mask"}
         :param augment_fn: augmentation function
-        '''
+        """
 
         self.dict2fn_dict = dict2fn_dict
         self.augment_fn = augment_fn
 
     def __call__(self, dict_):
-        '''
+        """
         :param dict_: dict - item form dataset
         :return dict_: dict - with augmented data
-        '''
+        """
         # link keys from dict_ with augment_fn keys
         data = {
             fn_key: dict_[dict_key]
