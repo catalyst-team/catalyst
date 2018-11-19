a499a9b92972c428b49bf6ff60111173fe5337e5
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2018-11-19 22:18:34 +0900

    Add UNet ternaus

--- models/segmentation/__init__.py
+++ models/segmentation/__init__.py
@@ -1,7 +1,11 @@
 from .linknet import LinkNet
 from .unet import UNet
+from .unet_ternaus import AlbuNet, UNet11, UNet16
 
 models = {
     'unet': UNet,
-    'linknet': LinkNet
+    'linknet': LinkNet,
+    'unet11': UNet11,
+    'unet16': UNet16,
+    'albunet': AlbuNet
 }
