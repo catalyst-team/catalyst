e9b200bca8b2af3d3533407c92e5936ecc5e3cf4
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2018-12-20 15:57:46 +0900

    Update BCEF1FocalLoss

--- losses/combine_loss.py
+++ losses/combine_loss.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from .f1 import F1Loss
+from .focal_loss import FocalLoss
 
 
 class BCEAndF1(nn.Module):
@@ -15,3 +16,17 @@ class BCEAndF1(nn.Module):
         f1 = self.f1_loss(logits, labels)
         bce = self.bce_loss(logits, labels)
         return self.bce_weight * bce + (1 - self.bce_weight) * f1
+
+
+class BCEF1FocalLoss(nn.Module):
+    def __init__(self):
+        super(BCEF1FocalLoss, self).__init__()
+        self.f1_loss = F1Loss()
+        self.bce_loss = F.binary_cross_entropy_with_logits
+        self.focal_loss = FocalLoss()
+
+    def forward(self, logits, labels):
+        f1 = self.f1_loss(logits, labels)
+        bce = self.bce_loss(logits, labels)
+        focal = self.focal_loss(logits, labels)
+        return 0.45 * bce + 0.45 * f1 + 0.1 * focal
