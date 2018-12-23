f56ef21b9f33bc7abcfb3b97bb986a4b368f0b83
Tezikov Roman <tez.romach@gmail.com>
2018-12-23 20:12:57 +0300

    travis plz 2

--- dl/callbacks/__init__.py
+++ dl/callbacks/__init__.py
@@ -2,8 +2,6 @@ from catalyst.dl.callbacks.core import Callback, CallbackCompose, Logger, \
     TensorboardLogger, CheckpointCallback, OptimizerCallback, \
     SchedulerCallback, ClassificationLossCallback, InferCallback, MixupCallback
 
-from catalyst.dl.callbacks import metrics, schedulers, utils
-
 __all__ = [
     "Callback",
     "CallbackCompose",
