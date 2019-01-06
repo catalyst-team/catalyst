6ef2c847668b29c13c66b3262f626a742c3983b9
Sergey Kolesnikov <scitator@gmail.com>
2019-01-06 22:10:58 +0300

    actor refactored; todo: critic

--- rl/networks/agents.py
+++ rl/networks/agents.py
@@ -12,20 +12,248 @@ LOG_SIG_MAX = 2
 LOG_SIG_MIN = -10
 
 
+class TemporalAttentionPooling(nn.Module):
+    name2fn = {
+        "softmax": nn.Softmax(dim=1),
+        "tanh": nn.Tanh(),
+        "sigmoid": nn.Sigmoid()
+    }
+
+    def __init__(self, features_in, pooling=None):
+        super().__init__()
+        self.features_in = features_in
+        pooling = pooling or "softmax"
+
+        self.attention_pooling = nn.Sequential(
+            nn.Conv1d(
+                in_channels=features_in,
+                out_channels=1,
+                kernel_size=1,
+                bias=True
+            ),
+            TemporalAttentionPooling.name2fn[pooling]
+        )
+        self.attention_pooling.apply(outer_init)
+
+    def forward(self, features):
+        """
+        :param features: [batch_size, history_len, feature_size]
+        :return:
+        """
+        x = features
+        batch_size, history_len, feature_size = x.shape
+
+        x = x.view(batch_size, history_len, -1)
+        x_a = x.transpose(1, 2)
+        x_attn = (self.attn(x_a) * x_a).transpose(1, 2)
+        x_attn = x_attn.sum(1, keepdim=True)
+
+        return x_attn
+
+
+class LamaPooling(nn.Module):
+    available_poolings = [
+        "last",
+        "avg_all", "avg",
+        "max_all", "max",
+        "softmax_all", "softmax",
+        "tanh_all", "tanh",
+        "sigmoid_all", "sigmoid"
+    ]
+
+    def __init__(self, features_in, poolings=None):
+        super().__init__()
+        self.features_in = features_in
+        self.poolings = poolings or ["last", "avg", "max", "softmax"]
+        self.features_out = features_in * len(poolings)
+
+        for key in self.poolings:
+            self._prepare_for_pooling(key)
+
+    def _prepare_for_pooling(self, key):
+        if any([x in key for x in ["softmax", "tanh", "sigmoid"]]):
+            key = key.split("_", 1)[0]
+            pooling = TemporalAttentionPooling(
+                features_in=self.features_in,
+                pooling=key)
+            # @TODO: fix hack
+            setattr(self, f"{key}_pooling", pooling)
+
+    def _pooling_fn(self, key, features):
+        x = features
+        key_ = key.split("_", 1)
+
+        if key != "last" and len(key_) == 1:
+            # all except last
+            x = x[:, :-1, :]
+            key = key_[0]
+
+        if key == "last":
+            x_out = x[:, -1:, :]
+        elif key == "avg":
+            x_out = x.mean(1, keepdim=True)
+        elif key == "max":
+            x_out = x.max(1, keepdim=True)[0]
+        elif any([x in key for x in ["softmax", "tanh", "sigmoid"]]):
+            pooling = getattr(self, f"{key}_pooling")
+            x_out = pooling(x)
+        else:
+            raise NotImplementedError
+
+        return x_out
+
+    def forward(self, features):
+        """
+        :param features: [batch_size, history_len, feature_size]
+        :return:
+        """
+        x = features
+        batch_size, history_len, feature_size = x.shape
+
+        features_ = []
+        for key in self.poolings:
+            pooling = self._pooling_fn(key, features)
+            features_.append(pooling)
+        x = torch.cat(features_, dim=1, )
+        x = x.view(batch_size, -1)
+
+        return x
+
+
 class Actor(nn.Module):
-    """ Actor which learns deterministic policy.
+    """
+    Actor which learns deterministic policy.
     """
 
+    def __init__(self, observation_net, policy_net, memory_net=None):
+        super().__init__()
+        self.observation_net = observation_net
+        self.memory_net = memory_net
+        self.policy_net = policy_net
+        self._forward_fn = None
+
+        if memory_net is not None and memory_net == "lama":
+            self._forward_fn = self._forward_lama
+        elif memory_net is not None and memory_net == "rnn":
+            self._forward_fn = self._forward_rnn
+        else:
+            self._forward_fn = self._forward_ff
+
+    def _forward_ff(self, observation):
+        x = observation.view(observation.shape[0], -1)
+        x = self.observation_net(x)
+        x = self.policy_net(x)
+        return x
+
+    def _forward_rnn(self, observation, hidden_state):
+        # @TODO
+        raise NotImplementedError
+        action, hidden_state = None, None
+        return action, hidden_state
+
+    def _forward_lama(self, observation):
+        x = observation
+        if len(x.shape) < 3:
+            x = x.unsqueeze(1)
+        batch_size, history_len, feature_size = x.shape
+        x = x.view(-1, feature_size)
+        x = self.observation_net(x)
+
+        x = x.view(batch_size, history_len, -1)
+        x = self.memory_net(x)
+        x = self.policy_net(x)
+        return x
+
+    def forward(self, *args, **kwargs):
+        action = self._forward_fn(*args, **kwargs)
+        return action
+
+    @staticmethod
+    def create_actor(
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            activation_fn=nn.ReLU,
+            dropout=None,
+            norm_fn=None,
+            bias=True,
+            layer_order=None,
+            residual=False,
+            out_activation=None,
+            memory_type=None,
+            **kwargs):
+        assert len(kwargs) == 0
+        # hack to prevent cycle imports
+        from catalyst.contrib.modules import name2nn
+
+        layer_fn = name2nn(layer_fn)
+        activation_fn = name2nn(activation_fn)
+        norm_fn = name2nn(norm_fn)
+        out_activation = name2nn(out_activation)
+
+        if isinstance(state_shape, int):
+            state_shape = (state_shape,)
+
+        if len(state_shape) in [1, 2]:
+            # linear case: one observation or several one
+            state_size = reduce(lambda x, y: x * y, state_shape)
+
+            observation_net = SequentialNet(
+                hiddens=[state_size] + hiddens[:-1],
+                layer_fn=layer_fn,
+                dropout=dropout,
+                activation_fn=activation_fn,
+                norm_fn=norm_fn,
+                bias=bias,
+                layer_order=layer_order,
+                residual=residual
+            )
+        elif len(state_shape) in [3, 4]:
+            # cnn case: one image or several one @TODO
+            raise NotImplementedError
+        else:
+            raise NotImplementedError
+
+        if memory_type == "lama":
+            raise NotImplementedError
+        elif memory_type == "rnn":
+            raise NotImplementedError
+        else:
+            memory_net = None
+            memory_out = hiddens[-1]
+
+        policy_net = SequentialNet(
+            hiddens=[memory_out, action_size],
+            layer_fn=nn.Linear,
+            activation_fn=out_activation,
+            norm_fn=None,
+            bias=True
+        )
+
+        inner_init = create_optimal_inner_init(nonlinearity=activation_fn)
+        observation_net.apply(inner_init)
+        policy_net.apply(outer_init)
+
+        actor_net = Actor(
+            observation_net=observation_net,
+            memory_net=memory_net,
+            policy_net=policy_net)
+
+        return actor_net
+
+
+class ActorDeprecated(nn.Module):
     def __init__(
-        self,
-        state_shape,
-        action_size,
-        hiddens,
-        layer_fn,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=nn.Tanh
+            self,
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=nn.Tanh
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -66,15 +294,15 @@ class Actor(nn.Module):
 
 class LamaActor(nn.Module):
     def __init__(
-        self,
-        state_shape,
-        action_size,
-        hiddens,
-        layer_fn,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=nn.Tanh
+            self,
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=nn.Tanh
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -153,15 +381,15 @@ class GaussActor(nn.Module):
     """
 
     def __init__(
-        self,
-        state_shape,
-        action_size,
-        hiddens,
-        layer_fn,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=nn.Sigmoid
+            self,
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=nn.Sigmoid
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -221,15 +449,15 @@ class RealNVPActor(nn.Module):
     """
 
     def __init__(
-        self,
-        state_shape,
-        action_size,
-        hiddens,
-        layer_fn,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=nn.Sigmoid
+            self,
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=nn.Sigmoid
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -301,22 +529,22 @@ class RealNVPActor(nn.Module):
         return action
 
 
-class Critic(nn.Module):
+class CriticDeprecated(nn.Module):
     """ Critic which learns state-action value function Q(s,a).
     """
 
     def __init__(
-        self,
-        state_shape,
-        action_size,
-        hiddens,
-        layer_fn,
-        concat_at=1,
-        n_atoms=1,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=None
+            self,
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            concat_at=1,
+            n_atoms=1,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=None
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -385,17 +613,17 @@ class Critic(nn.Module):
 
 class LamaCritic(nn.Module):
     def __init__(
-        self,
-        state_shape,
-        action_size,
-        hiddens,
-        layer_fn,
-        concat_at=1,
-        n_atoms=1,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=None
+            self,
+            state_shape,
+            action_size,
+            hiddens,
+            layer_fn,
+            concat_at=1,
+            n_atoms=1,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=None
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -503,15 +731,15 @@ class ValueCritic(nn.Module):
     """
 
     def __init__(
-        self,
-        state_shape,
-        hiddens,
-        layer_fn,
-        n_atoms=1,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=None
+            self,
+            state_shape,
+            hiddens,
+            layer_fn,
+            n_atoms=1,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=None
     ):
         super().__init__()
         # hack to prevent cycle imports
@@ -555,15 +783,15 @@ class ValueCritic(nn.Module):
 
 class LamaValueCritic(nn.Module):
     def __init__(
-        self,
-        state_shape,
-        hiddens,
-        layer_fn,
-        n_atoms=1,
-        activation_fn=nn.ReLU,
-        norm_fn=None,
-        bias=True,
-        out_activation=None
+            self,
+            state_shape,
+            hiddens,
+            layer_fn,
+            n_atoms=1,
+            activation_fn=nn.ReLU,
+            norm_fn=None,
+            bias=True,
+            out_activation=None
     ):
         super().__init__()
         # hack to prevent cycle imports
