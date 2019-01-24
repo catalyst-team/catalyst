2e683af25c18cf81c9d995e76ac6e782e9ef6d7f
Sergey Kolesnikov <scitator@gmail.com>
2019-01-24 23:07:08 +0300

    catalyst

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from shutil import rmtree
 from setuptools import find_packages, setup, Command
 
 # Package meta-data.
-NAME = "catalyst-ti"
+NAME = "catalyst"
 DESCRIPTION = "Catalyst. High-level utils for PyTorch DL & RL research."
 URL = "https://github.com/catalyst-team/catalyst"
 EMAIL = "scitator@gmail.com"
