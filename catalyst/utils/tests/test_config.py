1cc72b0e7db8554a36d5b10b0c0d30765f348c61
Tezikov Roman <tez.romach@gmail.com>
2019-02-15 21:46:34 +0300

    yapf

--- catalyst/utils/tests/test_config.py
+++ catalyst/utils/tests/test_config.py
@@ -6,20 +6,29 @@ from .. import config
 
 def test_parse_config_args():
     configuration = {
-        "stages": {"one": "uno", "two": "dos", "three": "tres"},
-        "key": {"value": "key2"}
+        "stages": {
+            "one": "uno",
+            "two": "dos",
+            "three": "tres"
+        },
+        "key": {
+            "value": "key2"
+        }
     }
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--command")
 
-    args, uargs = parser.parse_known_args([
-        "--command", "train",
-        "--path=test.yml:str",
-        "--stages/zero=cero:str"
-    ])
+    args, uargs = parser.parse_known_args(
+        [
+            "--command", "train", "--path=test.yml:str",
+            "--stages/zero=cero:str"
+        ]
+    )
 
-    configuration, args = config.parse_config_args(config=configuration, args=args, unknown_args=uargs)
+    configuration, args = config.parse_config_args(
+        config=configuration, args=args, unknown_args=uargs
+    )
     assert args.command == "train"
     assert args.path == "test.yml"
     assert configuration.get("stages") is not None
