f93a28065fcba8e8fc083d020432b817e901b8a3
Sergey Kolesnikov <scitator@gmail.com>
2019-03-07 09:31:32 +0300

    rl example

--- examples/cifar_stages/__init__.py
+++ examples/cifar_stages/__init__.py
@@ -1,3 +1,6 @@
+from catalyst.contrib.registry import Registry
 from catalyst.dl.experiments.runner import SupervisedRunner as Runner
 from .experiment import Experiment
-from .model import SimpleNet as Model
+from .model import SimpleNet
+
+Registry.model(SimpleNet)
