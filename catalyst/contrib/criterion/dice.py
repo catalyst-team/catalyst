9c747bf843d9fad73fa47944c7b1b2072db8fcb1
Tezikov Roman <tez.romach@gmail.com>
2019-04-24 14:20:12 +0300

    add eps to DiceLoss

--- catalyst/contrib/criterion/dice.py
+++ catalyst/contrib/criterion/dice.py
@@ -2,17 +2,6 @@ import torch.nn as nn
 from catalyst.dl import metrics
 
 
-class BCEDiceLoss(nn.Module):
-    def __init__(self, activation="sigmoid"):
-        super(BCEDiceLoss, self).__init__()
-        self.bce_loss = nn.BCEWithLogitsLoss()
-        self.dice_loss = DiceLoss(activation=activation)
-
-    def forward(self, outputs, targets):
-        return self.bce_loss(outputs,
-                             targets) + self.dice_loss(outputs, targets)
-
-
 class DiceLoss(nn.Module):
     def __init__(self, eps=1e-7, activation="sigmoid"):
         super(DiceLoss, self).__init__()
@@ -25,3 +14,14 @@ class DiceLoss(nn.Module):
         )
 
         return 1 - dice
+
+
+class BCEDiceLoss(nn.Module):
+    def __init__(self,  eps=1e-7, activation="sigmoid"):
+        super(BCEDiceLoss, self).__init__()
+        self.bce_loss = nn.BCEWithLogitsLoss()
+        self.dice_loss = DiceLoss(eps=eps, activation=activation)
+
+    def forward(self, outputs, targets):
+        return self.bce_loss(outputs,
+                             targets) + self.dice_loss(outputs, targets)
