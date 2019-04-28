502e890a176a75253e69f885039142138635e78c
Alex Gaziev <alex.gaziev@gmail.com>
2019-04-28 11:07:22 -0700

    Naming fixes

--- catalyst/contrib/scripts/tests/test_tag2label.py
+++ catalyst/contrib/scripts/tests/test_tag2label.py
@@ -2,14 +2,14 @@ from ..tag2label import prepare_df_from_dirs
 
 
 def _setup_dataset_fs(tmp_path):
-    def create_children(p, children):
-        for c, sub_children in children.items():
-            parent = p/c
-            parent.mkdir()
+    def create_children(parent, children):
+        for child, sub_children in children.items():
+            sub_parent = parent/child
+            sub_parent.mkdir()
             if type(sub_children) == list:
-                [(parent/f).touch() for f in sub_children]
+                [(sub_parent/sub_child).touch() for sub_child in sub_children]
             else:
-                create_children(parent, sub_children)
+                create_children(sub_parent, sub_children)
 
     fs_structure = {
         'datasets': {
@@ -28,8 +28,8 @@ def _setup_dataset_fs(tmp_path):
 
 
 def test_prepare_df_from_dirs_one(tmp_path):
-    def check_filepath(f):
-        return f.startswith('act1') or f.startswith('act2')
+    def check_filepath(filepath):
+        return filepath.startswith('act1') or filepath.startswith('act2')
 
     _setup_dataset_fs(tmp_path)
     root_path = tmp_path/'datasets/root1'
@@ -41,11 +41,11 @@ def test_prepare_df_from_dirs_one(tmp_path):
 
 
 def test_prepare_df_from_dirs_multi(tmp_path):
-    def check_filepath(f):
-        return f.startswith('root1/act1') or \
-               f.startswith('root1/act2') or \
-               f.startswith('root2/act1') or \
-               f.startswith('root2/act2')
+    def check_filepath(filepath):
+        return filepath.startswith('root1/act1') or \
+               filepath.startswith('root1/act2') or \
+               filepath.startswith('root2/act1') or \
+               filepath.startswith('root2/act2')
 
     _setup_dataset_fs(tmp_path)
     ds_path = tmp_path/'datasets'
