c1de4c61c4c29a2bda4a4cb818246efa10972c78
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-05-06 22:48:09 +0300

    If model returns a dict, we use it directly as output. If model returns a list and output_key is also a list - we use it to map output to dict with corresponding names.

--- catalyst/dl/experiments/runner.py
+++ catalyst/dl/experiments/runner.py
@@ -234,7 +234,12 @@ class SupervisedRunner(Runner):
 
     def predict_batch(self, batch: Mapping[str, Any]):
         output = self.model(batch[self.input_key])
-        output = {self.output_key: output}
+        if isinstance(output, dict):
+            pass
+        elif isinstance(output, (list,tuple)) and isinstance(self.output_key, list):
+            output = dict((key,value) for key, value in zip(self.output_key, output))
+        else:
+            output = {self.output_key: output}
         return output
 
     def train(
