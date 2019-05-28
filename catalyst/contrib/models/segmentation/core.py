5a7d724a4b0f030a55df848bdd4a35338dd71403
Sergey Kolesnikov <scitator@gmail.com>
2019-05-28 23:01:50 +0300

    no debug

--- catalyst/contrib/models/segmentation/core.py
+++ catalyst/contrib/models/segmentation/core.py
@@ -1,5 +1,4 @@
 from typing import List, Dict
-from pprint import pprint
 
 import torch
 import torch.nn as nn
@@ -25,15 +24,10 @@ class UnetSpec(nn.Module):
         self.head = head or (lambda x: x)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        pprint(f"x: {[z.shape for z in [x]]}")
         encoder_features: List[torch.Tensor] = self.encoder(x)
-        pprint(f"encoder_features: {[z.shape for z in encoder_features]}")
         bridge_features: List[torch.Tensor] = self.bridge(encoder_features)
-        pprint(f"bridge_features: {[z.shape for z in bridge_features]}")
         decoder_features: List[torch.Tensor] = self.decoder(bridge_features)
-        pprint(f"decoder_features: {[z.shape for z in decoder_features]}")
         output: torch.Tensor = self.head(decoder_features)
-        pprint(f"output: {[z.shape for z in [output]]}")
         return output
 
 
