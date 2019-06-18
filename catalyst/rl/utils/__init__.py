82a6a0dc215d035967ffe46ea9e391ecd6ac4118
Sergey Kolesnikov <scitator@gmail.com>
2019-06-18 08:55:02 +0300

    space extend func

--- catalyst/rl/utils/__init__.py
+++ catalyst/rl/utils/__init__.py
@@ -4,6 +4,7 @@ from .criterion import categorical_loss, quantile_loss
 
 from .buffers import OffpolicyReplayBuffer, OnpolicyRolloutBuffer
 from .gamma import hyperbolic_gammas
+from .gym import extend_space
 from .samplers import OffpolicyReplaySampler, OnpolicyRolloutSampler
 from .torch import get_trainer_components, \
     get_network_weights, set_network_weights
