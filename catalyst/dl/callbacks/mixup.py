56ec942ad3c87415cdadb6149179db81177d4716
Evgeny Semyonov <lightsanweb@yandex.ru>
2019-06-19 16:44:38 +0300

    mixup documentation

--- catalyst/dl/callbacks/mixup.py
+++ catalyst/dl/callbacks/mixup.py
@@ -9,8 +9,31 @@ from catalyst.dl.callbacks import CriterionCallback
 
 
 class MixupCallback(CriterionCallback):
+    """
+    Callback to do mixup augmentation.
+
+    Paper: https://arxiv.org/abs/1710.09412
+
+    Note:
+        MixupCallback is inherited from CriterionCallback and
+        does its work.
+
+        You may not use them together.
+    """
     def __init__(self, fields: List[str] = ("features",), alpha=1.0,
                  train_only=True, **kwargs):
+        """
+        Args:
+            fields (List[str]): list of features which must be affected.
+            alpha (float): beta distribution a=b parameters.
+                Must be >=0. The more alpha closer to zero
+                the less effect of the mixup
+            train_only (bool): Apply to train only.
+                As the mixup use the proxy inputs, the targets are also proxy.
+                We are not interested in them, are we?
+                So, if train_only is True, use a standard output/metric
+                for validation.
+        """
         assert len(fields) > 0, \
             "At least one field for MixupCallback is required"
         assert alpha >= 0, "alpha must be>=0"
@@ -22,12 +45,14 @@ class MixupCallback(CriterionCallback):
         self.alpha = alpha
         self.lam = 1
         self.index = None
+        self.is_needed = True
 
-    def _is_needed(self, state: RunnerState):
-        return not self.train_only or state.loader_name.startswith("train")
+    def on_loader_start(self, state: RunnerState):
+        self.is_needed = not self.train_only or \
+            state.loader_name.startswith("train")
 
     def on_batch_start(self, state: RunnerState):
-        if not self._is_needed(state):
+        if not self.is_needed:
             return
         if self.alpha > 0:
             self.lam = np.random.beta(self.alpha, self.alpha)
@@ -42,7 +67,7 @@ class MixupCallback(CriterionCallback):
                 (1 - self.lam) * state.input[f][self.index]
 
     def _compute_loss(self, state: RunnerState, criterion):
-        if not self._is_needed(state):
+        if not self.is_needed:
             return super()._compute_loss(state, criterion)
         pred = state.output[self.output_key]
         y_a = state.input[self.input_key]
