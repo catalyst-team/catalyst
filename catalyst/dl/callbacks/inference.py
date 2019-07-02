dcb296fa7eadbcfdaa63ccabe695412ee7adbade
Sergey Kolesnikov <scitator@gmail.com>
2019-07-02 13:05:29 +0300

    callback update

--- catalyst/dl/callbacks/inference.py
+++ catalyst/dl/callbacks/inference.py
@@ -1,14 +1,14 @@
 import os
 from collections import defaultdict
-import random
 import numpy as np
-import cv2
+import imageio
+from skimage.color import label2rgb
 
 import torch
 import torch.nn.functional as F
 
 from catalyst.dl.core import Callback, RunnerState
-from catalyst.utils import binary_mask_to_overlay_image
+from catalyst.utils import binary_mask_to_overlay_image, tensor_to_ndimage
 
 
 # @TODO: refactor
@@ -60,23 +60,22 @@ class InferMaskCallback(Callback):
         name_key=None,
         mean=None,
         std=None,
-        mask_type="soft",
         threshold=None,
-        dump_mask=False,
+        mask_strength: float = 0.5,
+        mask_type: str = "soft"
     ):
         self.out_dir = out_dir
         self.out_prefix = out_prefix
         self.mean = mean or np.array([0.485, 0.456, 0.406])
         self.std = std or np.array([0.229, 0.224, 0.225])
-        assert mask_type in ["soft", "hard"], mask_type
         assert input_key is not None
         assert output_key is not None
-        self.mask_type = mask_type
         self.threshold = threshold
+        self.mask_strength = mask_strength
+        self.mask_type = mask_type
         self.input_key = input_key
         self.output_key = output_key
         self.name_key = name_key
-        self.dump_mask = dump_mask
         self.counter = 0
         self._keys_from_state = ["out_dir", "out_prefix"]
 
@@ -91,20 +90,6 @@ class InferMaskCallback(Callback):
         if self.out_prefix is not None:
             os.makedirs(os.path.dirname(self.out_prefix), exist_ok=True)
 
-    @staticmethod
-    def _get_spaced_colors2(n_colors, seed=42):
-        random.seed(seed)
-        r, g, b = [int(random.random() * 256) for _ in range(3)]
-
-        step = 256 / n_colors
-        ret = []
-        for i in range(n_colors):
-            r += step
-            g += step
-            b += step
-            ret.append((int(r) % 256, int(g) % 256, int(b) % 256))
-        return ret
-
     def on_loader_start(self, state: RunnerState):
         lm = state.loader_name
         os.makedirs(f"{self.out_prefix}/{lm}/", exist_ok=True)
@@ -114,6 +99,8 @@ class InferMaskCallback(Callback):
         names = state.input.get(self.name_key, [])
 
         features = state.input[self.input_key]
+        images = tensor_to_ndimage(features)
+
         logits = state.output[self.output_key]
         logits = torch.unsqueeze_(logits, dim=1) \
             if len(logits.shape) < 4 \
@@ -124,44 +111,28 @@ class InferMaskCallback(Callback):
         else:
             probs = F.softmax(logits, dim=1)
 
-        features = features.detach().cpu().numpy()
-        features = np.transpose(features, (0, 2, 3, 1))
+        masks = []
+        for observation in probs:
+            result = np.zeros_like(observation[0], dtype=np.int32)
+            for i, ch in enumerate(observation):
+                result[ch >= self.threshold] = i + 1
+            masks.append(result)
 
-        probs = probs.detach().cpu().numpy()
-        probs = np.transpose(probs, (0, 2, 3, 1))
-
-        # colors = self._get_spaced_colors2(n_colors=probs.shape[3])
-        for i in range(probs.shape[0]):
-            img = np.uint8(255 * (self.std * features[i] + self.mean))
+        for i, (image, mask) in enumerate(zip(images, masks)):
             try:
                 suffix = names[i]
             except IndexError:
                 suffix = f"{self.counter:06d}"
             self.counter += 1
 
-            # shw = img.copy()
-            masks = []
-            for t in range(probs.shape[3]):
-                mask = probs[i, :, :, t] > self.threshold \
-                    if self.threshold is not None \
-                    else probs[i, :, :, t]
-                mask = mask.astype(np.float32)
+            mask = label2rgb(mask, bg_label=0)
 
-                if self.dump_mask:
-                    mask_ = np.concatenate(
-                        [np.expand_dims(mask, -1)] * 3,
-                        axis=-1).astype(np.float32) * 255
-                    filename_ = f"{self.out_prefix}/{lm}/{suffix}_{t}.jpg"
-                    cv2.imwrite(filename_, mask_)
-
-                masks.append(mask)
-                # mask = mask - erosion(mask, disk(4))
-                # shw[mask > 0.5] = colors[t]
-
-            shw = binary_mask_to_overlay_image(img.copy(), masks)
+            image = image * (1 - self.mask_strength) \
+                + mask * self.mask_strength
+            image = (image * 255).clip(0, 255).round().astype(np.uint8)
 
             filename = f"{self.out_prefix}/{lm}/{suffix}.jpg"
-            cv2.imwrite(filename, shw[:, :, ::-1])
+            imageio.imwrite(filename, image)
 
 
 __all__ = ["InferCallback", "InferMaskCallback"]
