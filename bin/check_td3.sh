dedb0ad14584e59b46476101a555a7751adc75be
Sergey Kolesnikov <scitator@gmail.com>
2019-07-13 23:12:56 +0300

    speedup base

--- bin/check_td3.sh
+++ bin/check_td3.sh
@@ -25,7 +25,7 @@ OMP_NUM_THREADS="1" MKL_NUM_THREADS="1" \
     python catalyst/rl/scripts/run_trainer.py \
     --config=./examples/_tests_rl_gym/config_td3_quantile.yml \
     --logdir=./examples/logs/_tests_rl_gym_td3_quantile &
-sleep 1500
+sleep 600
 
 killall -9 python
 sleep 3
@@ -47,7 +47,7 @@ OMP_NUM_THREADS="1" MKL_NUM_THREADS="1" \
     python catalyst/rl/scripts/run_samplers.py \
     --config=./examples/_tests_rl_gym/config_td3_quantile.yml \
     --logdir=./examples/logs/_tests_rl_gym_td3_quantile &
-sleep 180
+sleep 120
 
 #OMP_NUM_THREADS="1" MKL_NUM_THREADS="1" \
 #    PYTHONPATH=./examples:./catalyst:${PYTHONPATH} \
@@ -70,7 +70,7 @@ reward_goal = -8.0
 folder = list(pathlib.Path('./examples/logs/_tests_rl_gym_td3_base/').glob('sampler.valid*'))[0]
 checkpoint = utils.load_checkpoint(f'{folder}/checkpoints/best.pth')
 print('mean reward', np.mean(checkpoint['rewards']))
-#assert np.mean(checkpoint['rewards']) > reward_goal
+assert np.mean(checkpoint['rewards']) > reward_goal
 
 folder = list(pathlib.Path('./examples/logs/_tests_rl_gym_td3_categorical/').glob('sampler.valid*'))[0]
 checkpoint = utils.load_checkpoint(f'{folder}/checkpoints/best.pth')
