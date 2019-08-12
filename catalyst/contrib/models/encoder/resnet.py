91ffbeb2d77e5a711d331c64eb261d6c643777cc
Roman Tezikov <tez.romach@gmail.com>
2019-08-12 13:52:13 +0300

    Added param `state_dict` param for the resnet-encoder (#292)
    * add `state_dict` param for the resnet-encoder
    
    * allowed state_dict to be path or str
    
    * state_dict to ResnetEncoder
    
    * update docs

--- catalyst/contrib/models/encoder/resnet.py
+++ catalyst/contrib/models/encoder/resnet.py
@@ -1,3 +1,7 @@
+from pathlib import Path
+from typing import Union
+
+import torch
 import torch.nn as nn
 import torchvision
 
@@ -8,16 +12,40 @@ from catalyst.contrib.registry import MODULES
 class ResnetEncoder(nn.Module):
     def __init__(
         self,
-        arch="resnet34",
-        pretrained=True,
-        frozen=True,
-        pooling=None,
-        pooling_kwargs=None,
-        cut_layers=2
+        arch: str = "resnet34",
+        pretrained: bool = True,
+        frozen: bool = True,
+        pooling: str = None,
+        pooling_kwargs: dict = None,
+        cut_layers: int = 2,
+        state_dict: Union[dict, str, Path] = None,
     ):
+        """
+        Specifies an encoder for classification network
+        Args:
+            arch (str): Name for resnet. Have to be one of
+                resnet18, resnet34, resnet50, resnet101, resnet152
+            pretrained (bool): If True, returns a model pre-trained on ImageNet
+            frozen (bool): If frozen, sets requires_grad to False
+            pooling (str): pooling
+            pooling_kwargs (dict): params for pooling
+            state_dict (Union[dict, str, Path]): Path to ``torch.Model``
+                or a dict containing parameters and persistent buffers.
+        Examples:
+            >>> encoder = ResnetEncoder(
+            >>>    arch="resnet18",
+            >>>    pretrained=False,
+            >>>    state_dict="/model/path/resnet18-5c106cde.pth"
+            >>> )
+        """
         super().__init__()
 
         resnet = torchvision.models.__dict__[arch](pretrained=pretrained)
+        if state_dict is not None:
+            if isinstance(state_dict, (Path, str)):
+                state_dict = torch.load(str(state_dict))
+            resnet.load_state_dict(state_dict)
+
         modules = list(resnet.children())[:-cut_layers]  # delete last layers
 
         if frozen:
