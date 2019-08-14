2b150589cce172add4bc75fa5bebda3023760eae
Tezikov Roman <tez.romach@gmail.com>
2019-08-14 15:45:27 +0300

    add params to prepare_cudnn

--- catalyst/dl/utils/torch.py
+++ catalyst/dl/utils/torch.py
@@ -18,20 +18,32 @@ _Optimizer = optim.Optimizer
 _Scheduler = optim.lr_scheduler._LRScheduler
 
 
-def prepare_cudnn() -> None:
+def prepare_cudnn(
+    deterministic: bool = None,
+    benchmark: bool = None
+) -> None:
     """
     Prepares CuDNN benchmark and sets CuDNN
     to be deterministic/non-deterministic mode
+    Args:
+        deterministic (bool): deterministic mode if running in CuDNN backend.
+        benchmark (bool): If ``True`` use CuDNN heuristics to figure out
+            which algorithm will be most performant
+            for your model architecture and input.
+            Setting it to ``False`` may slow down your training.
     """
     if torch.cuda.is_available():
-        # https://discuss.pytorch.org/t/how-should-i-disable-using-cudnn-in-my-code/38053/4
-        benchmark = os.environ.get("CUDNN_BENCHMARK", "True") == "True"
-        cudnn.benchmark = benchmark
-
         # CuDNN reproducibility
         # https://pytorch.org/docs/stable/notes/randomness.html#cudnn
-        deterministic = os.environ.get("CUDNN_DETERMINISTIC", "True") == "True"
+        if deterministic is None:
+            deterministic = \
+                os.environ.get("CUDNN_DETERMINISTIC", "False") == "True"
         cudnn.deterministic = deterministic
+
+        # https://discuss.pytorch.org/t/how-should-i-disable-using-cudnn-in-my-code/38053/4
+        if benchmark is None:
+            benchmark = os.environ.get("CUDNN_BENCHMARK", "True") == "True"
+        cudnn.benchmark = benchmark
 
 
 def process_components(
