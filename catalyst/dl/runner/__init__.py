1512677e168e71a8f283b67b0c455bbc0081a10f
Sergey Kolesnikov <scitator@gmail.com>
2019-08-28 14:46:27 +0300

    simplification

--- catalyst/dl/runner/__init__.py
+++ catalyst/dl/runner/__init__.py
@@ -1,4 +1,14 @@
 # flake8: noqa
+import logging
+logger = logging.getLogger(__name__)
 
 from .supervised import SupervisedRunner
-from .wandb import *
+
+try:
+    import wandb
+    from .wandb import WandbRunner
+except ImportError:
+    logger.warning(
+        "wandb not available, switching to pickle. "
+        "To install wandb, run `pip install wandb`."
+    )
