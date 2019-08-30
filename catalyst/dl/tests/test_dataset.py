780b3af47c3fc43d01790e1a482bd0488b7c902b
Roman Tezikov <tez.romach@gmail.com>
2019-08-30 22:55:30 +0300

    Feature/paths dataset (#335)
    * Paths dataset that derives labels from filenames
    
    * Fix docstrings; replace literal dict initialization with dict()
    
    * Fix coding style
    
    * PathsDataset codestyle
    
    * fixed typing
    
    * added tests

---
+++ catalyst/dl/tests/test_dataset.py
@@ -0,0 +1,22 @@
+from catalyst.data.dataset import PathsDataset, _Path
+
+
+def test_PathsDataset() -> None:
+    def get_target(path: _Path) -> int:
+        result = str(path).split(".")[0].split("_")[1]
+        result = int(result)
+
+        return result
+
+    def identity(x):
+        return x
+
+    filenames = ["path1_1.jpg", "path2_1.jpg", "path_0.jpg"]
+    targets = [1, 1, 0]
+
+    dataset = PathsDataset(filenames, open_fn=identity, label_fn=get_target)
+
+    result = True
+    for data, target in zip(dataset.data, targets):
+        result &= data["targets"] == target
+        assert result
