0964c62de68f23d3701cb317cab8dd4f4f6dae72
Sergey Kolesnikov <scitator@gmail.com>
2019-09-09 20:45:31 +0300

    convolution net update

--- catalyst/contrib/models/functional.py
+++ catalyst/contrib/models/functional.py
@@ -3,8 +3,9 @@ from functools import partial
 
 import torch.nn as nn
 
-from .sequential import SequentialNet
+from catalyst.contrib.registry import MODULES
 from catalyst import utils
+from .sequential import SequentialNet
 
 
 def get_convolution_net(
@@ -15,12 +16,12 @@ def get_convolution_net(
     strides: List = None,
     groups: List = None,
     use_bias: bool = False,
-    use_normalization: bool = False,
+    normalization: str = None,
     dropout_rate: float = None,
     activation: str = "ReLU"
 ) -> nn.Module:
 
-    channels = channels or [16, 32, 16]
+    channels = channels or [32, 64, 64]
     kernel_sizes = kernel_sizes or [8, 4, 3]
     strides = strides or [4, 2, 1]
     groups = groups or [1, 1, 1]
@@ -29,8 +30,9 @@ def get_convolution_net(
 
     def _get_block(**conv_params):
         layers = [nn.Conv2d(**conv_params)]
-        if use_normalization:
-            layers.append(nn.InstanceNorm2d(conv_params["out_channels"]))
+        if normalization is not None:
+            normalization_fn = MODULES.get_if_str(normalization)
+            layers.append(normalization_fn(conv_params["out_channels"]))
         if dropout_rate is not None:
             layers.append(nn.Dropout2d(p=dropout_rate))
         layers.append(activation_fn(inplace=True))
