8ef3f88cfe38c9178ec53cca4025eeacbe230741
Sergey Kolesnikov <scitator@gmail.com>
2019-09-20 10:27:13 +0300

    Rl/env baseline (#369)
    * models functional refactoring
    
    * sequential networks fixes
    
    * raw reward support for valid sampler
    
    * tests update

--- catalyst/contrib/models/functional.py
+++ catalyst/contrib/models/functional.py
@@ -1,11 +1,8 @@
 from typing import List, Union
-from functools import partial
 
 import torch.nn as nn
 
-from catalyst.contrib.registry import MODULES
-from catalyst import utils
-from .sequential import SequentialNet
+from .sequential import SequentialNet, _process_additional_params
 
 
 def get_convolution_net(
@@ -15,50 +12,52 @@ def get_convolution_net(
     kernel_sizes: List = None,
     strides: List = None,
     groups: List = None,
-    use_bias: bool = False,
-    normalization: str = None,
-    dropout_rate: float = None,
-    activation: str = "ReLU"
+    use_bias: Union[bool, List] = False,
+    normalization: Union[str, List] = None,
+    dropout_rate: Union[float, List] = None,
+    activation: Union[str, List] = None,
+    residual: Union[bool, str] = False,
+    layer_order: List = None,
 ) -> nn.Module:
 
     channels = channels or [32, 64, 64]
     kernel_sizes = kernel_sizes or [8, 4, 3]
     strides = strides or [4, 2, 1]
     groups = groups or [1, 1, 1]
-    activation_fn = nn.__dict__[activation]
     assert len(channels) == len(kernel_sizes) == len(strides) == len(groups)
+    use_bias = _process_additional_params(use_bias, channels)
 
-    def _get_block(**conv_params):
-        layers = [nn.Conv2d(**conv_params)]
-        if normalization is not None:
-            normalization_fn = MODULES.get_if_str(normalization)
-            layers.append(normalization_fn(conv_params["out_channels"]))
-        if dropout_rate is not None:
-            layers.append(nn.Dropout2d(p=dropout_rate))
-        layers.append(activation_fn(inplace=True))
-        return layers
+    layer_fn = [
+        {
+            "module": nn.Conv2d,
+            "bias": bias,
+            "kernel_size": kernel_size,
+            "stride": stride,
+            "groups": group,
+        }
+        for bias, kernel_size, stride, group
+        in zip(use_bias, kernel_sizes, strides, groups)
+    ]
+
+    if dropout_rate is not None:
+        dropout_fn = {"module": nn.Dropout2d, "p": dropout_rate} \
+            if isinstance(dropout_rate, float) \
+            else [
+            {"module": nn.Dropout2d, "p": p} if p is not None else None
+            for p in dropout_rate]
+    else:
+        dropout_fn = None
 
     channels.insert(0, history_len * in_channels)
-    params = []
-    for i, (in_channels, out_channels) in enumerate(utils.pairwise(channels)):
-        params.append(
-            {
-                "in_channels": in_channels,
-                "out_channels": out_channels,
-                "bias": use_bias,
-                "kernel_size": kernel_sizes[i],
-                "stride": strides[i],
-                "groups": groups[i],
-            }
-        )
-
-    layers = []
-    for block_params in params:
-        layers.extend(_get_block(**block_params))
-
-    net = nn.Sequential(*layers)
-    net.apply(utils.create_optimal_inner_init(activation_fn))
-
+    net = SequentialNet(
+        hiddens=channels,
+        layer_fn=layer_fn,
+        norm_fn=normalization,
+        dropout_fn=dropout_fn,
+        activation_fn=activation,
+        residual=residual,
+        layer_order=layer_order,
+    )
     return net
 
 
@@ -66,25 +65,34 @@ def get_linear_net(
     in_features: int,
     history_len: int = 1,
     features: List = None,
-    use_bias: bool = False,
-    normalization: str = None,
-    dropout_rate: float = None,
-    activation: str = "ReLU",
+    use_bias: Union[bool, List] = False,
+    normalization: Union[str, List] = None,
+    dropout_rate: Union[float, List] = None,
+    activation: Union[str, List] = None,
     residual: Union[bool, str] = False,
     layer_order: List = None,
 ) -> nn.Module:
 
     features = features or [64, 128, 64]
-    features.insert(0, history_len * in_features)
 
+    layer_fn = {"module": nn.Linear, "bias": use_bias} \
+        if isinstance(use_bias, bool) \
+        else [{"module": nn.Linear, "bias": bias} for bias in use_bias]
+    if dropout_rate is not None:
+        dropout_fn = {"module": nn.Dropout, "p": dropout_rate} \
+            if isinstance(dropout_rate, float) \
+            else [
+            {"module": nn.Dropout, "p": p} if p is not None else None
+            for p in dropout_rate]
+    else:
+        dropout_fn = None
+
+    features.insert(0, history_len * in_features)
     net = SequentialNet(
         hiddens=features,
-        layer_fn=nn.Linear,
-        bias=use_bias,
+        layer_fn=layer_fn,
         norm_fn=normalization,
-        dropout=partial(nn.Dropout, p=dropout_rate)
-        if dropout_rate is not None
-        else None,
+        dropout_fn=dropout_fn,
         activation_fn=activation,
         residual=residual,
         layer_order=layer_order,
