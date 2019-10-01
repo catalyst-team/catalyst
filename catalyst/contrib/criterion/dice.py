2981c1d567499ea6aa1d90b541168ca4093b44bf
Vladimir Iglovikov <iglovikov@gmail.com>
2019-10-01 12:47:31 -0700

    Added weights to dice_loss

--- catalyst/contrib/criterion/dice.py
+++ catalyst/contrib/criterion/dice.py
@@ -30,16 +30,35 @@ class BCEDiceLoss(nn.Module):
         self,
         eps: float = 1e-7,
         threshold: float = None,
-        activation: str = "Sigmoid"
+        activation: str = "Sigmoid",
+        bce_weight: float = 0.5,
+        dice_weight: float = 0.5,
     ):
         super().__init__()
-        self.bce_loss = nn.BCEWithLogitsLoss()
-        self.dice_loss = DiceLoss(
-            eps=eps, threshold=threshold, activation=activation
-        )
+
+        if bce_weight == 0 and dice_weight == 0:
+            raise ValueError(
+                "Both bce_wight and dice_weight cannot be equal to 0 at the same time."
+            )
+
+        self.bce_weight = bce_weight
+        self.dice_weight = dice_weight
+
+        if self.bce_weight != 0:
+            self.bce_loss = nn.BCEWithLogitsLoss()
+
+        if self.dice_weight != 0:
+            self.dice_loss = DiceLoss(
+                eps=eps, threshold=threshold, activation=activation
+            )
 
     def forward(self, outputs, targets):
-        dice = self.dice_loss(outputs, targets)
+        if self.bce_weight == 0:
+            return self.dice_loss(outputs, targets)
+        if self.dice_loss == 0:
+            return self.bce_loss(outputs, targets)
+
+        dice = self.bce_loss(outputs, targets)
         bce = self.bce_loss(outputs, targets)
         loss = dice + bce
         return loss
