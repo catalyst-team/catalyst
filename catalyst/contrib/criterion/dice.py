f4dfaac7bc3fe98b2a0a9cf0b4347b100750f82f
Vladimir Iglovikov <ternaus@users.noreply.github.com>
2019-10-01 23:00:06 -0700

    Added weights to dice_loss (#394)
    * Added weights to dice_loss
    
    * Bugfix
    
    * Bugfix

--- catalyst/contrib/criterion/dice.py
+++ catalyst/contrib/criterion/dice.py
@@ -1,15 +1,16 @@
 from functools import partial
 
 import torch.nn as nn
+
 from catalyst.dl.utils import criterion
 
 
 class DiceLoss(nn.Module):
     def __init__(
-        self,
-        eps: float = 1e-7,
-        threshold: float = None,
-        activation: str = "Sigmoid"
+            self,
+            eps: float = 1e-7,
+            threshold: float = None,
+            activation: str = "Sigmoid"
     ):
         super().__init__()
 
@@ -27,19 +28,38 @@ class DiceLoss(nn.Module):
 
 class BCEDiceLoss(nn.Module):
     def __init__(
-        self,
-        eps: float = 1e-7,
-        threshold: float = None,
-        activation: str = "Sigmoid"
+            self,
+            eps: float = 1e-7,
+            threshold: float = None,
+            activation: str = "Sigmoid",
+            bce_weight: float = 0.5,
+            dice_weight: float = 0.5,
     ):
         super().__init__()
-        self.bce_loss = nn.BCEWithLogitsLoss()
-        self.dice_loss = DiceLoss(
-            eps=eps, threshold=threshold, activation=activation
-        )
+
+        if bce_weight == 0 and dice_weight == 0:
+            raise ValueError(
+                "Both bce_wight and dice_weight cannot be "
+                "equal to 0 at the same time."
+            )
+
+        self.bce_weight = bce_weight
+        self.dice_weight = dice_weight
+
+        if self.bce_weight != 0:
+            self.bce_loss = nn.BCEWithLogitsLoss()
+
+        if self.dice_weight != 0:
+            self.dice_loss = DiceLoss(
+                eps=eps, threshold=threshold, activation=activation
+            )
 
     def forward(self, outputs, targets):
-        dice = self.dice_loss(outputs, targets)
-        bce = self.bce_loss(outputs, targets)
-        loss = dice + bce
-        return loss
+        if self.bce_weight == 0:
+            return self.dice_weight * self.dice_loss(outputs, targets)
+        if self.dice_weight == 0:
+            return self.bce_weight * self.bce_loss(outputs, targets)
+
+        dice = self.dice_weight * self.dice_loss(outputs, targets)
+        bce = self.bce_weight * self.bce_loss(outputs, targets)
+        return dice + bce
