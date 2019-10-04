1cc3e6840444d270bd7c4da9613dedab0356d9d4
Sergey Kolesnikov <scitator@gmail.com>
2019-10-04 16:29:56 +0300

    config update

--- catalyst/dl/runner/gan.py
+++ catalyst/dl/runner/gan.py
@@ -28,22 +28,28 @@ class GanRunner(Runner):
         super()._prepare_for_stage(stage)
         self.generator = self.model[self.generator_key]
         self.discriminator = self.model[self.discriminator_key]
-        assert hasattr(self.state, "noise_dim") \
-               and self.state.noise_dim is not None
+
+        for key in [
+            "noise_dim",
+            "discriminator_train_phase",
+            "generator_train_phase"
+        ]:
+            assert hasattr(self.state, key) \
+                and getattr(self.state, key) is not None
 
     def forward(self, batch):
         real_features = batch[self.features_key]
         batch_size = real_features.shape[0]
 
-        fake_targets = torch.ones((batch_size, 1), device=self.device)
-        real_targets = torch.zeros((batch_size, 1), device=self.device)
-        self.state.input["fake_targets"] = fake_targets
+        real_targets = torch.ones((batch_size, 1), device=self.device)
+        fake_targets = torch.zeros((batch_size, 1), device=self.device)
         self.state.input["real_targets"] = real_targets
+        self.state.input["fake_targets"] = fake_targets
         z = torch.randn((batch_size, self.state.noise_dim), device=self.device)
 
         if (
             self.state.phase is None
-            or self.state.phase == "discriminator_train"
+            or self.state.phase == self.state.discriminator_train_phase
         ):
             # (None for validation mode)
             fake_features = self.generator(z)
@@ -56,7 +62,7 @@ class GanRunner(Runner):
                 "fake_logits": fake_logits,
                 "real_logits": real_logits
             }
-        else:
+        elif self.state.phase == self.state.generator_train_phase:
             fake_features = self.generator(z)
             fake_logits = self.discriminator(fake_features)
             # --> g_loss (fake logits + REAL targets)
@@ -64,6 +70,8 @@ class GanRunner(Runner):
                 "fake_features": fake_features,  # visualization purposes only
                 "fake_logits": fake_logits
             }
+        else:
+            raise NotImplementedError(f"Unknown phase: self.state.phase")
 
 
 __all__ = ["GanRunner"]
