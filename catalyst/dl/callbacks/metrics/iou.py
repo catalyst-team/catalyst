31c7230df22edcadb1b3c684453bf4c1b3c32761
Vladimir Iglovikov <iglovikov@gmail.com>
2019-10-13 10:42:05 -0700

    added dice and jaccard multiclass callbacks

--- catalyst/dl/callbacks/metrics/iou.py
+++ catalyst/dl/callbacks/metrics/iou.py
@@ -1,5 +1,10 @@
-from catalyst.dl.core import MetricCallback
+from typing import Dict
+
+import numpy as np
+
+from catalyst.dl.core import MetricCallback, Callback, RunnerState, CallbackOrder
 from catalyst.dl.utils import criterion
+from catalyst.utils.confusion_matrix import calculate_confusion_matrix_from_tensors, calculate_tp_fp_fn
 
 
 class IouCallback(MetricCallback):
@@ -8,13 +13,13 @@ class IouCallback(MetricCallback):
     """
 
     def __init__(
-        self,
-        input_key: str = "targets",
-        output_key: str = "logits",
-        prefix: str = "iou",
-        eps: float = 1e-7,
-        threshold: float = None,
-        activation: str = "Sigmoid",
+            self,
+            input_key: str = "targets",
+            output_key: str = "logits",
+            prefix: str = "iou",
+            eps: float = 1e-7,
+            threshold: float = None,
+            activation: str = "Sigmoid",
     ):
         """
         Args:
@@ -41,4 +46,75 @@ class IouCallback(MetricCallback):
 
 JaccardCallback = IouCallback
 
-__all__ = ["IouCallback", "JaccardCallback"]
+
+def calculate_jaccard(tp_fp_fn_dict: dict) -> np.array:
+    """Calculate list of Jaccard indices.
+    Args:
+        tp_fp_fn_dict: {"true_positives": true_positives,
+                        "false_positives": false_positives,
+                        "false_negatives": false_negatives}
+    Returns:
+    """
+    epsilon = 1e-7
+
+    true_positives = tp_fp_fn_dict["true_positives"]
+    false_positives = tp_fp_fn_dict["false_positives"]
+    false_negatives = tp_fp_fn_dict["false_negatives"]
+
+    jaccard = (true_positives + epsilon) / (true_positives + false_positives + false_negatives + epsilon)
+
+    if not np.all(jaccard <= 1):
+        raise ValueError("Jaccard index should be less than 1")
+
+    if not np.all(jaccard >= 0):
+        raise ValueError("Jaccard index should be more than 1")
+
+    return jaccard
+
+
+class MulticlassJaccardMetricCallback(Callback):
+    def __init__(
+            self, prefix: str = "jaccard", input_key: str = "targets", output_key: str = "logits", **metric_params
+    ):
+        super().__init__(CallbackOrder.Metric)
+        self.prefix = prefix
+        self.input_key = input_key
+        self.output_key = output_key
+        self.metric_params = metric_params
+        self.confusion_matrix = None
+        self.class_names = metric_params["class_names"]  # dictionary {class_id: class_name}
+        self.class_prefix = metric_params["class_prefix"]
+
+    def _reset_stats(self):
+        self.confusion_matrix = None
+
+    def on_batch_end(self, state: RunnerState):
+        outputs = state.output[self.output_key]
+        targets = state.input[self.input_key]
+
+        confusion_matrix = calculate_confusion_matrix_from_tensors(outputs, targets)
+
+        if self.confusion_matrix is None:
+            self.confusion_matrix = confusion_matrix
+        else:
+            self.confusion_matrix += confusion_matrix
+
+    def on_loader_end(self, state: RunnerState):
+
+        tp_fp_fn_dict = calculate_tp_fp_fn(self.confusion_matrix)
+
+        batch_metrics: Dict = calculate_jaccard(tp_fp_fn_dict)
+
+        for metric_id, jaccard_value in batch_metrics.items():
+            if metric_id not in self.class_names:
+                continue
+
+            metric_name = self.class_names[metric_id]
+            state.metrics.epoch_values[state.loader_name][f"{self.class_prefix}_{metric_name}"] = jaccard_value
+
+        state.metrics.epoch_values[state.loader_name]["mean"] = np.mean([x for x in batch_metrics.values()])
+
+        self._reset_stats()
+
+
+__all__ = ["IouCallback", "JaccardCallback", "MulticlassJaccardMetricCallback"]
