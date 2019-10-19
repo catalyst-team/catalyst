29d45001987987fd38fcbf9641804e930990ec87
Evgeny Semyonov <lightsanweb@yandex.ru>
2019-10-19 09:38:56 +0300

    the comment has been formatted to Google CodeStyle

--- catalyst/dl/runner/wandb.py
+++ catalyst/dl/runner/wandb.py
@@ -17,11 +17,13 @@ class WandbRunner(Runner):
     @staticmethod
     def _log_metrics(metrics: Dict, mode: str, suffix: str = ""):
         def key_locate(key: str):
-            """
-            wandb used first symbol _ for it service purposes,
+            """wandb uses first symbol _ for it service purposes
             because of that fact, we can not send original metric names
-            :param key: metric name
-            :return: formatted metric name
+
+            Args:
+                key: metric name
+            Returns:
+                formatted metric name
             """
             if key.startswith("_"):
                 return key[1:]
