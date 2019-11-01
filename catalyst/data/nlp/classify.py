ee32f0338a73f34cab5da45e9f07916aa9773d25
egorplotnikov <egorplotnikov@mail.ru>
2019-11-01 18:06:30 +0300

    add sequence bucketing

--- catalyst/data/nlp/classify.py
+++ catalyst/data/nlp/classify.py
@@ -3,64 +3,122 @@ import pandas as pd
 import logging
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from transformers import DistilBertTokenizer
+import numpy as np
 
 
 class ClassificationDataset(Dataset):
-    def __init__(self, texts: pd.Series, labels: pd.Series, max_seq_length=512):
-        logging.getLogger("transformers.tokenization_utils").setLevel(
-            logging.FATAL
-        )
+	def __init__(self, texts: pd.Series, labels: pd.Series, max_seq_length=512, batch_size=None, use_seq_bucketing=False):
+		logging.getLogger("transformers.tokenization_utils").setLevel(
+			logging.FATAL
+		)
 
-        # pandas Series with texts to classify
-        self.texts = texts
-        # pandas Series with classification labels (strings)
-        self.labels = labels
+		if use_seq_bucketing is True and batch_size is None:
+			raise Exception("You cannot use sequence bucketing without batch_size flag")
 
-        # {'class1': 0, 'class2': 1, 'class3': 2, ...}
-        # using this instead of `sklearn.preprocessing.LabelEncoder`
-        # no easily handle unknown target values
-        self.label_dict = dict(zip(labels.unique(), range(labels.nunique())))
-        self.max_seq_length = max_seq_length
+		self.batch_size = batch_size
+		self.use_seq_bucketing = use_seq_bucketing
 
-        self.tokenizer = DistilBertTokenizer.from_pretrained(
-            "distilbert-base-uncased"
-        )
-        self.sep_vid = self.tokenizer.vocab["[SEP]"]
-        self.cls_vid = self.tokenizer.vocab["[CLS]"]
-        self.pad_vid = self.tokenizer.vocab["[PAD]"]
+		# pandas Series with texts to classify
+		self.texts = texts
+		# pandas Series with classification labels (strings)
+		self.labels = labels
 
-    def __len__(self):
-        return len(self.texts)
+		# {'class1': 0, 'class2': 1, 'class3': 2, ...}
+		# using this instead of `sklearn.preprocessing.LabelEncoder`
+		# no easily handle unknown target values
+		self.label_dict = dict(zip(labels.unique(), range(labels.nunique())))
+		self.max_seq_length = max_seq_length
 
-    def __getitem__(self, index) -> Mapping[str, torch.Tensor]:
+		self.tokenizer = DistilBertTokenizer.from_pretrained(
+			"distilbert-base-uncased"
+		)
+		self.sep_vid = self.tokenizer.vocab["[SEP]"]
+		self.cls_vid = self.tokenizer.vocab["[CLS]"]
+		self.pad_vid = self.tokenizer.vocab["[PAD]"]
 
-        x, y = self.texts[index], self.labels[index]
+		self.frame_with_pad = None
 
-        x_encoded = self.tokenizer.encode(
-            x,
-            add_special_tokens=True,
-            max_length=self.max_seq_length,
-            return_tensors="pt",
-        ).squeeze(0)
+		if use_seq_bucketing is True:
+			self.frame_with_pad = self._generate_ind_list_()
 
-        true_seq_length = x_encoded.size(0)
-        pad_size = self.max_seq_length - true_seq_length
+	def _generate_ind_list_(self):
+		texts_length = self.texts.apply(lambda text: len(self.tokenizer.encode(
+														text, 
+														add_special_tokens=True, 
+														max_length=self.max_seq_length,
+													))
+		)
 
-        pad_ids = torch.Tensor([self.pad_vid] * pad_size).long()
-        x_tensor = torch.cat((x_encoded, pad_ids))
+		texts_length.sort_values(inplace=True, ascending=False)
+		texts_length.name = "texts length"
+		texts_length = texts_length.reset_index()
 
-        y_encoded = torch.Tensor([self.label_dict.get(y, -1)]).long().squeeze(0)
+		pad_length = []
 
-        mask = torch.ones_like(x_encoded, dtype=int)
-        mask_pad = torch.zeros_like(pad_ids, dtype=int)
-        mask = torch.cat((mask, mask_pad))
+		i = 0
+		for bs in range(len(texts_length) // self.batch_size + 1):
+			texts_length_batch = texts_length.iloc[i:i + self.batch_size]
+			i += self.batch_size
+			if texts_length_batch.empty is True:
+				break
+			max_len_batch = texts_length_batch['texts length'].max()
+			lengths = list(texts_length_batch['texts length'])
+			pad_length.extend(
+				[
+					max_len_batch - lengths[k]
+					for k in range(len(lengths))
+				]
+			)
+		assert len(pad_length) == len(texts_length)
+		texts_length['pad length'] = pad_length
+		
+		return texts_length
 
-        return {
-            "features": x_tensor,
-            "targets": y_encoded,
-            'mask': mask
-        }
+
+	def __len__(self):
+		return len(self.texts)
+
+	def __getitem__(self, index) -> Mapping[str, torch.Tensor]:
+
+		metadata = None
+		if self.use_seq_bucketing is True:
+			metadata = self.frame_with_pad.iloc[index]
+			x, y = 	self.texts[metadata["index"]], self.labels[metadata["index"]]
+		else:
+			x, y = self.texts[index], self.labels[index]
+
+		#TODO: remove max_length flag and use custom function for seq cropping
+		# (eg. 128 head tokens, 128 tail tokens)
+		x_encoded = self.tokenizer.encode(
+			x,
+			add_special_tokens=True,
+			max_length=self.max_seq_length,
+			return_tensors="pt",
+		).squeeze(0)
+
+		true_seq_length = x_encoded.size(0)
+
+		if self.use_seq_bucketing is True:
+			pad_size = metadata["pad length"]
+		else:
+			pad_size = self.max_seq_length - true_seq_length
+
+		pad_ids = torch.Tensor([self.pad_vid] * pad_size).long()
+		x_tensor = torch.cat((x_encoded, pad_ids))
+
+		y_encoded = torch.Tensor([self.label_dict.get(y, -1)]).long().squeeze(0)
+
+		mask = torch.ones_like(x_encoded, dtype=int)
+		mask_pad = torch.zeros_like(pad_ids, dtype=int)
+		mask = torch.cat((mask, mask_pad))
+
+		return {
+			"features": x_tensor,
+			"targets": y_encoded,
+			'mask': mask
+		}
 
 
 __all__ = ["ClassificationDataset"]
---
+++ catalyst/data/nlp/tests/test_classification_dataset.py
@@ -0,0 +1,52 @@
+import unittest
+
+import numpy as np
+from numpy.testing import assert_array_equal
+
+from catalyst.data.nlp.classify import ClassificationDataset
+
+from torch.utils.data import DataLoader
+
+import pandas as pd
+
+texts = pd.Series([
+    "Ryan is great at researching new technologies ",
+    "and companies and providing the right kind of analysis for PM",
+    "I think you wanna be a research scientist",
+    "An important concept in the paper is Back-Translation, in which a sentence is translated to the target language and back to the source.",
+    "This is a common pre-processing algorithm and a summary of it can be found here.",
+    "Each training sample consists of the same text in two languages, whereas in BERT each sample is built from a single language.",
+    "The new metadata helps the model learn the relationship between related tokens in different languages."
+])
+
+classes = pd.Series(['a', 'a', 'a', 'b', 'a', 'a', 'b'])
+
+"""
+frame_with_pad table in dataset for this texts
+
+   index  texts length  pad length
+0      3            29           0
+1      5            26           3
+2      4            20           9
+3      6            19           0
+4      1            13           6
+5      2            10           9
+6      0             9           0
+
+"""
+
+class ClassificationDatasetTests(unittest.TestCase):
+	def test_seq_bucketing(self):
+		dataset = ClassificationDataset(texts, classes, batch_size=3, use_seq_bucketing=True)
+		data, tg = dataset[0]["features"], dataset[0]["targets"]
+		self.assertEqual(1, tg.item())
+
+	def test_batches(self):
+		dataset = ClassificationDataset(texts, classes, batch_size=3, use_seq_bucketing=True)
+		loader = DataLoader(dataset, batch_size=3)
+		shapes = [29, 19, 9]
+
+		ans = []
+		for batch in loader:
+			ans.append(batch['features'].shape[1])
+		self.assertEqual(shapes, ans)
