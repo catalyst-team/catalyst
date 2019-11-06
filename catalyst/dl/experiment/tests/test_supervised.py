d1a151319dde9ed3aeebcf2cfbf73d4e1fd48ed6
Alex Gaziev <alex.gaziev@gmail.com>
2019-11-06 14:56:08 -0800

    Test for SupervisedExperiment defaults

---
+++ catalyst/dl/experiment/tests/test_supervised.py
@@ -0,0 +1,29 @@
+import torch
+from collections import OrderedDict
+from catalyst.dl.experiment.supervised import SupervisedExperiment
+from catalyst.dl.callbacks import (
+    CriterionCallback, ConsoleLogger, CheckpointCallback, OptimizerCallback,
+    RaiseExceptionCallback, TensorboardLogger
+)
+
+
+DEFAULT_CALLBACKS = OrderedDict([
+    ('_saver', CheckpointCallback),
+    ('console', ConsoleLogger),
+    ('tensorboard', TensorboardLogger),
+    ('exception', RaiseExceptionCallback)])
+
+
+def test_defaults():
+    model = torch.nn.Module()
+    dataset = torch.utils.data.Dataset()
+    dataloader = torch.utils.data.DataLoader(dataset)
+    loaders = OrderedDict()
+    loaders['train'] = dataloader
+
+    exp = SupervisedExperiment(model=model, loaders=loaders)
+
+    assert exp.get_callbacks("train").keys() == DEFAULT_CALLBACKS.keys()
+    cbs = zip(exp.get_callbacks("train").values(), DEFAULT_CALLBACKS.values())
+    for cb, klass in cbs:
+        assert isinstance(cb, klass)
