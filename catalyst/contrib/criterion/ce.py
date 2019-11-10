0a7f0668b783be9a5648cc839ab68195c35fd057
Bogdan Ivanyuk <35574157+KyloRen1@users.noreply.github.com>
2019-11-10 10:07:19 +0200

    Update ce.py
    implementation of Symmetric Cross Entropu

--- catalyst/contrib/criterion/ce.py
+++ catalyst/contrib/criterion/ce.py
@@ -14,3 +14,23 @@ class NaiveCrossEntropyLoss(nn.Module):
         loss = -torch.sum(input * target)
         loss = loss / input.size()[0] if self.size_average else loss
         return loss
+    
+class SymmetricCrossEntropyLoss(nn.Module):
+	def __init__(self, alpha, beta, num_classes = 10):
+		super(SymmetricCrossEntropyLoss, self).__init__()
+		self.alpha = alpha
+		self.beta = beta
+		self.num_classes = num_classes
+		self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+	def forward(self, input, target):
+		assert input.size() == target.size()
+		target_one_hot = F.one_hot(target, self.num_classes).float().to(self.device) 
+
+		input = torch.clamp(input, min=1e-7, max=1.0)
+		target_one_hot = torch.clamp(target_one_hot, min=1e-4, max=1.0)
+
+		cross_entropy = (-torch.sum(target_one_hot * torch.log(input), dim=1)).mean()
+		reverse_cross_entropy = (-torch.sum(input * torch.log(target_one_hot), dim=1)).mean()
+		loss = self.alpha * cross_entropy + self.beta * reverse_cross_entropy
+		return loss
