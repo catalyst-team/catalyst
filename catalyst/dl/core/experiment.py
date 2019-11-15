a0029491ff8aa2cc4f155608b3da3a2b93ad7e40
Tezikov Roman <tez.romach@gmail.com>
2019-11-15 19:59:39 +0300

    data_index

--- catalyst/dl/core/experiment.py
+++ catalyst/dl/core/experiment.py
@@ -106,13 +106,19 @@ class Experiment(ABC):
         """Returns the data transforms for a given stage and mode"""
         raise NotImplementedError
 
-    def get_native_batch(self, stage: str, loader: Union[str, int] = 0):
+    def get_native_batch(
+        self,
+        stage: str,
+        loader: Union[str, int] = 0,
+        data_index: int = 0
+    ):
         """Returns a batch from experiment loader
 
         Args:
             stage (str): stage name
             loader (Union[str, int]): loader name or its index,
                 default is the first loader
+            data_index (int): index in dataset from the loader
         """
         loaders = self.get_loaders(stage)
         if isinstance(loader, str):
@@ -125,7 +131,7 @@ class Experiment(ABC):
         dataset = _loader.dataset
         collate_fn = _loader.collate_fn
 
-        sample = collate_fn([dataset[0]])
+        sample = collate_fn([dataset[data_index]])
 
         return sample
 
