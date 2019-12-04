fa38a851dc3375ed0504619814e356f4c2a8e182
Nikita Pokidyshev <nv.pokidyshev@gmail.com>
2019-12-04 21:11:08 +0000

    Sort imports

--- catalyst/dl/callbacks/metrics/__init__.py
+++ catalyst/dl/callbacks/metrics/__init__.py
@@ -5,7 +5,7 @@ from .auc import AUCCallback
 from .dice import DiceCallback
 from .f1_score import F1ScoreCallback
 from .iou import (
-    IouCallback, JaccardCallback, 
-    ClasswiseIouCallback, ClasswiseJaccardCallback,
+    ClasswiseIouCallback, ClasswiseJaccardCallback, IouCallback,
+    JaccardCallback
 )
 from .ppv_tpr_f1 import PrecisionRecallF1ScoreCallback
