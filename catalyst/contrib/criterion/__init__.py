95b1bd106fcb217f4e5afd7a416090e25087bd07
Bogdan Ivanyuk <ivanyuk.bogdan1999@gmail.com>
2019-12-22 13:16:19 +0200

    fixed

--- catalyst/contrib/criterion/__init__.py
+++ catalyst/contrib/criterion/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 from torch.nn.modules.loss import *
 
-from .ce import NaiveCrossEntropyLoss, SymmetricCrossEntropyLoss,MaskCrossEntropyLoss
+from .ce import MaskCrossEntropyLoss, SymmetricCrossEntropyLoss,NaiveCrossEntropyLoss
 from .center import CenterLoss
 from .contrastive import ContrastiveDistanceLoss, ContrastiveEmbeddingLoss
 from .dice import BCEDiceLoss, DiceLoss
