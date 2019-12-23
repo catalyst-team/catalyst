cc1c02e1e0e973d6aceab92fa3b387370a3ca52e
smivv <smivvla@gmail.com>
2019-12-23 20:04:30 +0300

    Parameter `cv_loader_names` refactored. Functions order changed. Added some checks.

--- catalyst/contrib/callbacks/knn.py
+++ catalyst/contrib/callbacks/knn.py
@@ -3,7 +3,7 @@ from catalyst.dl.core import Callback, RunnerState, CallbackOrder
 import torch
 import numpy as np
 
-from typing import List
+from typing import List, Dict
 
 from math import ceil
 from scipy import stats
@@ -25,7 +25,7 @@ class KNNMetricCallback(Callback):
             prefix: str = "knn",
             num_classes: int = 2,
             class_names: dict = None,
-            cv_loader_names: List[str] = None,
+            cv_loader_names: Dict[str, List[str]] = None,
             metric_fn: str = "f1-score",
             knn_metric: str = "euclidean",
             n_neighbors: int = 5
@@ -37,106 +37,45 @@ class KNNMetricCallback(Callback):
             prefix (str): key to store in logs
             num_classes (int): Number of classes; must be > 1
             class_names (dict): of indexes and class names
-            cv_loader_names (list): list of loader_names for which cross
-                                    validation should be calculated.
+            cv_loader_names (dict): dict with keys and values of loader_names
+                                    for which cross validation should be
+                                    calculated.
+                                    For example {"train" : ["valid", "test"]}
             metric_fn (str): one of `accuracy`, `precision`, `recall`, 
                             `f1-score`, default is `f1-score`
             knn_metric (str): look sklearn.neighbors.NearestNeighbors parameter
             n_neighbors (int): number of neighbors, default is 5
         """
-
         super().__init__(CallbackOrder.Metric)
+
+        assert num_classes > 1, "`num_classes` should be more than 1"
+
+        metric_fns = {
+            "accuracy": accuracy_score,
+            "recall": recall_score,
+            "precision": precision_score,
+            "f1-score": f1_score,
+        }
+
+        assert metric_fn in metric_fns, \
+            f"Metric function with value `{metric_fn}` not implemented"
+
         self.prefix = prefix
         self.features_key = input_key
         self.targets_key = output_key
 
         self.num_classes = num_classes
-        self.class_names = class_names
+        self.class_names = class_names \
+            if class_names is not None \
+            else [str(i) for i in range(num_classes)]
 
-        self.cv_loader_names = [] \
-            if cv_loader_names is None else cv_loader_names
+        self.cv_loader_names = cv_loader_names
 
-        if metric_fn == "accuracy":
-            self.metric_fn = accuracy_score
-        elif metric_fn == "recall":
-            self.metric_fn = recall_score
-        elif metric_fn == "precision":
-            self.metric_fn = precision_score
-        elif metric_fn == "f1-score":
-            self.metric_fn = f1_score
-        else:
-            raise ValueError(f"Metric function with value `{metric_fn}`"
-                             f" not implemented")
-
+        self.metric_fn = metric_fns[metric_fn]
         self.knn_metric = knn_metric
         self.n_neighbors = n_neighbors
 
         self.n_folds = 1
-
-        self.reset_cache()
-        self.reset_sets()
-
-    def on_batch_end(self, state: RunnerState):
-        features: torch.Tensor = \
-            state.output[self.features_key].cpu().detach().numpy()
-        targets: torch.Tensor = \
-            state.input[self.targets_key].cpu().detach().numpy()
-
-        self.features.extend(features)
-        self.targets.extend(targets)
-
-    def on_loader_end(self, state: RunnerState):
-
-        self.features = np.stack(self.features)
-        self.targets = np.stack(self.targets)
-
-        if len(np.unique(self.targets)) > self.num_classes:
-            raise Warning("Targets has more classes than num_classes")
-
-        s = {
-            "values": self.features,
-            "labels": self.targets,
-        }
-
-        self.sets[state.loader_name] = s
-
-        y_true, y_pred = self._knn(s)
-
-        loader_values = state.metrics.epoch_values[state.loader_name]
-        if self.num_classes == 2:
-            loader_values[self.prefix] = \
-                self.metric_fn(y_true, y_pred, average="binary")
-        else:
-            values = self.metric_fn(y_true, y_pred, average=None)
-
-            loader_values[f"{self.prefix}"] = np.mean(values)
-            for i, value in enumerate(values):
-                postfix = self.class_names[i] \
-                    if self.class_names is not None else str(i)
-                loader_values[f"{self.prefix}/{postfix}"] = value
-
-        self.reset_cache()
-
-    def on_epoch_end(self, state: RunnerState):
-
-        for s in self.cv_loader_names:
-            if s in self.sets:
-                y_true, y_pred = self._knn(self.sets["train"], self.sets[s])
-
-                loader_values = \
-                    state.metrics.epoch_values[f"train_{s}_cv"]
-                if self.num_classes == 2:
-                    loader_values[f"{self.prefix}"] = \
-                        self.metric_fn(y_true, y_pred, average="binary")
-                else:
-                    values = self.metric_fn(y_true, y_pred, average=None)
-
-                    loader_values[f"{self.prefix}"] = np.mean(values)
-                    for i, value in enumerate(values):
-                        postfix = self.class_names[i] \
-                            if self.class_names is not None else str(i)
-                        loader_values[f"{self.prefix}/{postfix}"] = \
-                            value
 
         self.reset_cache()
         self.reset_sets()
@@ -156,7 +95,6 @@ class KNNMetricCallback(Callback):
         Returns:
             cm: np.ndarray of confusion matrix
         """
-
         # if the test_set is None, we will test train_set on itself,
         # in that case we need to delete the closest neighbor
         leave_one_out = test_set is None
@@ -196,6 +134,7 @@ class KNNMetricCallback(Callback):
                     if leave_one_out:
                         knn_ids = knn_ids[:, 1:]
 
+                    # calculate the most frequent class across k neighbors
                     knn_classes = y_train[knn_ids]
                     knn_classes, _ = stats.mode(knn_classes, axis=1)
 
@@ -213,6 +152,84 @@ class KNNMetricCallback(Callback):
                 result = None
 
         return result
+
+    def on_batch_end(self, state: RunnerState):
+        features: torch.Tensor = \
+            state.output[self.features_key].cpu().detach().numpy()
+        targets: torch.Tensor = \
+            state.input[self.targets_key].cpu().detach().numpy()
+
+        self.features.extend(features)
+        self.targets.extend(targets)
+
+    def on_loader_end(self, state: RunnerState):
+
+        self.features = np.stack(self.features)
+        self.targets = np.stack(self.targets)
+
+        if len(np.unique(self.targets)) > self.num_classes:
+            raise Warning("Targets has more classes than num_classes")
+
+        s = {
+            "values": self.features,
+            "labels": self.targets,
+        }
+
+        self.sets[state.loader_name] = s
+
+        y_true, y_pred = self._knn(s)
+
+        loader_values = state.metrics.epoch_values[state.loader_name]
+        if self.num_classes == 2:
+            loader_values[self.prefix] = \
+                self.metric_fn(y_true, y_pred, average="binary")
+        else:
+            values = self.metric_fn(y_true, y_pred, average=None)
+
+            loader_values[f"{self.prefix}"] = np.mean(values)
+            for i, value in enumerate(values):
+                loader_values[f"{self.prefix}/{self.class_names[i]}"] = value
+
+        self.reset_cache()
+
+    def on_epoch_end(self, state: RunnerState):
+
+        if self.cv_loader_names is not None:
+            for k, vs in self.cv_loader_names.items():
+
+                # checking for presence of subset
+                if k not in self.sets:
+                    print(f"Set `{k}` not found in the sets." 
+                          f"Please change `cv_loader_names` parameter.")
+                    continue
+
+                for v in vs:
+
+                    # checking for presence of subset
+                    if v not in self.sets:
+                        print(f"Set `{v}` not found in the sets." 
+                              f"Please change `cv_loader_names` parameter.")
+                        continue
+
+                    y_true, y_pred = \
+                        self._knn(self.sets[k], self.sets[v])
+
+                    loader_values = \
+                        state.metrics.epoch_values[f"{k}_{v}_cv"]
+
+                    if self.num_classes == 2:
+                        loader_values[f"{self.prefix}"] = \
+                            self.metric_fn(y_true, y_pred, average="binary")
+                    else:
+                        values = self.metric_fn(y_true, y_pred, average=None)
+
+                        loader_values[f"{self.prefix}"] = np.mean(values)
+                        for i, value in enumerate(values):
+                            prefix = f"{self.prefix}/{self.class_names[i]}"
+                            loader_values[prefix] = value
+
+        self.reset_cache()
+        self.reset_sets()
 
 
 __all__ = ["KNNMetricCallback"]
