bc29d0014a19b32a754f4fd6c2376cfea845f1df
Nikita Pokidyshev <nv.pokidyshev@gmail.com>
2019-12-25 16:58:27 +0000

    Faster F1 computation

--- catalyst/dl/meters/ppv_tpr_f1_meter.py
+++ catalyst/dl/meters/ppv_tpr_f1_meter.py
@@ -1,8 +1,6 @@
 from collections import defaultdict
 import numbers
 
-import numpy as np
-
 import torch
 
 from . import meter
@@ -72,32 +70,21 @@ class PrecisionRecallF1ScoreMeter(meter.Meter):
         Thresholds predictions and calculates the true positives,
         false positives, and false negatives in comparison to the target.
         Args:
-            output (torch.Tensor/numpy.ndarray/numbers.Number):
+            output (torch.Tensor):
                 prediction after activation function
                 shape should be (batch_size, ...), but works with any shape
-            target (torch.Tensor/numpy.ndarray/numbers.Number):
+            target (torch.Tensor):
                 label (binary)
                 shape should be the same as output's shape
         Returns:
             None
         """
-        if torch.is_tensor(output):
-            output = output.cpu().squeeze().numpy()
-        if torch.is_tensor(target):
-            target = target.cpu().squeeze().numpy()
-        elif isinstance(target, numbers.Number):
-            target = np.asarray([target])
-        target = np.int32(target)
-        assert output.size == target.size, \
-            "outputs and targets must have the same number of elements"
-        assert np.all(np.add(np.equal(target, 1), np.equal(target, 0))), \
-            "targets should be binary (0, 1)"
+        
+        output = (output > self.threshold).float()
 
-        output = (output > self.threshold).astype(np.int32)
-
-        tp = np.sum(target * output)
-        fp = np.sum(output) - tp
-        fn = np.sum(target) - tp
+        tp = torch.sum(target * output)
+        fp = torch.sum(output) - tp
+        fn = torch.sum(target) - tp
 
         self.tp_fp_fn_counts["tp"] += tp
         self.tp_fp_fn_counts["fp"] += fp
