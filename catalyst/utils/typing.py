cbe10bbc4c4076a9bc3cbf63a117e43cf60e746b
Artem Zolkin <arquestro93@gmail.com>
2020-01-10 00:11:07 +0300

    Added new types for typing.

--- catalyst/utils/typing.py
+++ catalyst/utils/typing.py
@@ -1,12 +1,23 @@
-from typing import Union  # isort:skip
+from typing import Union, Dict  # isort:skip
 
 import torch
 from torch import nn, optim
+from torch.utils import data
 
-Model = nn.Module
+Model = Union[nn.Module, Dict[str, nn.Module]]
 Criterion = nn.Module
-Optimizer = optim.Optimizer
+Optimizer = Union[optim.Adam, optim.SGD]
 Scheduler = optim.lr_scheduler._LRScheduler  # noinspection PyProtectedMember
+Dataset = data.Dataset
+DataLoader = data.DataLoader
 Device = Union[str, torch.device]
 
-__all__ = ["Model", "Criterion", "Optimizer", "Scheduler", "Device"]
+__all__ = [
+    "Model",
+    "Criterion",
+    "Optimizer",
+    "Scheduler",
+    "Device",
+    "Dataset",
+    "DataLoader"
+]
