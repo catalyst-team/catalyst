6f8bed11113f339e20be9da86a5a6f8058711204
Teamcity <teamcity@catalyst.github>
2020-01-15 14:48:36 +0000

    remove duplicated blocks of code from catalyst-init script (#588)

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '19.12.1',
+    VERSION: '20.01',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
