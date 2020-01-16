9adb8aa3f084ddf636b30715c9a2c552456700ac
Evgeny Semyonov <lightsanweb@yandex.ru>
2020-01-16 12:09:43 +0300

    diff

---
+++ teamcity/tests.sh
@@ -0,0 +1,17 @@
+echo "pip install -r requirements/requirements.txt"
+pip install -r requirements/requirements.txt
+
+echo "pip install -r requirements/requirements-nlp.txt"
+pip install -r requirements/requirements-nlp.txt
+
+echo "pip install -r requirements/requirements-rl.txt"
+pip install -r requirements/requirements-rl.txt
+
+echo "pip install -r requirements/requirements-dev.txt"
+pip install -r requirements/requirements-dev.txt
+
+echo "isort -rc --check-only --settings-path ./setup.cfg"
+isort -rc --check-only --settings-path ./setup.cfg
+
+echo "make check-style"
+make check-style
