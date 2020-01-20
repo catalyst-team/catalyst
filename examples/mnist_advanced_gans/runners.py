563ae835dd52defc52f3b4c64ca7455363ac585e
asmekal <andrey.zharkov@phystech.edu>
2020-01-20 23:20:59 +0300

    tiny runners refactoring + documentation

--- examples/mnist_advanced_gans/runners.py
+++ examples/mnist_advanced_gans/runners.py
@@ -25,11 +25,13 @@ class MultiPhaseRunner(Runner):
         :param model:
         :param device:
         :param input_batch_keys: list of strings of keys for batch elements, e.g.
-            input_batch_keys = ["features", "targets"] and your DataLoader returns 2 tensors (images and targets)
+            input_batch_keys = ["features", "targets"] and your
+            DataLoader returns 2 tensors (images and targets)
             when state.input will be {"features": batch[0], "targets": batch[1]}
-        :param registered_phases: Tuple of pairs (phase_name, phase_forward_function)
-            phase_forward_function's may be also str, in that case Runner should have method with same name,
-            which will be called
+        :param registered_phases:
+            Tuple of pairs (phase_name, phase_forward_function)
+            phase_forward_function's may be also str, in that case Runner
+            should have method with same name, which will be called
         """
         super().__init__(model, device)
 
@@ -65,9 +67,9 @@ class MultiPhaseRunner(Runner):
 
     def _prepare_for_stage(self, stage: str):
         super()._prepare_for_stage(stage)
-        self._memorize_models(stage=stage)
+        self._alias_inner_params(stage=stage)
 
-    def _memorize_models(self, stage: str):
+    def _alias_inner_params(self, stage: str):
         """Maybe useful to memorize something for convenience
         e.g. self.generator = self.models["generator"]
         """
@@ -78,7 +80,7 @@ class GANRunner(MultiPhaseRunner):
     """
     Runner with logic for single-generator single-discriminator GAN training
 
-    Various conditioning types, penalties and regularizations (such as WGAN-GP)
+    Various conditioning types, penalties and regularization (such as WGAN-GP)
     can be easily derived from this class
     """
 
@@ -106,6 +108,50 @@ class GANRunner(MultiPhaseRunner):
             generator_model_key: str = "generator",
             discriminator_model_key: str = "discriminator"
     ):
+        """
+
+        :param model:
+        :param device:
+        :param input_batch_keys: list of strings of keys for batch elements, e.g.
+            input_batch_keys = ["features", "targets"] and
+            your DataLoader returns 2 tensors (images and targets)
+            when state.input will be {"features": batch[0], "targets": batch[1]}
+
+        INPUT KEYS:
+        :param data_input_key: real distribution to fit
+        :param class_input_key: labels for real distribution
+        :param noise_input_key: noise
+
+        OUTPUT KEYS:
+        :param fake_logits_output_key:  prediction scores of discriminator for
+            fake data
+        :param real_logits_output_key: prediction scores of discriminator for
+            real data
+        :param fake_data_output_key: generated data
+
+        CONDITIONS:
+        :param condition_keys: list of all conditional inputs of generator
+            (appear in same order as in generator model forward() call)
+        :param d_fake_condition_keys: list of all conditional inputs of
+            discriminator (fake data conditions)
+            (appear in same order as in generator model forward() call)
+        :param d_real_condition_keys: list of all conditional inputs of
+            discriminator (real data conditions)
+            (appear in same order as in generator model forward() call)
+        Note: THIS RUNNER SUPPORTS ONLY EQUALLY CONDITIONED generator and
+            discriminator (i.e. if generator is conditioned on 3 variables,
+            discriminator must be conditioned on same 3 variables)
+
+        PHASES:
+        :param generator_train_phase(str): name for generator training phase
+        :param discriminator_train_phase(str): name for discriminator
+            training phase
+
+        MODEL KEYS:
+        :param generator_model_key: name for generator model, e.g. "generator"
+        :param discriminator_model_key: name for discriminator model,
+            e.g. "discriminator"
+        """
         input_batch_keys = input_batch_keys or [data_input_key]
         registered_phases = (
             (generator_train_phase, "_generator_train_phase"),
@@ -123,59 +169,108 @@ class GANRunner(MultiPhaseRunner):
         self.real_logits_output_key = real_logits_output_key
         self.fake_data_output_key = fake_data_output_key
         # condition keys
+        self.__init_condition_keys(condition_keys,
+                                   d_real_condition_keys,
+                                   d_fake_condition_keys)
+
+        # model keys
+        self.generator_key = generator_model_key
+        self.discriminator_key = discriminator_model_key
+
+    def __init_condition_keys(self, condition_keys,
+                              d_real_condition_keys,
+                              d_fake_condition_keys):
+        """
+        Condition keys initialization
+        :param condition_keys:
+        :param d_real_condition_keys:
+        :param d_fake_condition_keys:
+        :return:
+        """
+
         # make self.condition_keys to be a list
         condition_keys = condition_keys or []
         self.condition_keys = (
             [condition_keys] if isinstance(condition_keys, str)
             else condition_keys
         )
-        # TODO: simplify checks & transforms
-        assert not ((d_fake_condition_keys is None) ^ (d_real_condition_keys is None))
-        if d_fake_condition_keys is not None or d_real_condition_keys is not None:
-            assert len(self.condition_keys) > 0
+
+        assert (
+                (d_fake_condition_keys is None)
+                == (d_real_condition_keys is None)
+        ), "'d_fake_condition_keys' and 'd_real_condition_keys' " \
+           "should be either None or not None at same time"
+
+        if d_fake_condition_keys is not None:
+            # this generator supports only
+            # SAME CONDITIONED generator (G) and discriminator (D)
+            # below are some thoughts why:
+            #
+            # 1. G is more conditioned than D.
+            #
+            # it would be strange if G is conditioned on something
+            # and D is NOT conditioned on same variable
+            # which will most probably lead to interpreting that variable
+            # as additional noise
+            #
+            # 2. D is more conditioned than G.
+            #
+            # imagine D to have additional condition 'cond_var' which is not
+            # condition of G. now you have:
+            #   fake_data = G(z, *other_conditions)
+            #   fake_score = D(fake_data, cond_var, *other_conditions)
+            # in the above example fake_data and cond_var are ~independent?
+            # if they are not independent (e.g. cond_var represents
+            # class condition which is fixed to the single "cat" class,
+            # which may be used for finetuning pretrained GAN for specific
+            # class) such configuration may have some sense
+            # so they case #2 may have some sense, however for simplicity
+            # it is not implemented in this Runner
+
             assert all(
                 key in self.condition_keys for key in d_fake_condition_keys
             ), "all discriminator conditions must be generator conditions"
+            assert all(
+                key in d_fake_condition_keys for key in self.condition_keys
+            ), "all generator conditions must be discriminator conditions"
+            # additional check that discriminator will have
+            # same number of arguments for real/fake data
+            assert len(d_fake_condition_keys) == len(d_real_condition_keys), \
+                "Not the same number of real/fake conditions"
 
-        if (
-            not self.condition_keys
-            or d_real_condition_keys is None
-        ):
-            # simple case: no condition in discriminator
+        if not self.condition_keys:
+            # simple case: no condition
             d_real_condition_keys = []
             d_fake_condition_keys = []
-        else:
-            # additional check that discriminator will have
-            # same number of arguments
-            assert len(d_fake_condition_keys) == len(d_real_condition_keys)
+
         self.d_real_condition_keys = d_real_condition_keys
         self.d_fake_condition_keys = d_fake_condition_keys
 
-        # model keys
-        self.generator_key = generator_model_key
-        self.discriminator_key = discriminator_model_key
-
-    def _memorize_models(self, stage: str):
+    def _alias_inner_params(self, stage: str):
         self.generator = self.model[self.generator_key]
         self.discriminator = self.model[self.discriminator_key]
 
     # common utility functions
 
     def _get_noise_and_conditions(self):
+        """returns generator inputs"""
         z = self.state.input[self.noise_input_key]
         conditions = [self.state.input[key] for key in
                       self.condition_keys]  # TODO: maybe as dict?
         return z, conditions
 
     def _get_real_data_conditions(self):
+        """returns discriminator conditions (for real data)"""
         return [self.state.input[key] for key in self.d_real_condition_keys]
 
     def _get_fake_data_conditions(self):
+        """returns discriminator conditions (for fake data)"""
         return [self.state.input[key] for key in self.d_fake_condition_keys]
 
     # concrete phase methods
 
     def _generator_train_phase(self):
+        """forward() on generator training phase"""
         z, g_conditions = self._get_noise_and_conditions()
         d_fake_conditions = self._get_fake_data_conditions()
 
@@ -187,6 +282,7 @@ class GANRunner(MultiPhaseRunner):
         }
 
     def _discriminator_train_phase(self):
+        """forward() on discriminator training phase"""
         z, g_conditions = self._get_noise_and_conditions()
         d_fake_conditions = self._get_fake_data_conditions()
         d_real_conditions = self._get_real_data_conditions()
@@ -208,7 +304,7 @@ class WGANRunner(GANRunner):
         Note: all the changes made compared to GANRunner
             are just renaming some input/output keys to conventional ones
     Also this runner may be used unchanged for WGAN-GP
-        (just add gradient penalty loss in config)
+        (just add gradient penalty loss in yaml config)
     """
 
     def __init__(self, model: Union[Model, Dict[str, Model]] = None,
@@ -247,6 +343,10 @@ class WGANRunner(GANRunner):
 
 
 class CGanRunner(GANRunner):
+    """
+    (Class) Conditional GAN
+        both generator and discriminator are conditioned on one-hot class target
+    """
 
     def __init__(self, model: Union[Model, Dict[str, Model]] = None,
                  device: Device = None,
@@ -286,6 +386,14 @@ class CGanRunner(GANRunner):
 
 
 class ICGanRunner(CGanRunner):
+    """
+    (Image) Conditional GAN
+        both generator and discriminator are conditioned on same class image
+        assumed usage:
+            another_image_of_class_c = generator(noise, image_of_class_c)
+            real_score = discriminator(image1_class_c, image2_class_c)
+            fake_score = discriminator(gen_image_class_c, image2_class_c)
+    """
 
     def __init__(self, model: Union[Model, Dict[str, Model]] = None,
                  device: Device = None, data_input_key: str = "data",
@@ -377,7 +485,7 @@ class YAERunner(Runner):
         self.input_random_y_key = input_random_y_key
 
         self.input_key = (
-        self.input_img_key, self.input_y_key, self.input_random_y_key)
+            self.input_img_key, self.input_y_key, self.input_random_y_key)
 
         self.output_images_a_key = output_images_a_key
         self.output_images_b_key = output_images_b_key
@@ -427,7 +535,6 @@ class YAERunner(Runner):
             self.output_logits_b_key: expl_ab,
             self.output_implicit_loss_key: impl_loss,
         }
-
 
 # class D2GanRunner(BaseGANRunner):
 #     """TODO:
