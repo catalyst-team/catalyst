f0fc72ad9fcc487d0cfb6b5dad3b5bd5bd1eb18e
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-01-27 14:19:14 +0300

    update `catalyst.ToTensor` transform

--- catalyst/contrib/transforms/tensor.py
+++ catalyst/contrib/transforms/tensor.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from albumentations.core.transforms_interface import ImageOnlyTransform
+from albumentations import ImageOnlyTransform
+from albumentations.pytorch import ToTensorV2
 import torch
 
 
@@ -21,21 +22,41 @@ class TensorToImage(ImageOnlyTransform):
         return np.expand_dims(img.numpy(), axis=0)
 
 
-class ToTensor(ImageOnlyTransform):
+class ToTensor(ToTensorV2):
     """
-    Casts numpy array to torch.tensor, but do not move channels dim
+    Casts numpy array to ``torch.tensor``
     """
-    def __init__(self, always_apply=False, p=1.0):
+    def __init__(
+        self,
+        move_channels_dim: bool = True,
+        always_apply: bool = False,
+        p: float = 1.0,
+    ):
         """
         Args:
+            move_channels_dim (bool): if ``False``, casts numpy array
+                to ``torch.tensor``, but do not move channels dim
             always_apply (bool): need to apply this transform anyway
             p (float): probability for this transform
         """
         super().__init__(always_apply, p)
+        self.move_channels_dim = move_channels_dim
 
     def apply(self, img, **params):
         """Apply the transform to the image"""
-        return torch.from_numpy(img)
+        if self.move_channels_dim:
+            return super().apply(img, **params)
+        else:
+            return torch.from_numpy(img)
+
+    def apply_to_mask(self, mask: np.ndarray, **params) -> torch.Tensor:
+        """Apply the transform to the mask"""
+        if self.move_channels_dim:
+            mask = mask.transpose(2, 0, 1)
+        return super().apply_to_mask(mask.astype(np.float32), **params)
+
+    def get_transform_init_args_names(self):
+        return ("move_channels_dim",)
 
 
 __all__ = [
