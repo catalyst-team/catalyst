1ec6e20fa114198de7bb81ac45ef6ff0af7523cf
Artem Zolkin <arquestro93@gmail.com>
2020-02-04 18:33:19 +0300

    GanState, reduce default init arguments to kwargs.

--- catalyst/dl/state/gan.py
+++ catalyst/dl/state/gan.py
@@ -1,9 +1,4 @@
-from typing import Dict  # isort:skip
-
 from catalyst.dl.core import State
-from catalyst.utils.tools.typing import (
-    Criterion, Device, Model, Optimizer, Scheduler
-)
 
 
 class GanState(State):
@@ -15,38 +10,11 @@ class GanState(State):
     def __init__(
         self,
         *,
-        device: Device = None,
-        model: Model = None,
-        criterion: Criterion = None,
-        optimizer: Optimizer = None,
-        scheduler: Scheduler = None,
-        logdir: str = None,
-        stage: str = "infer",
-        num_epochs: int = 1,
-        main_metric: str = "loss",
-        minimize_metric: bool = True,
-        valid_loader: str = "valid",
-        verbose: bool = False,
-        checkpoint_data: Dict = None,
         batch_consistant_metrics: bool = False,
         **kwargs
     ):
         self.phase = None
-
         super().__init__(
-            device=device,
-            model=model,
-            criterion=criterion,
-            optimizer=optimizer,
-            scheduler=scheduler,
-            logdir=logdir,
-            stage=stage,
-            num_epochs=num_epochs,
-            main_metric=main_metric,
-            minimize_metric=minimize_metric,
-            valid_loader=valid_loader,
-            verbose=verbose,
-            checkpoint_data=checkpoint_data,
             batch_consistant_metrics=batch_consistant_metrics,
             **kwargs
         )
