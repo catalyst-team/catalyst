eeddc7476695994a3fbd7a2afa000cb71aacdbba
Sergey Kolesnikov <scitator@gmail.com>
2020-02-10 23:10:24 +0300

    sh update

--- bin/tests/check_dl.sh
+++ bin/tests/check_dl.sh
@@ -2,18 +2,17 @@
 pip install tifffile  #TODO: check if really required
 
 mkdir -p data
-
 # gdrive
 # gdrive_download 1N82zh0kzmnzqRvUyMgVOGsCoS1kHf3RP ./data/isbi.tar.gz
 # aws
-# wget https://catalyst-ai.s3-eu-west-1.amazonaws.com/isbi.tar.gz -O ./data/isbi.tar.gz
-# tar -xf ./data/isbi.tar.gz -C ./data/
+wget https://catalyst-ai.s3-eu-west-1.amazonaws.com/isbi.tar.gz -O ./data/isbi.tar.gz
+tar -xf ./data/isbi.tar.gz -C ./data/
 
 # @TODO: fix macos fail with sed
-# set -e
+set -e
 
 # imports check
-# (set -e; for f in examples/_tests_scripts/*.py; do PYTHONPATH=./catalyst:${PYTHONPATH} python "$f"; done)
+(set -e; for f in examples/_tests_scripts/*.py; do PYTHONPATH=./catalyst:${PYTHONPATH} python "$f"; done)
 #(set -e; for f in examples/_tests_scripts/dl_*.py; do PYTHONPATH=./catalyst:${PYTHONPATH} python "$f"; done)
 #(set -e; for f in examples/_tests_scripts/z_*.py; do PYTHONPATH=./catalyst:${PYTHONPATH} python "$f"; done)
 
