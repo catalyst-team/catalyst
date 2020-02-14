9302858c22f8320704b6b5ac0326c6031f50fed2
Sergey Kolesnikov <scitator@gmail.com>
2020-02-14 17:09:35 +0300

    simplified gan

--- bin/tests/check_dl_gan.sh
+++ bin/tests/check_dl_gan.sh
@@ -47,9 +47,9 @@ print('loss_d', loss_d)
 # assert 0.28 < loss_d_fake < 0.58
 # assert 0.3 < loss_d < 0.6
 assert loss_g < 2.0
-assert loss_d_real < 0.9
-assert loss_d_fake < 0.9
-assert loss_d < 0.9
+assert loss_d_real < 1.0
+assert loss_d_fake < 1.0
+assert loss_d < 1.0
 """
 
 rm -rf ${LOGDIR}
