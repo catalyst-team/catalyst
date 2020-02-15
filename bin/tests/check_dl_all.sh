16e838b280a233ef19927bd07ced0b3cd18d684d
Sergey Kolesnikov <scitator@gmail.com>
2020-02-15 12:37:46 +0300

    bash fix

--- bin/tests/check_dl_all.sh
+++ bin/tests/check_dl_all.sh
@@ -15,7 +15,7 @@ bash ./bin/tests/check_dl_cv.sh
 
 
 ####################################  GAN  ####################################
-if [[ "$CUDA_VISIBLE_DEVICES" == "" ] || [ "$CUDA_VISIBLE_DEVICES" == "0" ]]; then
+if [[ "$CUDA_VISIBLE_DEVICES" == "" || "$CUDA_VISIBLE_DEVICES" == "0" ]]; then
     echo './bin/tests/check_dl_gan.sh'
     bash ./bin/tests/check_dl_gan.sh
 fi
