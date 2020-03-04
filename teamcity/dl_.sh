f471ce40a649a4a523a0ad2a868d248228aed906
Evgeny Semyonov <lightsanweb@yandex.ru>
2020-03-04 21:19:59 +0300

    last changes

--- teamcity/dl_.sh
+++ teamcity/dl_.sh
@@ -28,9 +28,10 @@ rm -rf ./data
 # load the data
 mkdir -p data
 
-if [ -d /mount/isbi ] && [ -d /mount/segmentation_data ]; then
+if [ -d /mount/isbi ] && [ -d /mount/segmentation_data ] && [ -d /mount/MNIST]; then
   cp -a /mount/isbi data/isbi
   cp -a /mount/segmentation_data data/segmentation_data
+  cp -a /mount/MNIST data/MNIST
 else
   bash bin/scripts/download-gdrive 1N82zh0kzmnzqRvUyMgVOGsCoS1kHf3RP ./data/isbi.tar.gz
   tar -xf ./data/isbi.tar.gz -C ./data/
