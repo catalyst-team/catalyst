34add2c76b2bc9c42fcfa54c29bd9138118fe037
Evgeny Semyonov <lightsanweb@yandex.ru>
2020-03-05 09:01:23 +0300

    last changes

--- teamcity/dl_.sh
+++ teamcity/dl_.sh
@@ -28,16 +28,31 @@ rm -rf ./data
 # load the data
 mkdir -p data
 
-if [ -d /mount/isbi ] && [ -d /mount/segmentation_data ] && [ -d /mount/MNIST ]; then
+if [[ -d /mount/isbi ]]; then
   cp -a /mount/isbi data/isbi
-  cp -a /mount/segmentation_data data/segmentation_data
-  cp -a /mount/MNIST data/MNIST
 else
   bash bin/scripts/download-gdrive 1N82zh0kzmnzqRvUyMgVOGsCoS1kHf3RP ./data/isbi.tar.gz
   tar -xf ./data/isbi.tar.gz -C ./data/
+fi
 
+if [[ -d /mount/segmentation_data ]]; then
+  cp -a /mount/segmentation_data data/segmentation_data
+else
   bash bin/scripts/download-gdrive 1iYaNijLmzsrMlAdMoUEhhJuo-5bkeAuj ./data/segmentation_data.zip
   unzip -qqo ./data/segmentation_data.zip -d ./data 2> /dev/null || true
 fi
 
+if [[ -d /mount/MNIST ]]; then
+  cp -a /mount/MNIST data/MNIST
+else
+  bash bin/scripts/download-gdrive 1D_sz7bQSSBDQKNUMSEniXHc9_jHP4EXO ./data/MNIST.zip
+  unzip -qqo ./data/MNIST.zip -d ./data 2> /dev/null || true
+fi
 
+mkdir -p ~/.cache/torch/checkpoints
+
+if [[ -f /mount/resnext50_32x4d-7cdf4587.pth ]]; then
+  cp -a /mount/resnext50_32x4d-7cdf4587.pth ~/.cache/torch/checkpoints/resnext50_32x4d-7cdf4587.pth
+else
+  wget https://download.pytorch.org/models/resnext50_32x4d-7cdf4587.pth -O ~/.cache/torch/checkpoints/resnext50_32x4d-7cdf4587.pth
+fi
