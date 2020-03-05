9c4617e14181e8b5701269e3b1afa367b24f4880
Evgeny Semyonov <lightsanweb@yandex.ru>
2020-03-05 11:37:20 +0300

    last changes

--- teamcity/py_codestyle.sh
+++ teamcity/py_codestyle.sh
@@ -3,12 +3,6 @@
 # Cause the script to exit if a single command fails
 set -eo pipefail -v
 
-if [[ $BUILD_NUMBER == '670' ]]; then
-  exit 0
-else
-  exit 1
-fi
-
 pip install -r requirements/requirements.txt
 
 pip install -r requirements/requirements-cv.txt
