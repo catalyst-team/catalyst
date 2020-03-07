fed75e7a0ffaec83bfbe3a40a5c4a9a073fc7020
Teamcity <teamcity@catalyst.github>
2020-03-07 05:31:18 +0000

    Runner-State-Loggers refactoring, v20.03 (#677)
    * init concept
    
    * renaming and runner simplification
    
    * logger simplification; state.metrics simplification
    
    * PoC for metics starts
    
    * metric aggregation merge
    
    * in the middle of nowhere
    
    * first working example
    
    * scheduler works
    
    * restructuration
    
    * second one
    
    * check for supervised returned
    
    * 1cycle works
    
    * checkpoint callback patch
    
    * isort
    
    * codestyle
    
    * sorting and tests
    
    * arch changes
    
    * codestyle
    
    * codestyle2
    
    * simplifiaction
    
    * codestyle
    
    * backward compatability
    
    * codestyle
    
    * arch
    
    * arch
    
    * arch
    
    * works?
    
    * metrics aggregated
    
    * fix
    
    * fix
    
    * epoch count fix
    
    * codeslty
    
    * contrib2
    
    * hotfixes
    
    * order fix
    
    * naming
    
    * order
    
    * docs
    
    * merged
    
    * PoC for experiment-wise callbacks
    
    * codestyle
    
    * contrib refactoring
    
    * logging updaete
    
    * naming
    
    * examples update
    
    * fix
    
    * fix
    
    * docs fix
    
    * codestyle

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '20.02.4',
+    VERSION: '20.03',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
