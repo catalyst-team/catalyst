800848030b6c182968e3ae3a5223ae7c72bddd7e
Nikolay Korolev <korolevns98@gmail.com>
2020-03-23 15:50:53 +0300

    Move useless warning

--- catalyst/contrib/dl/callbacks/__init__.py
+++ catalyst/contrib/dl/callbacks/__init__.py
@@ -13,11 +13,11 @@ try:
     import alchemy
     from .alchemy import AlchemyLogger
 except ImportError as ex:
-    logger.warning(
-        "alchemy not available, to install alchemy, "
-        "run `pip install alchemy-catalyst`."
-    )
     if os.environ.get("USE_ALCHEMY", "0") == "1":
+        logger.warning(
+            "alchemy not available, to install alchemy, "
+            "run `pip install alchemy-catalyst`."
+        )
         raise ex
 
 try:
