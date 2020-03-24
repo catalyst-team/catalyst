1b902c9fe97cbe6a100fc104401e115de751bd0b
Sergey Kolesnikov <scitator@gmail.com>
2020-03-24 10:18:26 +0300

    Hydra

--- catalyst/contrib/models/__init__.py
+++ catalyst/contrib/models/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 
 from .functional import get_convolution_net, get_linear_net
+from .hydra import Hydra
 from .sequential import (
     _process_additional_params, ResidualWrapper, SequentialNet
 )
