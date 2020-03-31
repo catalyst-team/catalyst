819a4c62ae2f811baa40ab1f3099cc0cbd0add60
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-03-31 13:07:54 +0300

    add tests

---
+++ catalyst/utils/tests/test_torch.py
@@ -0,0 +1,45 @@
+import torch
+from torch import nn
+
+from .. import torch as utils  # import
+
+
+def test_network_output():
+    """
+    Test for ``catalyst.utils.torch.get_network_output``
+    """
+    # case #0
+    net = nn.Identity()
+    assert utils.get_network_output(net, (1, 20)).shape == (1, 1, 20)
+
+    # case #1
+    net = nn.Linear(20, 10)
+    assert utils.get_network_output(net, (1, 20)).shape == (1, 1, 10)
+
+    # case #2
+    class Net(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.net1 = nn.Linear(20, 10)
+            self.net2 = nn.Linear(10, 5)
+
+        def forward(self, x, y):
+            z = torch.cat((self.net1(x), self.net2(y)), dim=-1)
+            return z
+
+    net = Net()
+    assert utils.get_network_output(net, (1, 20), (1, 10)).shape == (1, 1, 15)
+
+    # case #3
+    class Net(nn.Module):
+        def __init__(self):
+            super().__init__()
+            self.net = nn.Linear(20, 10)
+
+        def forward(self, input):
+            z = self.net(input["x"])
+            return z
+
+    net = Net()
+    input_shapes = {"x": (1, 20)}
+    assert utils.get_network_output(net, input_shapes).shape == (1, 1, 10)
