ed381543c7fef98a62cff31b6299b3383f8ad98f
vsokhatskyi <vova.sokhatskyi.gmail.com>
2020-03-31 11:00:30 +0300

    add tests for dice metric

---
+++ catalyst/utils/metrics/tests/test_dice.py
@@ -0,0 +1,26 @@
+import torch
+
+from catalyst.utils import metrics
+
+
+def test_dice():
+    size = 4
+    half_size = size // 2
+    shape = (1, 1, size, size)
+
+    # check 0: one empty
+    empty = torch.zeros(shape)
+    full = torch.ones(shape)
+    assert metrics.dice(empty, full, activation="none").item() == 0
+
+    # check 0: no overlap
+    left = torch.ones(shape)
+    left[:, :, :, half_size:] = 0
+    right = torch.ones(shape)
+    right[:, :, :, :half_size] = 0
+    assert metrics.dice(left, right, activation="none").item() == 0
+
+    # check 1: both empty, both full, complete overlap
+    assert metrics.dice(empty, empty, activation="none") == 1
+    assert metrics.dice(full, full, activation="none") == 1
+    assert metrics.dice(left, left, activation="none") == 1
