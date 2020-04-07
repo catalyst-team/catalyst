238c2124d34900cf5bd729d21bd096892c0b2429
Nguyen Xuan Bac <ngxbac.dt@gmail.com>
2020-04-07 14:17:07 +0700

    Add experiment to registry

--- examples/cifar_simple/__init__.py
+++ examples/cifar_simple/__init__.py
@@ -1,5 +1,10 @@
 # flake8: noqa
-from catalyst.dl import SupervisedRunner as Runner
+
+import experiments as exp
+
+from catalyst.dl import registry, SupervisedRunner as Runner
 
 from .experiment import Experiment
 from .model import SimpleNet
+
+registry.EXPERIMENTS.add_from_module(exp)
