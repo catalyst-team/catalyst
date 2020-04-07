9fa64eecebe2995169291aa963aa96a1544b28a9
Pavel Danilov <pdanilov94@gmail.com>
2020-04-07 20:24:27 +0600

    Updated TiledInferenceExperiment with callbacks

--- catalyst/contrib/dl/experiment/inference.py
+++ catalyst/contrib/dl/experiment/inference.py
@@ -1,8 +1,12 @@
 from collections import OrderedDict
 
+import skimage.io
+
 from torch.utils.data import Dataset
 
 from catalyst.contrib.data.cv.datasets import TiledImageDataset
+from catalyst.contrib.dl.callbacks.inference import TiledInferenceCallback
+from catalyst.core import Callback
 from catalyst.dl.experiment import ConfigExperiment
 
 
@@ -35,3 +39,32 @@ class TiledInferenceExperiment(ConfigExperiment):
         )
 
         return dataset
+
+    def get_callbacks(self, stage: str) -> OrderedDict[Callback]:
+        """
+        Updates callbacks with additional one for tiled inference.
+        """
+        callbacks = super().get_callbacks(stage)
+        data_params = dict(self.stages_config[stage]["data_params"])
+
+        save_path = data_params["save_path"]
+        image_path = data_params["image_path"]
+        n_channels = data_params["n_channels"]
+        tile_size = data_params["tile_size"]
+        tile_step = data_params["tile_step"]
+        output_key = data_params["output_key"]
+
+        image = skimage.io.imread(image_path)
+        *image_size, _ = image.shape
+
+        tiled_inference_callback = TiledInferenceCallback(
+            save_path,
+            image_size,
+            n_channels,
+            tile_size,
+            tile_step,
+            output_key=output_key,
+        )
+        callbacks.update(tiled_inference=tiled_inference_callback)
+
+        return callbacks
