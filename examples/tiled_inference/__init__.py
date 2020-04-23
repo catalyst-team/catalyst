bb35ce22ef9a3541981d86928518dbf124325406
Pavel Danilov <pdanilov94@gmail.com>
2020-04-23 22:15:11 +0600

    Moved TiledInferenceExperiment

--- examples/tiled_inference/__init__.py
+++ examples/tiled_inference/__init__.py
@@ -1,8 +1,6 @@
 import segmentation_models_pytorch as smp
 
-from catalyst.contrib.dl.experiment import (
-    TiledInferenceExperiment as Experiment,
-)
+from .experiment import TiledInferenceExperiment as Experiment
 from catalyst.dl import registry
 from catalyst.dl.runner import SupervisedRunner as Runner
 
