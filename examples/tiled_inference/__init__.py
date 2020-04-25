ade7ca96f00a9635fe2eaeaa51265d4599f3b6f3
Pavel Danilov <pdanilov94@gmail.com>
2020-04-25 19:00:13 +0600

    Update experiment config & readme

--- examples/tiled_inference/__init__.py
+++ examples/tiled_inference/__init__.py
@@ -1,7 +1,8 @@
 import segmentation_models_pytorch as smp
 
-from .experiment import TiledInferenceExperiment as Experiment
 from catalyst.dl import registry
 from catalyst.dl.runner import SupervisedRunner as Runner
+
+from .experiment import TiledInferenceExperiment as Experiment
 
 registry.Model(smp.FPN)
