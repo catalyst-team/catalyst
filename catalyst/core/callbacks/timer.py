60db7424f199b58222c501987d5bc48e7e2bdf43
Sergey Kolesnikov <scitator@gmail.com>
2020-04-27 17:52:48 +0300

    Update timer.py

--- catalyst/core/callbacks/timer.py
+++ catalyst/core/callbacks/timer.py
@@ -49,8 +49,8 @@ class TimerCallback(Callback):
         self.timer.stop("_timer/batch_time")
 
         # @TODO: just a trick
-        self.timer.elapsed["_timer/_fps"] = (
-            state.batch_size / (self.timer.elapsed["_timer/batch_time"] + EPS)
+        self.timer.elapsed["_timer/_fps"] = state.batch_size / (
+            self.timer.elapsed["_timer/batch_time"] + EPS
         )
         for key, value in self.timer.elapsed.items():
             state.batch_metrics[key] = value
