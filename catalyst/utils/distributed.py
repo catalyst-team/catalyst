2041d117ca96c3bc10081c6961f0a36cc9810415
Sergey Kolesnikov <scitator@gmail.com>
2020-05-19 21:34:58 +0300

    device fix

--- catalyst/utils/distributed.py
+++ catalyst/utils/distributed.py
@@ -231,6 +231,7 @@ def process_components(
     distributed_params = distributed_params or {}
     distributed_params = copy.deepcopy(distributed_params)
     distributed_params.update(get_distributed_params())
+
     if device is None:
         device = get_device()
 
@@ -286,7 +287,11 @@ def process_components(
                 model, optimizer, **distributed_params
             )
 
-        if torch.cuda.device_count() > 1:
+        if (
+            torch.cuda.device_count() > 1
+            and device.type != "cpu"
+            and device.index is None
+        ):
             if isinstance(model, nn.Module):
                 model = nn.DataParallel(model)
             elif isinstance(model, dict):
