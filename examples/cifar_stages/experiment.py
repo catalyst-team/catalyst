9f8c85d40baf1e11f03263d351de8b5e5f2263f9
Sergey Kolesnikov <scitator@gmail.com>
2020-06-11 20:04:38 +0300

    update

--- examples/cifar_stages/experiment.py
+++ examples/cifar_stages/experiment.py
@@ -32,7 +32,8 @@ class Experiment(ConfigExperiment):
 
     def get_model(self, stage: str):
         """
-        Model specification for currect stage
+        Model specification for current stage
+
         Args:
             stage: current stage name
 
@@ -43,8 +44,10 @@ class Experiment(ConfigExperiment):
         if isinstance(model, torch.nn.DataParallel):
             model = model.module
 
+        conv_layres = ["conv1", "pool", "conv2"]
+
         if stage == "stage2":
-            for key in ["conv1", "pool", "conv2"]:
+            for key in conv_layres:
                 layer = getattr(model, key)
                 utils.set_requires_grad(layer, requires_grad=False)
         return model
