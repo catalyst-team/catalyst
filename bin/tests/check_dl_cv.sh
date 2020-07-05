2bacb5bc3c53cbdada339cbf421a662f7e80cb0e
Sergey Kolesnikov <scitator@gmail.com>
2020-07-05 16:08:42 +0300

    Update check_dl_cv.sh

--- bin/tests/check_dl_cv.sh
+++ bin/tests/check_dl_cv.sh
@@ -409,7 +409,8 @@ python -c """
 from catalyst import utils
 metrics = utils.load_config('$LOGFILE')
 # assert metrics['stage1.2']['loss'] < metrics['stage1.1']['loss']
-assert metrics['stage1.2']['loss'] < 2.33
+assert metrics['stage1.1']['loss'] < 3.3
+assert metrics['stage1.2']['loss'] < 3.3
 """
 
 rm -rf ${LOGDIR}
