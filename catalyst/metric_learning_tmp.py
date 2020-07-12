8ae4e3fe1850eb9a72c689a8638c7124c3769db0
AlekseySh <lexibender@ya.ru>
2020-07-13 02:47:07 +0500

    minor changes in example

--- catalyst/metric_learning_tmp.py
+++ catalyst/metric_learning_tmp.py
@@ -1,14 +1,35 @@
-from torch import nn
-from torch.optim import Adam
-from torch.utils.data import DataLoader
+import catalyst.data.sampler_inbatch as si
 import torchvision.transforms as t
-
 from catalyst.contrib.datasets.metric_learning import MnistML
 from catalyst.contrib.nn.criterion.triplet import TripletMarginLossWithSampling
 from catalyst.contrib.nn.modules.common import Normalize
 from catalyst.data.sampler import BalanceBatchSampler
-import catalyst.data.sampler_inbatch as si
 from catalyst.dl.runner import SupervisedRunner
+from torch import nn
+from torch.nn.functional import relu, avg_pool2d, max_pool2d, log_softmax
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torch
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, 3, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1)
+        self.fc1 = nn.Linear(9216, 128)
+        self.fc2 = nn.Linear(128, 16)
+        self.norm = Normalize()
+
+    def forward(self, x):
+        x = relu(self.conv1(x))
+        x = relu(self.conv2(x))
+        x = max_pool2d(x, 2)
+        x = torch.flatten(x, 1)
+        x = relu(self.fc1(x))
+        x = self.fc2(x)
+        x = self.norm(x)
+        return x
 
 
 def metric_learning_minimal_example() -> None:
@@ -24,24 +45,22 @@ def metric_learning_minimal_example() -> None:
         root="/Users/alexeyshab/Downloads/",
         train=True,
         download=True,
-        transform=t.ToTensor(),
+        transform=t.Compose([
+            t.ToTensor(),
+            t.Normalize((0.1307,), (0.3081,))
+        ]),
     )
-    sampler = BalanceBatchSampler(labels=dataset.get_labels(), p=10, k=100)
+    sampler = BalanceBatchSampler(labels=dataset.get_labels(), p=10, k=10)
     train_loader = DataLoader(
         dataset=dataset, sampler=sampler, batch_size=sampler.batch_size
     )
 
     # model
-    model = nn.Sequential(*[nn.Flatten(), nn.Linear(28 * 28, 32), Normalize()])
-    # from torchvision.models import resnet18
-    # import catalyst.contrib.models.cv.encoders.resnet as r
-    # model = r.adopt_resnet_for_1channel(resnet18())
+    model = Net()
     optimizer = Adam(model.parameters(), lr=1e-2)
 
     # criterion
-    # you can also use si.AllTripletsSampler
-    # sampler_inbatch = si.HardTripletsSampler(need_norm=True)
-    sampler_inbatch = si.AllTripletsSampler(max_output_triplets=1000)
+    sampler_inbatch = si.HardTripletsSampler(False)
     criterion = TripletMarginLossWithSampling(
         margin=0.5, sampler_inbatch=sampler_inbatch
     )
@@ -53,7 +72,7 @@ def metric_learning_minimal_example() -> None:
         criterion=criterion,
         optimizer=optimizer,
         loaders={"train": train_loader},
-        num_epochs=1000,
+        num_epochs=100000,
         verbose=True,
     )
 
