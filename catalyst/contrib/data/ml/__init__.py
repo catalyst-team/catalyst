30dafa5132d2a712d470bedfc0663ecfbd7e0e3b
Nikita <Balaganskij.NN@phystech.edu>
2020-07-13 17:39:34 +0300

    add test pipeline

--- catalyst/contrib/data/ml/__init__.py
+++ catalyst/contrib/data/ml/__init__.py
@@ -1,5 +1,9 @@
-from .query_gallery_dataset import QueryGalleryDataset
+from .query_gallery_dataset import (
+    QueryGalleryDataset,
+    QueryGalleryFolderDataset,
+)
 
 __all__ = [
     "QueryGalleryDataset",
+    "QueryGalleryFolderDataset",
 ]
