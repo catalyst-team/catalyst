e458a8d739a0039c0bbf6820c02378efec730e8c
AlekseySh <lexibender@ya.ru>
2020-07-22 22:04:57 +0500

    minor

--- catalyst/contrib/datasets/metric_learning.py
+++ catalyst/contrib/datasets/metric_learning.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict
-from typing import List, Optional, Callable
+from typing import Dict, Any, List, Optional, Callable
 
 import torch
 from torch.utils.data import Dataset
