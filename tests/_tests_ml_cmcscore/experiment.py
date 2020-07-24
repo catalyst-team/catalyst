f2f8dce5ad330fa98d84ebaaf18813e54df6ca68
AlekseySh <lexibender@ya.ru>
2020-07-25 01:39:56 +0500

    style

--- tests/_tests_ml_cmcscore/experiment.py
+++ tests/_tests_ml_cmcscore/experiment.py
@@ -2,8 +2,8 @@
 from collections import OrderedDict
 
 from catalyst.contrib.data.transforms import Compose, Normalize, ToTensor
-from catalyst.contrib.datasets.metric_learning import MnistQGDataset
 from catalyst.contrib.datasets import MNIST
+from catalyst.contrib.datasets.metric_learning import MnistQGDataset
 from catalyst.dl import ConfigExperiment
 
 
