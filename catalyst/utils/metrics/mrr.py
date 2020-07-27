668196f521e47ebfd6632d4fd82f76ab76773841
Daniel Chepenko <dchepenk@yahoo-corp.co>
2020-07-27 20:54:25 +0900

    make codestyle

--- catalyst/utils/metrics/mrr.py
+++ catalyst/utils/metrics/mrr.py
@@ -5,9 +5,7 @@ MRR metric.
 import torch
 
 
-def mrr(outputs: torch.Tensor, 
-        targets: torch.Tensor
-    ) -> torch.Tensor:
+def mrr(outputs: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
 
     """
     Calculate the MRR score given model ouptputs and targets
@@ -22,7 +20,9 @@ def mrr(outputs: torch.Tensor,
     max_rank = targets.shape[0]
 
     _, indices_for_sort = outputs.sort(descending=True, dim=-1)
-    true_sorted_by_preds = torch.gather(targets, dim=-1, index=indices_for_sort)
+    true_sorted_by_preds = torch.gather(
+        targets, dim=-1, index=indices_for_sort
+    )
     values, indices = torch.max(true_sorted_by_preds, dim=0)
     indices = indices.type_as(values).unsqueeze(dim=0).t()
     max_rank_rep = torch.tensor(
--- catalyst/utils/metrics/tests/test_mrr.py
+++ catalyst/utils/metrics/tests/test_mrr.py
@@ -29,11 +29,13 @@ def test_mrr():
     mrr = metrics.mrr(torch.Tensor(y_pred), torch.Tensor(y_true))
     assert mrr == 1.0
 
-    #test batched slates
+    # test batched slates
     y_pred_1 = [0.2, 0.5]
     y_pred_05 = [0.5, 0.2]
     y_true = [0.0, 1.0]
 
-    mrr = metrics.mrr(torch.Tensor([y_pred_1, y_pred_05]), torch.Tensor([y_true, y_true]))
+    mrr = metrics.mrr(
+        torch.Tensor([y_pred_1, y_pred_05]), torch.Tensor([y_true, y_true])
+    )
     assert mrr[0][0] == 1.0
     assert mrr[1][0] == 0.5
