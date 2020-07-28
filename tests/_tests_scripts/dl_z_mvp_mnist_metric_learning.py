475785856e83b62693102e9f7b09934fbfc92f7b
AlekseySh <lexibender@ya.ru>
2020-07-29 01:12:14 +0500

    refactoring imports

--- tests/_tests_scripts/dl_z_mvp_mnist_metric_learning.py
+++ tests/_tests_scripts/dl_z_mvp_mnist_metric_learning.py
@@ -3,24 +3,14 @@ import os
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
-import catalyst.contrib.data.transforms as t
-from catalyst.contrib.datasets.mnist import MnistMLDataset, MnistQGDataset
-from catalyst.contrib.dl.callbacks import PeriodicLoaderCallback
-from catalyst.contrib.models.simple_conv import SimpleConv
-from catalyst.contrib.nn.criterion.triplet import TripletMarginLossWithSampling
+from catalyst import contrib as ctb, data, dl
 from catalyst.core.callbacks import ControlFlowCallback
-from catalyst.data.sampler import BalanceBatchSampler
-from catalyst.data.sampler_inbatch import (
-    AllTripletsSampler,
-    HardTripletsSampler,
-    InBatchTripletsSampler,
-)
-from catalyst.dl import CriterionCallback
 from catalyst.dl.callbacks.metrics.cmc import CMCScoreCallback
-from catalyst.dl.runner import SupervisedRunner
 
 
-def run_ml_pipeline(sampler_inbatch: InBatchTripletsSampler) -> float:
+def run_ml_pipeline(
+    sampler_inbatch: data.sampler_inbatch.InBatchTripletsSampler,
+) -> float:
     """
     Full metric learning pipeline, including train and val.
 
@@ -35,40 +25,42 @@ def run_ml_pipeline(sampler_inbatch: InBatchTripletsSampler) -> float:
     """
     # 1. train and valid datasets
     dataset_root = "./data"
-    transforms = t.Compose([t.ToTensor(), t.Normalize((0.1307,), (0.3081,))])
+    transforms = ctb.data.transforms.Compose(
+        [t.ToTensor(), t.Normalize((0.1307,), (0.3081,))]
+    )
 
-    dataset_train = MnistMLDataset(
+    dataset_train = ctb.datasets.mnist.MnistMLDataset(
         root=dataset_root, train=True, download=True, transform=transforms,
     )
-    sampler = BalanceBatchSampler(
+    sampler = data.sampler.BalanceBatchSampler(
         labels=dataset_train.get_labels(), p=10, k=10
     )
     train_loader = DataLoader(
         dataset=dataset_train, sampler=sampler, batch_size=sampler.batch_size
     )
 
-    dataset_val = MnistQGDataset(
+    dataset_val = ctb.datasets.mnist.MnistQGDataset(
         root=dataset_root, transform=transforms, gallery_fraq=0.2
     )
     val_loader = DataLoader(dataset=dataset_val, batch_size=1024)
 
     # 2. model and optimizer
-    model = SimpleConv(features_dim=16)
+    model = ctb.models.simple_conv.SimpleConv(features_dim=16)
     optimizer = Adam(model.parameters(), lr=0.0005)
 
     # 3. criterion with triplets sampling
-    criterion = TripletMarginLossWithSampling(
+    criterion = ctb.nn.criterion.triplet.TripletMarginLossWithSampling(
         margin=0.5, sampler_inbatch=sampler_inbatch
     )
 
     # 4. training with catalyst Runner
     callbacks = [
-        ControlFlowCallback(CriterionCallback(), loaders="train"),
+        ControlFlowCallback(dl.CriterionCallback(), loaders="train"),
         ControlFlowCallback(CMCScoreCallback(topk_args=[1]), loaders="valid"),
-        PeriodicLoaderCallback(valid=600),
+        dl.callbacks.PeriodicLoaderCallback(valid=600),
     ]
 
-    runner = SupervisedRunner(device="cuda:0")
+    runner = dl.runner.SupervisedRunner(device="cuda:0")
     runner.train(
         model=model,
         criterion=criterion,
@@ -91,8 +83,8 @@ def main() -> None:
     """
     cmc_score_th = 0.97
 
-    all_sampler = AllTripletsSampler(max_output_triplets=512)
-    hard_sampler = HardTripletsSampler(norm_required=False)
+    all_sampler = data.sampler_inbatch.AllTripletsSampler(512)
+    hard_sampler = data.sampler_inbatch.HardTripletsSampler(False)
 
     assert run_ml_pipeline(all_sampler) > cmc_score_th
     assert run_ml_pipeline(hard_sampler) > cmc_score_th
