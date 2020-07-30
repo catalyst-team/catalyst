d9d776402b13b37f7e5f50401f99ef543f63e92c
Daniel Chepenko <dchepenk@yahoo-corp.co>
2020-07-30 14:47:18 +0900

    minor improvements

--- catalyst/utils/metrics/tests/test_mrr.py
+++ catalyst/utils/metrics/tests/test_mrr.py
@@ -7,7 +7,6 @@ def test_mrr():
     """
     Tests for catalyst.utils.metrics.mrr metric.
     """
-
     # check 0 simple case
     y_pred = [0.5, 0.2]
     y_true = [1.0, 0.0]
@@ -30,12 +29,12 @@ def test_mrr():
     assert mrr == 1.0
 
     # test batched slates
-    y_pred_1 = [0.2, 0.5]
-    y_pred_05 = [0.5, 0.2]
+    y_pred1 = [0.2, 0.5]
+    y_pred05 = [0.5, 0.2]
     y_true = [0.0, 1.0]
 
     mrr = metrics.mrr(
-        torch.Tensor([y_pred_1, y_pred_05]), torch.Tensor([y_true, y_true])
+        torch.Tensor([y_pred1, y_pred05]), torch.Tensor([y_true, y_true])
     )
     assert mrr[0][0] == 1.0
     assert mrr[1][0] == 0.5
