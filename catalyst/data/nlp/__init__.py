75ae9166f896f777c75189fd3ec8e84440b9d3c6
Sergey Kolesnikov <scitator@gmail.com>
2020-08-09 12:48:52 +0300

    imports fixes

--- catalyst/data/nlp/__init__.py
+++ catalyst/data/nlp/__init__.py
@@ -1,5 +1,21 @@
 # flake8: noqa
-from catalyst.data.nlp.dataset import (
-    LanguageModelingDataset,
-    TextClassificationDataset,
-)
+import logging
+
+from catalyst.tools import settings
+
+logger = logging.getLogger(__name__)
+
+try:
+    from catalyst.data.nlp.dataset.language_modeling import (
+        LanguageModelingDataset,
+    )
+    from catalyst.data.nlp.dataset.text_classification import (
+        TextClassificationDataset,
+    )
+except ImportError as ex:
+    if settings.nlp_required:
+        logger.warning(
+            "some of catalyst-nlp dependencies not available,"
+            " to install dependencies, run `pip install catalyst[nlp]`."
+        )
+        raise ex
