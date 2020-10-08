3bdb7726735244606150872af4c28b8fd568da50
Daniel Chepenko <dchepenk@yahoo-corp.co>
2020-10-08 17:49:02 +0900

    typo in tests

--- catalyst/utils/metrics/tests/test_hitrate.py
+++ catalyst/utils/metrics/tests/test_hitrate.py
@@ -11,13 +11,12 @@ def test_hitrate():
     y_pred = [0.5, 0.2]
     y_true = [1.0, 0.0]
 
-    mrr = metrics.hitrate(torch.Tensor([y_pred]), torch.Tensor([y_true]))
-    assert mrr == 0.5
+    hitrate = metrics.hitrate(torch.Tensor([y_pred]), torch.Tensor([y_true]))
+    assert hitrate == 0.5
 
     # check 1 simple case
     y_pred = [0.5, 0.2]
     y_true = [0.0, 0.0]
 
-    mrr = metrics.hitrate(torch.Tensor([y_pred]), torch.Tensor([y_true]))
-    # mrr = metrics.mrr(torch.Tensor(y_pred), torch.Tensor(y_true))
-    assert mrr == 0.0
+    hitrate = metrics.hitrate(torch.Tensor([y_pred]), torch.Tensor([y_true]))
+    assert hitrate == 0.0
