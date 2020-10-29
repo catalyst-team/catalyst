944eab17d8b751e765c507d7824e5062184bbe2e
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2020-10-29 11:59:31 +0200

    Fix code style

--- catalyst/utils/components.py
+++ catalyst/utils/components.py
@@ -109,7 +109,9 @@ def process_components(
             if syncbn:
                 model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
 
-            find_unused = distributed_params.get("find_unused_parameters", False)
+            find_unused = distributed_params.get(
+                "find_unused_parameters", False
+            )
             model = nn.parallel.DistributedDataParallel(
                 model,
                 device_ids=[local_rank],
