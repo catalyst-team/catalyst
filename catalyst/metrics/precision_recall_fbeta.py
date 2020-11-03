3494ce5c9c890b09b82ef6e42e8bac6e06511da7
Nikita Balagansky <balaganskij.nn@phystech.edu>
2020-11-03 18:44:06 +0300

    docs

--- catalyst/metrics/precision_recall_fbeta.py
+++ catalyst/metrics/precision_recall_fbeta.py
@@ -12,6 +12,20 @@ def precision_recall_fbeta(
     argmax_dim: int = -1,
     num_classes: Optional[int] = None
 ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    """
+    Counts precision, recall, fbeta_score.
+
+    Args:
+        outputs: A list of predicted elements
+        targets:  A list of elements that are to be predicted
+        beta: beta param for f_score
+        eps: epsilon to avoid zero division
+        argmax_dim: int, that specifies dimension for argmax transformation
+            in case of scores/probabilities in ``outputs``
+        num_classes: int, that specifies number of classes if it known
+    Returns:
+        tuple of precision, recall, fbeta_score
+    """
     tn, fp, fn, tp, support = get_multiclass_statistics(
         outputs=outputs,
         targets=targets,
