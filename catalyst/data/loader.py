3cda19cc08895166e2edb732a6a66ecc7d189721
Sergey Kolesnikov <scitator@gmail.com>
2020-11-08 11:00:21 +0300

    docs draft

--- catalyst/data/loader.py
+++ catalyst/data/loader.py
@@ -207,6 +207,86 @@ def _prefetch_loader(loader: DataLoader, num_prefetches: int) -> Iterable:
 
 
 class BatchPrefetchLoaderWrapper(ILoaderWrapper):
+    """
+
+    Base usage:
+
+    .. code-block:: python
+
+        import torch
+        from torch.utils.data import DataLoader, TensorDataset
+        from catalyst.data import BatchPrefetchLoaderWrapper
+
+        num_samples, num_features = int(1e4), int(1e1)
+        X, y = torch.rand(num_samples, num_features), torch.rand(num_samples)
+        dataset = TensorDataset(X, y)
+        loader = DataLoader(dataset, batch_size=32, num_workers=1)
+        loader = BatchPrefetchLoaderWrapper(loader)
+
+    Minimal working example:
+
+    .. code-block:: python
+
+        import os
+        import torch
+        from torch.nn import functional as F
+        from torch.utils.data import DataLoader
+        from catalyst import dl, metrics
+        from catalyst.data.cv import ToTensor
+        from catalyst.contrib.datasets import MNIST
+        from catalyst.data import BatchPrefetchLoaderWrapper
+
+        class CustomRunner(dl.Runner):
+
+            def predict_batch(self, batch):
+                # model inference step
+                return self.model(batch[0].to(self.device).view(batch[0].size(0), -1))
+
+            def _handle_batch(self, batch):
+                # model train/valid step
+                x, y = batch
+                y_hat = self.model(x.view(x.size(0), -1))
+
+                loss = F.cross_entropy(y_hat, y)
+                accuracy01, accuracy03 = metrics.accuracy(y_hat, y, topk=(1, 3))
+                self.batch_metrics.update(
+                    {"loss": loss, "accuracy01": accuracy01, "accuracy03": accuracy03}
+                )
+
+                if self.is_train_loader:
+                    loss.backward()
+                    self.optimizer.step()
+                    self.optimizer.zero_grad()
+
+        model = torch.nn.Linear(28 * 28, 10)
+        optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
+
+        batch_size=32
+        loaders = {
+            "train": DataLoader(MNIST(os.getcwd(), train=True, download=True, transform=ToTensor()), batch_size=batch_size),
+            "valid": DataLoader(MNIST(os.getcwd(), train=False, download=True, transform=ToTensor()), batch_size=batch_size),
+        }
+        loaders = {k: BatchPrefetchLoaderWrapper(v) for k, v in loaders.items()}
+
+        runner = CustomRunner()
+        # model training
+        runner.train(
+            model=model,
+            optimizer=optimizer,
+            loaders=loaders,
+            logdir="./logs",
+            num_epochs=5,
+            verbose=True,
+            load_best_on_end=True,
+        )
+        # model inference
+        for prediction in runner.predict_loader(loader=loaders["valid"]):
+            assert prediction.detach().cpu().numpy().shape[-1] == 10
+        # model tracing
+        traced_model = runner.trace(loader=loaders["valid"])
+
+    """
+
     def __init__(self, loader: DataLoader, num_prefetches: int = None):
         super().__init__(loader)
         self.num_prefetches = num_prefetches or loader.batch_size
