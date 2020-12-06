b7405f1f584fa4564f67fdff995ea90055798d2d
Sergey Kolesnikov <scitator@gmail.com>
2020-12-06 12:15:20 +0300

    +

--- catalyst/metrics/tests/test_dice.py
+++ catalyst/metrics/tests/test_dice.py
@@ -14,16 +14,16 @@ def test_dice():
     # check 0: one empty
     empty = torch.zeros(shape)
     full = torch.ones(shape)
-    assert dice(empty, full, activation="none").item() == 0
+    assert dice(empty, full).item() == 0
 
     # check 0: no overlap
     left = torch.ones(shape)
     left[:, :, :, half_size:] = 0
     right = torch.ones(shape)
     right[:, :, :, :half_size] = 0
-    assert dice(left, right, activation="none").item() == 0
+    assert dice(left, right).item() == 0
 
     # check 1: both empty, both full, complete overlap
-    assert dice(empty, empty, activation="none") == 1
-    assert dice(full, full, activation="none") == 1
-    assert dice(left, left, activation="none") == 1
+    assert dice(empty, empty) == 1
+    assert dice(full, full) == 1
+    assert dice(left, left) == 1
