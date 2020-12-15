4424deba697f57b21b33702c4b2aecf8dc81398a
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-12-15 21:26:55 +0300

    upd registry

--- catalyst/experiments/config.py
+++ catalyst/experiments/config.py
@@ -361,9 +361,12 @@ class ConfigExperiment(IExperiment):
     ) -> Union[Scheduler, Dict[str, Scheduler]]:
         optimizer_key = params.pop("_optimizer", None)
         optimizer = optimizer[optimizer_key] if optimizer_key else optimizer
-        scheduler = REGISTRY.get_from_params(
-            subregistry="scheduler", **params, optimizer=optimizer
-        )
+        try:
+            scheduler = REGISTRY.get_from_params(
+                subregistry="scheduler", **params, optimizer=optimizer
+            )
+        except Exception:
+            raise Exception(f"{params}")
 
         return scheduler
 
