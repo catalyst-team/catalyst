6f2775fef090345736dec29c2cc9b824527b706a
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2020-12-15 23:21:05 +0200

    init & destroy process group

--- catalyst/engines/distributed.py
+++ catalyst/engines/distributed.py
@@ -1,11 +1,42 @@
+import os
 import torch.nn as nn
+import torch.distributed as dist
 
 from catalyst.engines.device import DeviceEngine
 
 
 class DistributedDeviceEngine(DeviceEngine):
-    # TODO: maybe handle unpacking DataParallel to simple nn.Module
+    def __init__(
+        self,
+        address: str = "localhost",
+        port: str = "12345",
+        backend: str = "nccl",
+    ):
+        self.device = "ddp"
+        self.address = address
+        self.port = port
+        self.backend = backend
 
+    def __repr__(self):
+        return (
+            f"DistributedDeviceEngine(address={self.address},"
+            f"port={self.port},backend='{self.backend}')"
+        )
+
+    def setup_experiment(self, rank: int, world_size: int):
+        """
+        Args:
+            rank: process rank
+            world_size: total number of processes in experiment
+        """
+        os.environ["MASTER_ADDR"] = str(self.address)
+        os.environ["MASTER_PORT"] = str(self.port)
+        dist.init_process_group(self.backend, rank=rank, world_size=world_size)
+
+    def cleanup(self):
+        dist.destroy_process_group()
+
+    # TODO: maybe handle unpacking DataParallel to simple nn.Module
     def load_checkpoint(
         self,
         file: str,
@@ -27,3 +58,4 @@ class DistributedDeviceEngine(DeviceEngine):
 
         if "scheduler_state_dict" in content and scheduler is not None:
             scheduler.load_state_dict(content["scheduler_state_dict"])
+
