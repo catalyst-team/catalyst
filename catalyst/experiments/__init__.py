d8044b881617a8d430e67f4b1fa4c12f4ff28802
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2021-02-04 16:01:04 +0300

    upd callbacks imports

--- catalyst/experiments/__init__.py
+++ catalyst/experiments/__init__.py
@@ -4,7 +4,7 @@ from catalyst.experiments.misc import SingleStageExperiment
 
 # from catalyst.experiments.experiment import Experiment
 # from catalyst.experiments.auto import AutoCallbackExperiment
-# from catalyst.experiments.config import ConfigExperiment
+from catalyst.experiments.config import ConfigExperiment
 
 # from catalyst.settings import IS_HYDRA_AVAILABLE
 #
