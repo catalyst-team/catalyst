2732d55a1dde67cdd5bc9b12f443dda1ba9f1c92
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-02-12 19:58:45 +0300

    Add some docs

--- catalyst/metrics/accuracy.py
+++ catalyst/metrics/accuracy.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, Tuple
 
 import numpy as np
 
@@ -19,6 +19,15 @@ class AccuracyMetric(ICallbackBatchMetric):
         prefix: str = None,
         suffix: str = None,
     ):
+        """
+        Init AccuracyMetric
+        Args:
+            topk_args: list of `topk` for accuracy@topk computing
+            num_classes: number of classes
+            compute_on_call: if True, computes and returns metric value during metric call
+            prefix: metric prefix
+            suffix: metric suffix
+        """
         super().__init__(
             compute_on_call=compute_on_call, prefix=prefix, suffix=suffix
         )
@@ -32,21 +41,39 @@ class AccuracyMetric(ICallbackBatchMetric):
         ]
 
     def reset(self) -> None:
+        """Reset all fields"""
         for metric in self.additive_metrics:
             metric.reset()
 
-    def update(
-        self, logits: torch.Tensor, targets: torch.Tensor
-    ) -> List[float]:
+    def update(self, logits: torch.Tensor, targets: torch.Tensor) -> List[float]:
+        """
+        Update metric value with accuracy for new data and return intermediate metrics values.
+
+        Args:
+            logits: tensor of logits
+            targets: tensor of targets
+
+        Returns:
+            list of accuracy@k values
+        """
         values = accuracy(logits, targets, topk=self.topk_args)
         values = [v.item() for v in values]
         for value, metric in zip(values, self.additive_metrics):
             metric.update(value, len(targets))
         return values
 
-    def update_key_value(
-        self, logits: torch.Tensor, targets: torch.Tensor
-    ) -> Dict[str, float]:
+    def update_key_value(self, logits: torch.Tensor, targets: torch.Tensor) -> Dict[str, float]:
+        """
+        Update metric value with accuracy for new data and return intermediate metrics
+        values in key-value format.
+
+        Args:
+            logits: tensor of logits
+            targets: tensor of targets
+
+        Returns:
+            dict of accuracy@k values
+        """
         values = self.update(logits=logits, targets=targets)
         output = {
             f"{self.prefix}accuracy{key:02d}{self.suffix}": value
@@ -57,13 +84,22 @@ class AccuracyMetric(ICallbackBatchMetric):
         ]
         return output
 
-    def compute(self) -> Any:
-        means, stds = zip(
-            *(metric.compute() for metric in self.additive_metrics)
-        )
+    def compute(self) -> Tuple[List[float], List[float]]:
+        """
+        Compute accuracy for all data
+        Returns:
+            list of mean values, list of std values
+        """
+        means, stds = zip(*(metric.compute() for metric in self.additive_metrics))
         return means, stds
 
     def compute_key_value(self) -> Dict[str, float]:
+        """
+        Compute accuracy for all data and return results in key-value format
+
+        Returns:
+            dict of metrics
+        """
         means, stds = self.compute()
         output_mean = {
             f"{self.prefix}accuracy{key:02d}{self.suffix}": value
@@ -73,16 +109,17 @@ class AccuracyMetric(ICallbackBatchMetric):
             f"{self.prefix}accuracy{key:02d}{self.suffix}/std": value
             for key, value in zip(self.topk_args, stds)
         }
-        output_mean[self.metric_name_mean] = output_mean[
-            f"{self.prefix}accuracy01{self.suffix}"
-        ]
-        output_std[self.metric_name_std] = output_std[
-            f"{self.prefix}accuracy01{self.suffix}/std"
-        ]
+        output_mean[self.metric_name_mean] = output_mean[f"{self.prefix}accuracy01{self.suffix}"]
+        output_std[self.metric_name_std] = output_std[f"{self.prefix}accuracy01{self.suffix}/std"]
         return {**output_mean, **output_std}
 
 
 class MultilabelAccuracyMetric(AdditiveValueMetric, ICallbackBatchMetric):
+    """
+    This metric computes accuracy for multilabel classification case.
+    It computes mean value of accuracy and it's approximate std value
+    (note that it's not a real std but something close).
+    """
     def __init__(
         self,
         compute_on_call: bool = True,
@@ -95,13 +132,11 @@ class MultilabelAccuracyMetric(AdditiveValueMetric, ICallbackBatchMetric):
 
         Args:
             compute_on_call: if True, computes and returns metric value during metric call
-            prefix: ?
-            suffix: ?
+            prefix: metric prefix
+            suffix: metric suffix
             thresholds: thresholds to predict labels
         """
-        super(MultilabelAccuracyMetric, self).__init__(
-            compute_on_call=compute_on_call
-        )
+        super().__init__(compute_on_call=compute_on_call)
         self.prefix = prefix or ""
         self.suffix = suffix or ""
         self.metric_name_mean = f"{self.prefix}accuracy{self.suffix}"
@@ -144,16 +179,13 @@ class MultilabelAccuracyMetric(AdditiveValueMetric, ICallbackBatchMetric):
 
     def compute_key_value(self) -> Dict[str, float]:
         """
-        Compute accuracy for all data
+        Compute accuracy for all data and return results in key-value format
 
         Returns:
             dict of metrics
         """
         metric_mean, metric_std = self.compute()
-        return {
-            self.metric_name_mean: metric_mean,
-            self.metric_name_std: metric_std,
-        }
+        return {self.metric_name_mean: metric_mean, self.metric_name_std: metric_std, }
 
 
 __all__ = ["AccuracyMetric", "MultilabelAccuracyMetric"]
--- catalyst/metrics/additive.py
+++ catalyst/metrics/additive.py
@@ -6,7 +6,15 @@ from catalyst.metrics.metric import IMetric
 
 
 class AdditiveValueMetric(IMetric):
+    """This metric computes mean and std values of input data."""
+
     def __init__(self, compute_on_call: bool = True):
+        """
+        Init AdditiveValueMetric
+
+        Args:
+            compute_on_call: if True, computes and returns metric value during metric call
+        """
         super().__init__(compute_on_call=compute_on_call)
         self.n = 0
         self.value = 0.0
@@ -17,6 +25,7 @@ class AdditiveValueMetric(IMetric):
         self.num_samples = 0
 
     def reset(self) -> None:
+        """Reset all fields"""
         self.n = 0
         self.value = 0.0
         self.mean = np.nan
@@ -26,6 +35,13 @@ class AdditiveValueMetric(IMetric):
         self.num_samples = 0
 
     def update(self, value: float, num_samples: int) -> float:
+        """
+        Update mean metric value and std with new value.
+
+        Args:
+            value: value to update mean and std with
+            num_samples: number of value samples that metrics should be updated with
+        """
         self.value = value
         self.n += 1
         self.num_samples += num_samples
@@ -46,6 +62,12 @@ class AdditiveValueMetric(IMetric):
         return value
 
     def compute(self) -> Tuple[float, float]:
+        """
+        Returns mean and std values of all the input data
+
+        Returns:
+            tuple of mean and std values
+        """
         return self.mean, self.std
 
 
