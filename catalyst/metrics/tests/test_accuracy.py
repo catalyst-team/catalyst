6be01af26eed6e369be7fa90041515d6018f25cf
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-02-20 09:33:01 +0300

    hotfix

--- catalyst/metrics/tests/test_accuracy.py
+++ catalyst/metrics/tests/test_accuracy.py
@@ -36,7 +36,7 @@ from catalyst.metrics.accuracy import AccuracyMetric, MultilabelAccuracyMetric
             torch.tensor(
                 [
                     [0.1, 0.2, 0.7, 0.],
-                    [0.5, 0.5, 0.0, 0.0],
+                    [0.49, 0.51, 0.0, 0.0],
                     [0.6, 0.3, 0.1, 0.0],
                 ],
             ),
