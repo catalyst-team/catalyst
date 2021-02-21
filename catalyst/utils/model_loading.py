09d4f85aca4c883552b7139472b801e84bfc2133
Nikita <Balaganskij.NN@phystech.edu>
2021-02-21 20:07:14 +0300

    model loading

--- catalyst/utils/model_loading.py
+++ catalyst/utils/model_loading.py
@@ -10,17 +10,10 @@ from catalyst.utils import (
 
 
 def load_model(logdir: Path, checkpoint_name: str = "best", stage: str = None):
-    config_path = logdir / "configs" / "_config.json"
     checkpoint_path = logdir / "checkpoints" / f"{checkpoint_name}.pth"
 
-    config: Dict[str, dict] = load_config(config_path)
 
-    # Get expdir name
-    config_expdir = Path(config["args"]["expdir"])
-    # We will use copy of expdir from logs for reproducibility
-    expdir = Path(logdir) / "code" / config_expdir.name
-
-    experiment, runner, _ = prepare_config_api_components(expdir=expdir, config=config)
+    experiment, runner = load_experiment(logdir=logdir)
 
     if stage is None:
         stage = list(experiment.stages)[0]
@@ -29,3 +22,16 @@ def load_model(logdir: Path, checkpoint_name: str = "best", stage: str = None):
     checkpoint = load_checkpoint(checkpoint_path)
     unpack_checkpoint(checkpoint, model=model)
     return model
+
+
+def load_experiment(logdir: Path):
+    config_path = logdir / "configs" / "_config.json"
+    config: Dict[str, dict] = load_config(config_path)
+
+    # Get expdir name
+    config_expdir = Path(config["args"]["expdir"])
+    # We will use copy of expdir from logs for reproducibility
+    expdir = Path(logdir) / "code" / config_expdir.name
+
+    experiment, runner, _ = prepare_config_api_components(expdir=expdir, config=config)
+    return experiment, runner
