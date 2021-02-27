c7cf9c06117db8b80417a9afdf57a40b7736a1d2
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-02-27 19:20:40 +0300

    Start metric callbacks

--- catalyst/callbacks/metric.py
+++ catalyst/callbacks/metric.py
@@ -1,53 +1,118 @@
 # @TODO: add metric aggregation, etc callback
+from abc import abstractmethod, ABC
+from typing import Dict, Iterable, Union, Tuple
+
+import torch
+
 from catalyst.core.callback import Callback, CallbackNode, CallbackOrder
 from catalyst.core.runner import IRunner
-from catalyst.metrics._metric import ICallbackBatchMetric, ICallbackLoaderMetric
+from catalyst.metrics import ICallbackBatchMetric, ICallbackLoaderMetric, IMetric
 
 
-class IMetricCallback(Callback):
+class IMetricCallback(Callback, ABC):
     """Metric callback interface, abstraction over metric step."""
+    @abstractmethod
+    def on_loader_start(self, runner: "IRunner") -> None:
+        pass
 
-    pass
+    @abstractmethod
+    def on_batch_end(self, runner: "IRunner") -> None:
+        pass
+
+    @abstractmethod
+    def on_loader_end(self, runner: "IRunner") -> None:
+        pass
 
 
-# @TODO: add KV support for input/output
-class BatchMetricCallback(IMetricCallback):
+class MetricCallback(IMetricCallback):
+    def __init__(
+            self,
+            metric: Union[ICallbackBatchMetric, ICallbackLoaderMetric],
+            input_key: Union[str, Iterable[str], Dict[str, str]],
+            target_key: str,
+    ):
+        super().__init__(order=CallbackOrder.metric, node=CallbackNode.all)
+        self.metric = metric
+        self._validate_metric(metric=metric)
+        self.input_key = input_key
+        self.target_key = target_key
+        self._is_value_input = isinstance(input_key, str)
+        self._keys = {
+            **self._format_keys_to_kv(input_key),
+            **self._format_keys_to_kv(target_key),
+        }
+
+    @staticmethod
+    def _validate_metric(metric: IMetric) -> None:
+        assert isinstance(metric, IMetric)
+
+    @staticmethod
+    def _format_keys_to_kv(keys: Union[str, Iterable[str], Dict[str, str]]) -> Dict[str, str]:
+        kv_keys = {}
+        if isinstance(keys, dict):
+            kv_keys.update(keys)
+        elif isinstance(keys, str):
+            kv_keys[keys] = keys
+        else:
+            for key in keys:
+                kv_keys[key] = key
+        return kv_keys
+
+    def on_loader_end(self, runner: "IRunner") -> None:
+        runner.loader_metrics.update(self.metric.compute_key_value())
+
+    def _get_value_inputs(self, runner: "Runner") -> Tuple[torch.Tensor, torch.Tensor]:
+        inputs, targets = runner.batch[self.input_key], runner.batch[self.target_key]
+        inputs, targets = runner.engine.sync_tensor(inputs), runner.engine.sync_tensor(targets)
+        return inputs, targets
+
+    def _get_key_value_inputs(self, runner: "Runner") -> Dict[str, torch.Tensor]:
+        kv_inputs = {}
+        for key in self._keys:
+            kv_inputs[self._keys[key]] = runner.engine.sync_tensor(runner.batch[key])
+        return kv_inputs
+
+    def on_batch_end(self, runner: "IRunner") -> None:
+        raise NotImplementedError
+
+    def on_loader_start(self, runner: "IRunner") -> None:
+        raise NotImplementedError
+
+
+class BatchMetricCallback(MetricCallback):
     def __init__(
         self,
-        metric: ICallbackBatchMetric,
-        input_key: str,
+        metric: Union[ICallbackBatchMetric, ICallbackLoaderMetric],
+        input_key: Union[str, Iterable[str], Dict[str, str]],
         target_key: str,
         log_on_batch: bool = True,
     ):
-        super().__init__(order=CallbackOrder.metric, node=CallbackNode.all)
-        assert isinstance(metric, ICallbackBatchMetric)
-        self.metric = metric
-        self.input_key = input_key
-        self.target_key = target_key
+        super().__init__(metric=metric, input_key=input_key, target_key=target_key)
         self.log_on_batch = log_on_batch
+
+    @staticmethod
+    def _validate_metric(metric: IMetric) -> None:
+        assert isinstance(metric, ICallbackBatchMetric)
 
     def on_loader_start(self, runner: "IRunner") -> None:
         self.metric.reset()
 
     def on_batch_end(self, runner: "IRunner") -> None:
-        inputs, targets = runner.batch[self.input_key], runner.batch[self.target_key]
-        inputs, targets = runner.engine.sync_tensor(inputs), runner.engine.sync_tensor(targets)
+        if self._is_value_input:
+            inputs, targets = self._get_value_inputs(runner=runner)
+            metrics = self.metric.update_key_value(inputs, targets)
+        else:
+            kv_inputs = self._get_key_value_inputs(runner=runner)
+            metrics = self.metric.update_key_value(**kv_inputs)
 
-        metrics = self.metric.update_key_value(inputs, targets)
         if self.log_on_batch:
             runner.batch_metrics.update(metrics)
 
-    def on_loader_end(self, runner: "IRunner") -> None:
-        runner.loader_metrics.update(self.metric.compute_key_value())
 
-
-class LoaderMetricCallback(IMetricCallback):
-    def __init__(self, metric: ICallbackLoaderMetric, input_key: str, target_key: str):
-        super().__init__(order=CallbackOrder.metric, node=CallbackNode.all)
+class LoaderMetricCallback(MetricCallback):
+    @staticmethod
+    def _validate_metric(metric: IMetric) -> None:
         assert isinstance(metric, ICallbackLoaderMetric)
-        self.metric = metric
-        self.input_key = input_key
-        self.target_key = target_key
 
     def on_loader_start(self, runner: "IRunner") -> None:
         self.metric.reset(
@@ -55,12 +120,12 @@ class LoaderMetricCallback(IMetricCallback):
         )
 
     def on_batch_end(self, runner: "IRunner") -> None:
-        inputs, targets = runner.batch[self.input_key], runner.batch[self.target_key]
-        inputs, targets = runner.engine.sync_tensor(inputs), runner.engine.sync_tensor(targets)
-        self.metric.update(inputs, targets)
-
-    def on_loader_end(self, runner: "IRunner") -> None:
-        runner.loader_metrics.update(self.metric.compute_key_value())
+        if self._is_value_input:
+            inputs, targets = self._get_value_inputs(runner=runner)
+            self.metric.update(inputs, targets)
+        else:
+            kv_inputs = self._get_key_value_inputs(runner=runner)
+            self.metric.update(**kv_inputs)
 
 
 __all__ = ["IMetricCallback", "BatchMetricCallback", "LoaderMetricCallback"]
