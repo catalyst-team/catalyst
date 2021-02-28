270f0fef5af0bfc6f866a8153df412a4d30973bc
Julia Shenshina <yuliya.shenshina@phystech.edu>
2021-02-28 20:47:24 +0300

    Add some tests

--- catalyst/callbacks/metric.py
+++ catalyst/callbacks/metric.py
@@ -1,19 +1,23 @@
 # @TODO: add metric aggregation, etc callback
-from abc import abstractmethod, ABC
-from typing import Dict, Iterable, Union, Tuple, Optional
+from typing import Dict, Iterable, Optional, Tuple, Union
+from abc import ABC, abstractmethod
 
 import torch
 
 from catalyst.core.callback import Callback, CallbackNode, CallbackOrder
 from catalyst.core.runner import IRunner
-from catalyst.metrics import ICallbackBatchMetric, ICallbackLoaderMetric, IMetric
-
+from catalyst.metrics import (
+    ICallbackBatchMetric,
+    ICallbackLoaderMetric,
+    IMetric,
+)
 
 TORCH_BOOL = torch.bool if torch.__version__ > "1.1.0" else torch.ByteTensor
 
 
 class IMetricCallback(Callback, ABC):
     """Metric callback interface, abstraction over metric step."""
+
     @abstractmethod
     def on_loader_start(self, runner: "IRunner") -> None:
         """
@@ -47,10 +51,10 @@ class IMetricCallback(Callback, ABC):
 
 class MetricCallback(IMetricCallback):
     def __init__(
-            self,
-            metric: Union[ICallbackBatchMetric, ICallbackLoaderMetric],
-            input_key: Union[str, Iterable[str], Dict[str, str]],
-            target_key: Union[str, Iterable[str], Dict[str, str]],
+        self,
+        metric: Union[ICallbackBatchMetric, ICallbackLoaderMetric],
+        input_key: Union[str, Iterable[str], Dict[str, str]],
+        target_key: Union[str, Iterable[str], Dict[str, str]],
     ):
         """
         Init MetricCallback
@@ -95,7 +99,9 @@ class MetricCallback(IMetricCallback):
         assert isinstance(metric, IMetric)
 
     @staticmethod
-    def _convert_keys_to_kv(keys: Union[str, Iterable[str], Dict[str, str]]) -> Dict[str, str]:
+    def _convert_keys_to_kv(
+        keys: Union[str, Iterable[str], Dict[str, str]]
+    ) -> Dict[str, str]:
         """
         Convert keys to key-value format
 
@@ -116,7 +122,9 @@ class MetricCallback(IMetricCallback):
                 kv_keys[key] = key
         return kv_keys
 
-    def _get_value_inputs(self, runner: "Runner") -> Tuple[torch.Tensor, torch.Tensor]:
+    def _get_value_inputs(
+        self, runner: "Runner"
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
         """
         Get data from batch in value input case
 
@@ -126,11 +134,19 @@ class MetricCallback(IMetricCallback):
         Returns:
             tuple of tensor of inputs and tensor of targets
         """
-        inputs, targets = runner.batch[self.input_key], runner.batch[self.target_key]
-        inputs, targets = runner.engine.sync_tensor(inputs), runner.engine.sync_tensor(targets)
+        inputs, targets = (
+            runner.batch[self.input_key],
+            runner.batch[self.target_key],
+        )
+        inputs, targets = (
+            runner.engine.sync_tensor(inputs),
+            runner.engine.sync_tensor(targets),
+        )
         return inputs, targets
 
-    def _get_key_value_inputs(self, runner: "Runner") -> Dict[str, torch.Tensor]:
+    def _get_key_value_inputs(
+        self, runner: "Runner"
+    ) -> Dict[str, torch.Tensor]:
         """
         Get data from batch in key-value input case
 
@@ -142,11 +158,13 @@ class MetricCallback(IMetricCallback):
         """
         kv_inputs = {}
         for key in self._keys:
-            kv_inputs[self._keys[key]] = runner.engine.sync_tensor(runner.batch[key])
+            kv_inputs[self._keys[key]] = runner.engine.sync_tensor(
+                runner.batch[key]
+            )
         return kv_inputs
 
     def _update_value_metric(
-            self, inputs_tuple: Tuple[torch.Tensor, torch.Tensor]
+        self, inputs_tuple: Tuple[torch.Tensor, torch.Tensor]
     ) -> Optional[Dict[str, float]]:
         """
         Update metric in value input case
@@ -161,7 +179,7 @@ class MetricCallback(IMetricCallback):
         return self._metric_update_method(inputs, targets)
 
     def _update_key_value_metric(
-            self, kv_inputs: Dict[str, torch.Tensor]
+        self, kv_inputs: Dict[str, torch.Tensor]
     ) -> Optional[Dict[str, float]]:
         """
         Update metric in key-value input case
@@ -221,7 +239,9 @@ class BatchMetricCallback(MetricCallback):
             target_key: keys of tensors that should be used as targets in metric calculation
             log_on_batch: if True update runner's batch metrics every batch
         """
-        super().__init__(metric=metric, input_key=input_key, target_key=target_key)
+        super().__init__(
+            metric=metric, input_key=input_key, target_key=target_key
+        )
         self.log_on_batch = log_on_batch
         self._metric_update_method = self.metric.update_key_value
 
@@ -268,8 +288,13 @@ class LoaderMetricCallback(MetricCallback):
             runner: current runner
         """
         self.metric.reset(
-            num_batches=runner.loader_batch_len, num_samples=runner.loader_sample_len
+            num_batches=runner.loader_batch_len,
+            num_samples=runner.loader_sample_len,
         )
 
 
-__all__ = ["IMetricCallback", "BatchMetricCallback", "LoaderMetricCallback", ]
+__all__ = [
+    "IMetricCallback",
+    "BatchMetricCallback",
+    "LoaderMetricCallback",
+]
--- catalyst/metrics/_cmc_score.py
+++ catalyst/metrics/_cmc_score.py
@@ -1,11 +1,10 @@
+from typing import Any, Dict, Iterable, List, Optional
 from collections import defaultdict
-from typing import Dict, Any, Iterable, List, Optional
 
 import torch
 
-from catalyst.metrics.functional._cmc_score import cmc_score
 from catalyst.metrics import ICallbackLoaderMetric
-
+from catalyst.metrics.functional._cmc_score import cmc_score
 
 TORCH_BOOL = torch.bool if torch.__version__ > "1.1.0" else torch.ByteTensor
 
@@ -14,11 +13,11 @@ class AccumulationMetric(ICallbackLoaderMetric):
     """This metric accumulates all the input data along loader"""
 
     def __init__(
-            self,
-            compute_on_call: bool = True,
-            prefix: Optional[str] = None,
-            suffix: Optional[str] = None,
-            accumulative_fields: Iterable[str] = (),
+        self,
+        compute_on_call: bool = True,
+        prefix: Optional[str] = None,
+        suffix: Optional[str] = None,
+        accumulative_fields: Iterable[str] = (),
     ) -> None:
         """
         Init AccumulationMetric
@@ -29,7 +28,9 @@ class AccumulationMetric(ICallbackLoaderMetric):
             suffix: metric's suffix
             accumulative_fields: list of keys to accumulate data from batch
         """
-        super().__init__(compute_on_call=compute_on_call, prefix=prefix, suffix=suffix)
+        super().__init__(
+            compute_on_call=compute_on_call, prefix=prefix, suffix=suffix
+        )
         self.accumulative_fields = accumulative_fields
         self.storage = None
         self.num_samples = None
@@ -60,7 +61,8 @@ class AccumulationMetric(ICallbackLoaderMetric):
         self.storage = defaultdict(torch.Tensor)
         for key in shape_type_dict:
             self.storage[key] = torch.empty(
-                size=shape_type_dict[key]["shape"], dtype=shape_type_dict[key]["dtype"]
+                size=shape_type_dict[key]["shape"],
+                dtype=shape_type_dict[key]["dtype"],
             )
 
     def update(self, **kwargs) -> None:
@@ -75,15 +77,17 @@ class AccumulationMetric(ICallbackLoaderMetric):
             for field_name in self.accumulative_fields:
                 shape_type_dict[field_name] = {}
                 shape_type_dict[field_name]["shape"] = (
-                    self.num_samples, *(kwargs[field_name].shape[1:])
+                    self.num_samples,
+                    *(kwargs[field_name].shape[1:]),
                 )
                 shape_type_dict[field_name]["dtype"] = kwargs[field_name].dtype
             self._allocate_memory(shape_type_dict=shape_type_dict)
         bs = 0
         for field_name in self.accumulative_fields:
             bs = kwargs[field_name].shape[0]
-            self.storage[field_name][self.collected_samples: self.collected_samples + bs, ...] \
-                = kwargs[field_name].detach().cpu()
+            self.storage[field_name][
+                self.collected_samples : self.collected_samples + bs, ...
+            ] = (kwargs[field_name].detach().cpu())
         self.collected_samples += bs
         self.collected_batches += 1
 
@@ -110,14 +114,14 @@ class CMCMetric(AccumulationMetric):
     """Cumulative Matching Characteristics"""
 
     def __init__(
-            self,
-            compute_on_call: bool = True,
-            prefix: Optional[str] = None,
-            suffix: Optional[str] = None,
-            embeddings_key: str = "features",
-            labels_key: str = "targets",
-            is_query_key: str = "is_query",
-            topk_args: Iterable[int] = (1, ),
+        self,
+        compute_on_call: bool = True,
+        prefix: Optional[str] = None,
+        suffix: Optional[str] = None,
+        embeddings_key: str = "features",
+        labels_key: str = "targets",
+        is_query_key: str = "is_query",
+        topk_args: Iterable[int] = (1,),
     ) -> None:
         """
         Init CMCMetric
@@ -135,7 +139,7 @@ class CMCMetric(AccumulationMetric):
             compute_on_call=compute_on_call,
             prefix=prefix,
             suffix=suffix,
-            accumulative_fields=[embeddings_key, labels_key, is_query_key]
+            accumulative_fields=[embeddings_key, labels_key, is_query_key],
         )
         self.embeddings_key = embeddings_key
         self.labels_key = labels_key
@@ -161,7 +165,9 @@ class CMCMetric(AccumulationMetric):
         gallery_embeddings = embeddings[~query_mask]
         gallery_labels = labels[~query_mask]
 
-        conformity_matrix = (gallery_labels == query_labels.reshape(-1, 1)).to(TORCH_BOOL)
+        conformity_matrix = (gallery_labels == query_labels.reshape(-1, 1)).to(
+            TORCH_BOOL
+        )
 
         metrics = []
         for k in self.topk_args:
@@ -184,9 +190,12 @@ class CMCMetric(AccumulationMetric):
         """
         values = self.compute()
         kv_metrics = {
-            f"{self.metric_name}{k:02d}": value for k, value in zip(self.topk_args, values)
+            f"{self.metric_name}{k:02d}": value
+            for k, value in zip(self.topk_args, values)
         }
         return kv_metrics
 
 
-__all__ = ["CMCMetric", ]
+__all__ = [
+    "CMCMetric",
+]
