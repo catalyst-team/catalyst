fa5281bd80ca70400b05d5abc73ed17a76715cb9
Dokholyan <Dokholyan97@mail.ru>
2021-03-02 22:35:11 +0300

    add full example

--- catalyst/callbacks/aggregation.py
+++ catalyst/callbacks/aggregation.py
@@ -1,6 +1,6 @@
 import torch
 from catalyst.dl import Callback, CallbackOrder, CallbackNode
-from typing import Union, List, Dict, Any
+from typing import Union, List, Dict, Any, Callable
 
 
 class MetricAggregationCallback(Callback):
@@ -10,7 +10,7 @@ class MetricAggregationCallback(Callback):
         self,
         prefix: str,
         metrics: Union[str, List[str], Dict[str, float]] = None,
-        mode: str = "mean",
+        mode: Union[str, Callable] = "mean",
         scope: str = "batch",
         multiplier: float = 1.0,
     ) -> None:
@@ -30,20 +30,67 @@ class MetricAggregationCallback(Callback):
                 ``epoch``
             multiplier: scale factor for the aggregated metric.
         Examples:
-            Loss depends on epoch(Note that all loss functions that are used
-            in the aggregation function must be defined.)
-            >>> from catalyst.dl import MetricAggregationCallback
+            Loss is a weighted sum of cross entropy loss and binary cross entropy loss
+            >>> import torch
+            >>> from torch.utils.data import DataLoader, TensorDataset
+            >>> from catalyst import dl
             >>>
-            >>> def aggregation_function(metrics, runner):
-            >>>     epoch = runner.stage_epoch_step
-            >>>     region_loss = metrics["loss_dice"] + metrics["loss_iou"]
-            >>>     bce_loss = metrics['loss_bce']
-            >>>     loss = 1 / epoch * bce_loss + epoch / 3 * region_loss
-            >>>     return loss
+            >>> # sample data
+            >>> num_samples, num_features, num_classes = int(1e4), int(1e1), 4
+            >>> X = torch.rand(num_samples, num_features)
+            >>> y = (torch.rand(num_samples, ) * num_classes).to(torch.int64)
             >>>
-            >>> MetricAggregationCallback(
-            >>>     mode=aggregation_function,
-            >>>     prefix='loss',
+            >>> # pytorch loaders
+            >>> dataset = TensorDataset(X, y)
+            >>> loader = DataLoader(dataset, batch_size=32, num_workers=1)
+            >>> loaders = {"train": loader, "valid": loader}
+            >>>
+            >>> # model, criterion, optimizer, scheduler
+            >>> model = torch.nn.Linear(num_features, num_classes)
+            >>> criterion = {"ce": torch.nn.CrossEntropyLoss(), "bce": torch.nn.BCEWithLogitsLoss()}
+            >>> optimizer = torch.optim.Adam(model.parameters())
+            >>> scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [2])
+            >>>
+            >>> class CustomSupervisedRunner(dl.Runner):
+            >>>     def handle_batch(self, batch):
+            >>>         x, y = batch
+            >>>         logits = self.model(x)
+            >>>         num_classes = logits.shape[-1]
+            >>>         self.batch = {
+            >>>             "features": x,
+            >>>             "logits": logits,
+            >>>             "targets": y,
+            >>>             "targets_onehot": torch.nn.functional.one_hot(y, num_classes=num_classes).float()
+            >>>         }
+            >>>
+            >>> # model training
+            >>> runner = CustomSupervisedRunner()
+            >>> runner.train(
+            >>>     model=model,
+            >>>     criterion=criterion,
+            >>>     optimizer=optimizer,
+            >>>     scheduler=scheduler,
+            >>>     loaders=loaders,
+            >>>     logdir="./logdir",
+            >>>     num_epochs=3,
+            >>>     callbacks=[
+            >>>         dl.AccuracyCallback(input_key="logits",
+            >>>                             target_key="targets",
+            >>>                             num_classes=num_classes),
+            >>>         dl.CriterionCallback(input_key="logits",
+            >>>              target_key="targets",
+            >>>              metric_key="loss_ce",
+            >>>              criterion_key="ce"),
+            >>>         dl.CriterionCallback(input_key="logits",
+            >>>                              target_key="targets_onehot",
+            >>>                              metric_key="loss_bce",
+            >>>                              criterion_key="bce"),
+            >>>         # loss aggregation
+            >>>         dl.MetricAggregationCallback(prefix='loss',
+            >>>                                      metrics={'loss_ce': 0.6, 'loss_bce': 0.4},
+            >>>                                      mode='weighted_sum'),
+            >>>         dl.OptimizerCallback(metric_key="loss")
+            >>>     ]
             >>> )
         """
         super().__init__(
