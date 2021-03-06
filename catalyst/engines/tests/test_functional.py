09b104b4f63b030b0666ca5a5991ec97007fd681
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2021-03-06 20:33:08 +0200

    removed process_engine tests

--- catalyst/engines/tests/test_functional.py
+++ catalyst/engines/tests/test_functional.py
@@ -11,60 +11,12 @@ from catalyst.engines import (
     DataParallelEngine,
     DeviceEngine,
     DistributedDataParallelEngine,
-    process_engine,
 )
 from catalyst.engines.functional import all_gather, mean_reduce, sum_reduce
 from catalyst.settings import IS_CUDA_AVAILABLE, NUM_CUDA_DEVICES
 
 if NUM_CUDA_DEVICES > 1:
     os.environ["MKL_SERVICE_FORCE_INTEL"] = "1"
-
-
-@mark.skipif(
-    not IS_CUDA_AVAILABLE or (NUM_CUDA_DEVICES != 1), reason="CUDA device is not available",
-)
-def test_device_engine_from_none():
-    actual = process_engine(None)
-    assert isinstance(actual, DeviceEngine)
-    assert actual.device == "cuda"
-
-
-@mark.skipif(
-    not IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES < 2, reason="Number of CUDA devices is less than 2",
-)
-def test_dp_engine_from_none():
-    actual = process_engine(None)
-    assert isinstance(actual, DataParallelEngine)
-    assert actual.device == f"cuda:{torch.cuda.current_device()}"
-
-
-def test_engine_from_str_on_cpu():
-    actual = process_engine("cpu")
-    assert isinstance(actual, DeviceEngine)
-    assert actual.device == "cpu"
-
-
-@mark.skipif(not IS_CUDA_AVAILABLE, reason="CUDA device is not available")
-def test_engine_from_str_on_cuda():
-    actual = process_engine("cuda")
-    assert isinstance(actual, DeviceEngine)
-    assert actual.device == "cuda"
-
-
-@mark.skipif(not IS_CUDA_AVAILABLE, reason="CUDA device is not available")
-def test_engine_from_str_on_cuda_0():
-    actual = process_engine("cuda:0")
-    assert isinstance(actual, DeviceEngine)
-    assert actual.device == "cuda:0"
-
-
-@mark.skipif(
-    not IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES < 2, reason="Number of CUDA devices is less than 2",
-)
-def test_engine_from_str_on_cuda_1():
-    actual = process_engine("cuda:1")
-    assert isinstance(actual, DeviceEngine)
-    assert actual.device == "cuda:1"
 
 
 def _setup(rank: int, world_size: int, backend: str = "gloo") -> None:
