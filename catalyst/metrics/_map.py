c3092d8b3a4ca1f1471e668e738b0dee4f006e6e
Dany Chepenko <daniel.chepenko@yahoo.com>
2021-03-07 18:55:13 +0300

    Docs for recsys metrics (#1103)
    * docs for map
    
    * docs for map
    
    * docs for mrr
    
    * docs for ndcg
    
    * docs for hitrate
    
    * pep8
    
    Co-authored-by: Даниил <zkid18@MacBook-Pro.Dlink>

--- catalyst/metrics/_map.py
+++ catalyst/metrics/_map.py
@@ -8,7 +8,13 @@ from catalyst.metrics.functional._average_precision import mean_average_precisio
 
 
 class MAPMetric(ICallbackBatchMetric):
-    """@TODO: docs here"""
+    """
+    Calculate the Mean Average Precision (MAP) for RecSys.
+    The precision metric summarizes the fraction of relevant items
+    out of the whole the recommendation list.
+
+    It computes mean value of map and it's approximate std value
+    """
 
     def __init__(
         self,
@@ -17,7 +23,15 @@ class MAPMetric(ICallbackBatchMetric):
         prefix: str = None,
         suffix: str = None,
     ):
-        """@TODO: docs here"""
+        """
+        Init MAPMetric
+
+        Args:
+            topk_args: list of `topk` for map@topk computing
+            compute_on_call: if True, computes and returns metric value during metric call
+            prefix: metric prefix
+            suffix: metric suffix
+        """
         super().__init__(compute_on_call=compute_on_call, prefix=prefix, suffix=suffix)
         self.metric_name_mean = f"{self.prefix}map{self.suffix}"
         self.metric_name_std = f"{self.prefix}map{self.suffix}/std"
@@ -27,12 +41,21 @@ class MAPMetric(ICallbackBatchMetric):
         ]
 
     def reset(self) -> None:
-        """@TODO: docs here"""
+        """Reset all fields"""
         for metric in self.additive_metrics:
             metric.reset()
 
     def update(self, logits: torch.Tensor, targets: torch.Tensor) -> List[float]:
-        """@TODO: docs here"""
+        """
+        Update metric value with map for new data and return intermediate metrics values.
+
+        Args:
+            logits (torch.Tensor): tensor of logits
+            targets (torch.Tensor): tensor of targets
+
+        Returns:
+            list of map@k values
+        """
         values = mean_average_precision(logits, targets, topk=self.topk_args)
         values = [v.item() for v in values]
         for value, metric in zip(values, self.additive_metrics):
@@ -40,7 +63,17 @@ class MAPMetric(ICallbackBatchMetric):
         return values
 
     def update_key_value(self, logits: torch.Tensor, targets: torch.Tensor) -> Dict[str, float]:
-        """@TODO: docs here"""
+        """
+        Update metric value with map for new data and return intermediate metrics
+        values in key-value format.
+
+        Args:
+            logits (torch.Tensor): tensor of logits
+            targets (torch.Tensor): tensor of targets
+
+        Returns:
+            dict of map@k values
+        """
         values = self.update(logits=logits, targets=targets)
         output = {
             f"{self.prefix}map{key:02d}{self.suffix}": value
@@ -50,12 +83,22 @@ class MAPMetric(ICallbackBatchMetric):
         return output
 
     def compute(self) -> Any:
-        """@TODO: docs here"""
+        """
+        Compute map for all data
+
+        Returns:
+            list of mean values, list of std values
+        """
         means, stds = zip(*(metric.compute() for metric in self.additive_metrics))
         return means, stds
 
     def compute_key_value(self) -> Dict[str, float]:
-        """@TODO: docs here"""
+        """
+        Compute map for all data and return results in key-value format
+
+        Returns:
+            dict of metrics
+        """
         means, stds = self.compute()
         output_mean = {
             f"{self.prefix}map{key:02d}{self.suffix}": value
