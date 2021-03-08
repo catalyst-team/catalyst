c63bbf00d29712da02c60d3fce9b446ba367d82e
Dokholyan <Dokholyan97@mail.ru>
2021-03-08 18:57:21 +0300

    add micro and macro mode in update_key_value

--- catalyst/metrics/_segmentation.py
+++ catalyst/metrics/_segmentation.py
@@ -101,8 +101,10 @@ class RegionBasedMetric(ICallbackBatchMetric):
                 self.statistics[idx]["fp"] = fp_class
                 self.statistics[idx]["fn"] = fn_class
 
-        values = self.metric_fn(tp, fp, fn)
-        return values
+        metrics_per_class = self.metric_fn(tp, fp, fn)
+        micro_metric = torch.mean(metrics_per_class)
+        macro_metric = self.metric_fn(tp.sum(), fp.sum(), fn.sum())
+        return metrics_per_class, micro_metric, macro_metric
 
     def update_key_value(
         self, outputs: torch.Tensor, targets: torch.Tensor
@@ -116,18 +118,19 @@ class RegionBasedMetric(ICallbackBatchMetric):
         Returns:
             dict of metric for each class and weighted (if weights were given) metric
         """
-        values = self.update(outputs, targets)
+        metrics_per_class, micro_metric, macro_metric = self.update(outputs, targets)
         # need only one time
         if not self._checked_params:
             self._check_parameters()
             self._checked_params = True
         metrics = {
-            f"{self.prefix}/{self.class_names[idx]}": value for idx, value in enumerate(values)
+            f"{self.prefix}/{self.class_names[idx]}": value for idx, value in enumerate(metrics_per_class)
         }
-        metrics[self.prefix] = torch.mean(values)
+        metrics[f"{self.prefix}/micro"] = micro_metric
+        metrics[f"{self.prefix}/macro"] = macro_metric
         if self.weights is not None:
             weighted_metric = 0
-            for idx, value in enumerate(values):
+            for idx, value in enumerate(metrics_per_class):
                 weighted_metric += value * self.weights[idx]
             metrics[f"{self.prefix}/weighted"] = weighted_metric
         return metrics
