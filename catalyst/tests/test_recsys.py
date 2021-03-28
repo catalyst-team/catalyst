c929ab530c6d823586bf1b8f4057a1433fa02877
Sergey Kolesnikov <scitator@gmail.com>
2021-03-28 20:03:05 +0300

    Github CI fixes (#1143)
    * verbose
    
    * Utils callbacks (#1127)
    
    * quantization.py
    
    * onnx and quantization
    
    * tracing.py
    
    * docs
    
    * fix
    
    * fix
    
    * quantization test
    
    * fix test
    
    * fix comments
    
    * fix comments
    
    * Update catalyst/callbacks/__init__.py
    
    Co-authored-by: Sergey Kolesnikov <scitator@gmail.com>
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * tests
    
    * pycharm tricks
    
    * extra tests
    
    * Utils callbacks (#1145)
    
    * quantization.py
    
    * onnx and quantization
    
    * tracing.py
    
    * docs
    
    * fix
    
    * fix
    
    * quantization test
    
    * fix test
    
    * fix comments
    
    * fix comments
    
    * quantization example
    
    * examples
    
    * CHANGELOG.md
    
    * fix
    
    * fix
    
    * Apply suggestions from code review
    
    Co-authored-by: Sergey Kolesnikov <scitator@gmail.com>
    
    * extra tests 2
    
    * extra tests 2
    
    * extra tests 2
    
    * extra tests 2
    
    * extra tests 2
    
    * extra tests 2
    
    * extra tests 2
    
    * extra tests 2
    
    Co-authored-by: Nikita Balagansky <37884009+elephantmipt@users.noreply.github.com>

--- catalyst/tests/test_recsys.py
+++ catalyst/tests/test_recsys.py
@@ -7,10 +7,22 @@ import torch
 from torch.utils.data import DataLoader, TensorDataset
 
 from catalyst import dl
-from catalyst.settings import IS_CUDA_AVAILABLE, NUM_CUDA_DEVICES
+from catalyst.settings import IS_CUDA_AVAILABLE, NUM_CUDA_DEVICES, SETTINGS
 
 
-def train_experiment(device):
+class CustomRunner(dl.Runner):
+    def handle_batch(self, batch):
+        x, y = batch
+        logits = self.model(x)
+        self.batch = {
+            "features": x,
+            "logits": logits,
+            "scores": torch.sigmoid(logits),
+            "targets": y,
+        }
+
+
+def train_experiment(device, engine=None):
     with TemporaryDirectory() as logdir:
         # sample data
         num_users, num_features, num_items = int(1e4), int(1e1), 10
@@ -28,21 +40,28 @@ def train_experiment(device):
         optimizer = torch.optim.Adam(model.parameters())
         scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [2])
 
-        class CustomRunner(dl.Runner):
-            def handle_batch(self, batch):
-                x, y = batch
-                logits = self.model(x)
-                self.batch = {
-                    "features": x,
-                    "logits": logits,
-                    "scores": torch.sigmoid(logits),
-                    "targets": y,
-                }
+        callbacks = [
+            dl.CriterionCallback(input_key="logits", target_key="targets", metric_key="loss"),
+            dl.AUCCallback(input_key="scores", target_key="targets"),
+            dl.HitrateCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
+            dl.MRRCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
+            dl.MAPCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
+            dl.NDCGCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
+            dl.OptimizerCallback(metric_key="loss"),
+            dl.SchedulerCallback(),
+            dl.CheckpointCallback(
+                logdir=logdir, loader_key="valid", metric_key="map01", minimize=False
+            ),
+        ]
+        if engine is None or not isinstance(
+            engine, (dl.AMPEngine, dl.DataParallelAMPEngine, dl.DistributedDataParallelAMPEngine)
+        ):
+            callbacks.append(dl.AUCCallback(input_key="logits", target_key="targets"))
 
         # model training
         runner = CustomRunner()
         runner.train(
-            engine=dl.DeviceEngine(device),
+            engine=engine or dl.DeviceEngine(device),
             model=model,
             criterion=criterion,
             optimizer=optimizer,
@@ -50,33 +69,84 @@ def train_experiment(device):
             loaders=loaders,
             num_epochs=1,
             verbose=False,
-            callbacks=[
-                dl.CriterionCallback(input_key="logits", target_key="targets", metric_key="loss"),
-                dl.AUCCallback(input_key="scores", target_key="targets"),
-                dl.HitrateCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
-                dl.MRRCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
-                dl.MAPCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
-                dl.NDCGCallback(input_key="scores", target_key="targets", topk_args=(1, 3, 5)),
-                dl.OptimizerCallback(metric_key="loss"),
-                dl.SchedulerCallback(),
-                dl.CheckpointCallback(
-                    logdir=logdir, loader_key="valid", metric_key="map01", minimize=False
-                ),
-            ],
+            callbacks=callbacks,
         )
 
 
-def test_finetune_on_cpu():
+# Torch
+def test_on_cpu():
     train_experiment("cpu")
 
 
 @mark.skipif(not IS_CUDA_AVAILABLE, reason="CUDA device is not available")
-def test_finetune_on_cuda():
+def test_on_torch_cuda0():
     train_experiment("cuda:0")
 
 
 @mark.skipif(
-    not IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES < 2, reason="Number of CUDA devices is less than 2",
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2), reason="No CUDA>=2 found",
 )
-def test_finetune_on_cuda_device():
+def test_on_torch_cuda1():
     train_experiment("cuda:1")
+
+
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2), reason="No CUDA>=2 found",
+)
+def test_on_torch_dp():
+    train_experiment(None, dl.DataParallelEngine())
+
+
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2), reason="No CUDA>=2 found",
+)
+def test_on_torch_ddp():
+    train_experiment(None, dl.DistributedDataParallelEngine())
+
+
+# AMP
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and SETTINGS.amp_required), reason="No CUDA or AMP found",
+)
+def test_on_amp():
+    train_experiment(None, dl.AMPEngine())
+
+
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2 and SETTINGS.amp_required),
+    reason="No CUDA>=2 or AMP found",
+)
+def test_on_amp_dp():
+    train_experiment(None, dl.DataParallelAMPEngine())
+
+
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2 and SETTINGS.amp_required),
+    reason="No CUDA>=2 or AMP found",
+)
+def test_on_amp_ddp():
+    train_experiment(None, dl.DistributedDataParallelAMPEngine())
+
+
+# APEX
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and SETTINGS.apex_required), reason="No CUDA or Apex found",
+)
+def test_on_apex():
+    train_experiment(None, dl.APEXEngine())
+
+
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2 and SETTINGS.apex_required),
+    reason="No CUDA>=2 or Apex found",
+)
+def test_on_apex_dp():
+    train_experiment(None, dl.DataParallelApexEngine())
+
+
+@mark.skipif(
+    not (IS_CUDA_AVAILABLE and NUM_CUDA_DEVICES >= 2 and SETTINGS.apex_required),
+    reason="No CUDA>=2 or Apex found",
+)
+def test_on_apex_ddp():
+    train_experiment(None, dl.DistributedDataParallelApexEngine())
