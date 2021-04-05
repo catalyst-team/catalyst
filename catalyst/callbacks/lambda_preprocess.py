7e8bbe6aca699f0d5eb1dc98e34cbafc066f46ea
Nikita <Balaganskij.NN@phystech.edu>
2021-04-05 22:21:42 +0300

    refactoring

--- catalyst/callbacks/lambda_preprocess.py
+++ catalyst/callbacks/lambda_preprocess.py
@@ -8,12 +8,13 @@ class LambdaPreprocessCallback(Callback):
     Preprocess your batch with specified function.
 
     Args:
-            lambda_fn (Callable): Function to apply.
-            input_keys (Union[List[str], str], optional): Keys in batch dict to apply function.
-                Defaults to ``"logits"``.
-            output_keys (Union[List[str], str], optional): Keys for output.
-                If None then will apply function inplace to ``keys_to_apply``.
-                Defaults to ``None``.
+        lambda_fn (Callable): Function to apply.
+        scope (str): ``"on_batch_end"`` or ``"on_batch_start"``
+        input_keys (Union[List[str], str], optional): Keys in batch dict to apply function.
+            Defaults to ``None``.
+        output_keys (Union[List[str], str], optional): Keys for output.
+            If None then will apply function inplace to ``keys_to_apply``.
+            Defaults to ``None``.
 
     Raises:
         TypeError: When keys_to_apply is not str or list.
@@ -85,15 +86,16 @@ class LambdaPreprocessCallback(Callback):
     def __init__(
         self,
         lambda_fn: Callable,
-        input_keys: Union[List[str], str] = "logits",
+        scope: str,
+        input_keys: Union[List[str], str] = None,
         output_keys: Union[List[str], str] = None,
     ):
         """Wraps input for your callback with specified function.
 
         Args:
             lambda_fn (Callable): Function to apply.
+            scope (str): ``"on_batch_end"`` or ``"on_batch_start"``
             input_keys (Union[List[str], str], optional): Keys in batch dict to apply function.
-                Defaults to ``"logits"``.
             output_keys (Union[List[str], str], optional): Keys for output.
                 If None then will apply function inplace to ``keys_to_apply``.
                 Defaults to ``None``.
@@ -102,8 +104,9 @@ class LambdaPreprocessCallback(Callback):
             TypeError: When keys_to_apply is not str or list.
         """
         super().__init__(order=CallbackOrder.Internal)
-        if not isinstance(input_keys, (list, str)):
-            raise TypeError("keys to apply should be str or list of str.")
+        if input_keys is not None:
+            if not isinstance(input_keys, (list, str)):
+                raise TypeError("keys to apply should be str or list of str.")
         elif isinstance(input_keys, str):
             input_keys = [input_keys]
         if output_keys is not None:
@@ -111,12 +114,31 @@ class LambdaPreprocessCallback(Callback):
                 raise TypeError("output keys should be str or list of str.")
             if isinstance(output_keys, str):
                 output_keys = [output_keys]
+        if isinstance(scope, str) and scope in ["on_batch_end", "on_batch_start"]:
+            self.scope = scope
         else:
-            output_keys = input_keys
-        self.keys_to_apply = input_keys
+            raise TypeError(
+                """Expected scope to be on of the ["on_batch_end", "on_batch_start"]"""
+            )
+        self.input_keys = input_keys
         self.output_keys = output_keys
         self.lambda_fn = lambda_fn
-        self.handler: Union[None, Callable] = None
+        self.input_handler = (
+            (lambda batch: batch)
+            if input_keys is None
+            else (lambda batch: [batch[key] for key in input_keys])
+        )
+        self.output_handler: Union[None, Callable] = None
+
+    def on_batch_start(self, runner: "IRunner") -> None:
+        """
+        On batch start action.
+
+        Args:
+            runner: runner for the experiment.
+        """
+        if self.scope == "on_batch_start":
+            self._handle_batch(runner)
 
     def on_batch_end(self, runner) -> None:
         """
@@ -124,38 +146,60 @@ class LambdaPreprocessCallback(Callback):
 
         Args:
             runner: runner for the experiment.
-
-        Raises:
-            TypeError: If lambda_fn output has a wrong type.
-
         """
-        fn_inp = [runner.batch[key] for key in self.keys_to_apply]
-        fn_output = self.lambda_fn(*fn_inp)
-        if self.handler is None:
-            if isinstance(fn_output, dict):
-                self.handler = self._handle_output_dict
-            if isinstance(fn_output, tuple):
-                if len(fn_output) != len(self.output_keys):
-                    raise TypeError(
-                        "Unexpected output. "
-                        "Expect function to return tuple same length as output_keys, "
-                        f"which is {len(self.output_keys)}, "
-                        f"but got output length of {len(fn_output)}"
-                        "Use output_keys argument to specify output keys."
-                    )
-                self.handler = self._handle_output_tuple
-            else:
-                if len(self.output_keys) > 1:
-                    raise TypeError(
-                        "Unexpected output. "
-                        "Expect function to return tuple or dict, but got "
-                        f"{type(fn_output)}. "
-                        "Use output_keys argument to specify output key."
-                    )
-                self.handler = self._handle_output_value
-        runner.batch = self.handler(
+        if self.scope == "on_batch_end":
+            self._handle_batch(runner)
+
+    def _handle_batch(self, runner):
+        fn_input = self.input_handler(runner.batch)
+        fn_output = self.lambda_fn(*fn_input)
+
+        if self.output_handler is None:
+            self.output_handler = self._get_output_handler(fn_output=fn_output)
+
+        runner.batch = self.output_handler(
             batch=runner.batch, function_output=fn_output, output_keys=self.output_keys
         )
+
+    def _get_output_handler(self, fn_output):
+        # First batch case. Executes only ones.
+        # Structure:
+        # if isinstance(fn_output, type):
+        #     if not output_keys match output:
+        #         raise Exception
+        #     handler = _handler_for_type
+        output_handler = None
+        if isinstance(fn_output, dict):
+            if self.output_keys is not None:
+                raise Exception(
+                    "If your function outputs dict you " "should left output_keys=None."
+                )
+            output_handler = self._handle_output_dict
+        else:
+            if self.output_keys is None:
+                raise Exception(
+                    "If function does not output " "dict you should specify `output_keys`"
+                )
+        if isinstance(fn_output, tuple):
+            if len(fn_output) != len(self.output_keys):
+                raise TypeError(
+                    "Unexpected output. "
+                    "Expect function to return tuple same length as output_keys, "
+                    f"which is {len(self.output_keys)}, "
+                    f"but got output length of {len(fn_output)}"
+                    "Use output_keys argument to specify output keys."
+                )
+            output_handler = self._handle_output_tuple
+        if not isinstance(fn_output, (tuple, dict)):
+            if len(self.output_keys) > 1:
+                raise TypeError(
+                    "Unexpected output. "
+                    "Expect function to return tuple, but got "
+                    f"{type(fn_output)}. "
+                    "Use output_keys argument to specify output key."
+                )
+            output_handler = self._handle_output_value
+        return output_handler
 
     @staticmethod
     def _handle_output_tuple(
