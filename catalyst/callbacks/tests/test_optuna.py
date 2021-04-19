e081cedbf9ed18b18e40fb835be0c254db20649a
Sergey Kolesnikov <scitator@gmail.com>
2021-04-19 09:01:23 +0300

    extras github action

--- catalyst/callbacks/tests/test_optuna.py
+++ catalyst/callbacks/tests/test_optuna.py
@@ -6,7 +6,6 @@ from torch import nn
 from torch.utils.data import DataLoader
 
 from catalyst import dl
-from catalyst.callbacks import OptunaPruningCallback
 from catalyst.contrib.datasets import MNIST
 from catalyst.contrib.nn.modules import Flatten
 from catalyst.data.transforms import ToTensor
@@ -15,6 +14,8 @@ from catalyst.settings import SETTINGS
 
 if SETTINGS.optuna_required:
     import optuna
+
+    from catalyst.callbacks import OptunaPruningCallback
 
 
 @pytest.mark.skipif(
