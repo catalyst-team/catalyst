cfe0112c2dde44d621c11923c692c71b20fea7aa
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-04-24 12:07:05 +0200

    added log_artifact to catalyst

--- catalyst/core/logger.py
+++ catalyst/core/logger.py
@@ -68,6 +68,33 @@ class ILogger:
         """Logs hyperparameters to the logger."""
         pass
 
+    def log_artifact(
+        self,
+        tag: str,
+        artifact: object = None,
+        path_to_artifact: str = None,
+        scope: str = None,
+        # experiment info
+        run_key: str = None,
+        global_epoch_step: int = 0,
+        global_batch_step: int = 0,
+        global_sample_step: int = 0,
+        # stage info
+        stage_key: str = None,
+        stage_epoch_len: int = 0,
+        stage_epoch_step: int = 0,
+        stage_batch_step: int = 0,
+        stage_sample_step: int = 0,
+        # loader info
+        loader_key: str = None,
+        loader_batch_len: int = 0,
+        loader_sample_len: int = 0,
+        loader_batch_step: int = 0,
+        loader_sample_step: int = 0,
+    ) -> None:
+        """Logs arbitrary file (audio, video, csv, etc.) to the logger. Log from disk or from memory."""
+        pass
+
     def flush_log(self) -> None:
         """Flushes the logger."""
         pass
