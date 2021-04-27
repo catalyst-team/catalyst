8392e3c70dfb160e456fa2aba906dee5c8d7e0b5
kamil-kaczmarek <kamil.kaczmarek@neptune.ml>
2021-04-27 14:10:40 +0200

    Corrected docstring to be 1-liner.

--- catalyst/core/logger.py
+++ catalyst/core/logger.py
@@ -100,9 +100,7 @@ class ILogger:
         loader_batch_step: int = 0,
         loader_sample_step: int = 0,
     ) -> None:
-        """Logs artifact (arbitrary file like audio, video, csv, etc.)
-        to the logger (from disk or from memory).
-        """
+        """Logs artifact (arbitrary file like audio, video, model weights) to the logger."""
         pass
 
     def flush_log(self) -> None:
