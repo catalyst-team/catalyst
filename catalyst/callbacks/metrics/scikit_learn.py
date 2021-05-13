2e52ee51f0c399faee2a8d1d45e01c6e6801f026
Kseniia Iagafarova <xeniayagafarova@gmail.com>
2021-05-13 15:33:25 +0300

    docstrings

--- catalyst/callbacks/metrics/scikit_learn.py
+++ catalyst/callbacks/metrics/scikit_learn.py
@@ -12,13 +12,77 @@ if SETTINGS.ml_required:
 
 
 class SklearnCallback(FunctionalBatchMetricCallback):
-    """
+    """SklearnCallback implements an integration of **batch-based** Sklearn metrics.
 
     Args:
-        keys:
-        metric_fn:
-        metric_key:
-        log_on_batch:
+        keys: a dictionary containing:
+            a mapping between ``metric_fn`` arguments and keys in ``runner.batch``
+            other arguments needed for ``metric_fn``
+        metric_fn: metric function that gets outputs, targets, and other arguments given
+            in ``keys`` and returns score
+        metric_key: key to store computed metric in ``runner.batch_metrics`` dictionary
+        log_on_batch: boolean flag to log computed metrics every batch
+
+    Examples:
+
+    .. code-block:: python
+
+        import sklearn
+        import torch
+        from torch.utils.data import DataLoader, TensorDataset
+        from catalyst import dl
+
+        # sample data
+        num_samples, num_features, num_classes = int(1e4), int(1e1), 4
+        X = torch.rand(num_samples, num_features)
+        y = (torch.rand(num_samples, num_classes) > 0.5).to(torch.float32)
+
+        # pytorch loaders
+        dataset = TensorDataset(X, y)
+        loader = DataLoader(dataset, batch_size=32, num_workers=1)
+        loaders = {"train": loader, "valid": loader}
+
+        # model, criterion, optimizer, scheduler
+        model = torch.nn.Linear(num_features, num_classes)
+        criterion = torch.nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam(model.parameters())
+        scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, [2])
+
+        # model training
+        runner = dl.SupervisedRunner(
+            input_key="features", output_key="logits", target_key="targets", loss_key="loss"
+        )
+        runner.train(
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            scheduler=scheduler,
+            loaders=loaders,
+            num_epochs=3,
+            verbose=True,
+            callbacks=[
+                dl.BatchTransformCallback(
+                    transform=torch.sigmoid,
+                    scope="on_batch_end",
+                    input_key="logits",
+                    output_key="scores"
+                ),
+                dl.AUCCallback(input_key="logits", target_key="targets"),
+                dl.MultilabelAccuracyCallback(
+                    input_key="logits", target_key="targets", threshold=0.5
+                ),
+                dl.SklearnCallback(
+                    keys={"y_score": "scores", "y_true": "targets"},
+                    metric_fn=sklearn.metrics.coverage_error,
+                    metric_key="coverage_error",
+                )
+            ]
+        )
+
+    .. note::
+        Please follow the `minimal examples`_ sections for more use cases.
+
+        .. _`minimal examples`: https://github.com/catalyst-team/catalyst#minimal-examples
     """
 
     def __init__(
