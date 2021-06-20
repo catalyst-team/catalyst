296d98ce498b6f733a8903a1461deab738c78840
Dokholyan <Dokholyan97@mail.ru>
2021-06-20 19:36:41 +0300

    Update MixUp: fix batch_size, keys, example

--- catalyst/callbacks/mixup.py
+++ catalyst/callbacks/mixup.py
@@ -1,3 +1,5 @@
+from typing import List, Union
+
 import numpy as np
 import torch
 
@@ -5,31 +7,32 @@ from catalyst.core import Callback, CallbackOrder, IRunner
 
 
 class MixupCallback(Callback):
-    """Callback to do mixup augmentation.
-
-    More details about mixin can be found in the paper `mixup: Beyond Empirical Risk Minimization.
+    """
+    Callback to do mixup augmentation. More details about mixin can be found in the paper
+    `mixup: Beyond Empirical Risk Minimization.
 
     Examples:
 
     .. code-block:: python
 
         import os
+        import numpy as np
         import torch
         from torch import nn
         from torch.utils.data import DataLoader
-        from typing import Dict, Any
         from catalyst import dl
         from catalyst.callbacks import MixupCallback
         from catalyst.data.transforms import ToTensor
         from catalyst.contrib.datasets import MNIST
+        from typing import Dict, Any
 
 
         class SimpleNet(nn.Module):
 
             def __init__(self, in_channels, in_hw, out_features):
                 super().__init__()
-                self.encoder = nn.Sequential(nn.Conv2d(in_channels,
-                                                       in_channels, 3, 1, 1), nn.Tanh())
+                self.encoder = nn.Sequential(nn.Conv2d(in_channels, in_channels, 3, 1, 1),
+                                             nn.Tanh())
                 self.clf = nn.Linear(in_channels * in_hw * in_hw, out_features)
 
             def forward(self, x):
@@ -40,8 +43,8 @@ class MixupCallback(Callback):
 
 
         class SimpleDataset(torch.utils.data.Dataset):
-            def __init__(self):
-                self.mnist = MNIST(os.getcwd(), train=False, download=True, transform=ToTensor())
+            def __init__(self, train: bool = False):
+                self.mnist = MNIST(os.getcwd(), train=train, download=True, transform=ToTensor())
 
             def __len__(self) -> int:
                 return len(self.mnist)
@@ -50,7 +53,9 @@ class MixupCallback(Callback):
                 x, y = self.mnist.__getitem__(idx)
                 y_one_hot = np.zeros(10)
                 y_one_hot[y] = 1
-                return {'image': x, 'clf_targets_one_hot': torch.Tensor(y_one_hot)}
+                return {'image': x,
+                        'clf_targets': y,
+                        'clf_targets_one_hot': torch.Tensor(y_one_hot)}
 
 
         model = SimpleNet(1, 28, 10)
@@ -58,8 +63,8 @@ class MixupCallback(Callback):
         optimizer = torch.optim.Adam(model.parameters(), lr=0.02)
 
         loaders = {
-            "train": DataLoader(SimpleDataset(), batch_size=32),
-            "valid": DataLoader(SimpleDataset(), batch_size=32),
+            "train": DataLoader(SimpleDataset(train=True), batch_size=32),
+            "valid": DataLoader(SimpleDataset(train=False), batch_size=32),
         }
 
 
@@ -84,52 +89,55 @@ class MixupCallback(Callback):
             valid_metric="loss",
             minimize_valid_metric=True,
             callbacks={
-                "mixup": MixupCallback(input_key='image', output_key='clf_targets_one_hot'),
+                "mixup": MixupCallback(keys=['image', 'clf_targets_one_hot']),
                 "criterion": dl.CriterionCallback(
                     metric_key="loss",
                     input_key="clf_logits",
                     target_key="clf_targets_one_hot",
                 ),
                 "optimizer": dl.OptimizerCallback(metric_key="loss"),
+                "classification": dl.ControlFlowCallback(
+                    dl.PrecisionRecallF1SupportCallback(input_key="clf_logits",
+                                                        target_key="clf_targets",
+                                                        num_classes=10, ),
+                    ignore_loaders='train',
+                ),
+
             },
         )
-        
+
     .. note::
-        1) Callback can only be used with an even batch size
-        2) With running this callback, many metrics (for example, accuracy) become undefined.
+        Callback can only be used with an even batch size
+        With running this callback, many metrics (for example, accuracy) become undefined, so
+        use ControlFlowCallback in order to evaluate model(see example)
+
     """
 
     def __init__(
-        self,
-        input_key: str = "targets",
-        output_key: str = "logits",
-        alpha=0.2,
-        mode="replace",
-        on_train_only=True,
-        **kwargs,
+        self, keys: Union[str, List[str]], alpha=0.2, mode="replace", on_train_only=True, **kwargs,
     ):
         """
 
         Args:
-            input_key: feature key to which you want to apply augmentation
-            output_key: target key to which you want to apply augmentation
+            keys: batch keys to which you want to apply augmentation
             alpha: beta distribution a=b parameters. Must be >=0. The more alpha closer to zero the
             less effect of the mixup.
             mode: mode determines the method of use. Must be in ["replace", "add"]. If "replace"
-            then replaces the batch with a mixed one, while the batch size is reduced by 2 times.
+            then replaces the batch with a mixed one, while the batch size is not changed
             If "add", concatenates mixed examples to the current ones, the batch size increases by
-            1.5 times.
+            s times.
             on_train_only: apply to train only. As the mixup use the proxy inputs, the targets are
             also proxy. We are not interested in them, are we? So, if on_train_only is True, use a
             standard output/metric for validation.
             **kwargs:
         """
-        assert isinstance(input_key, str) and isinstance(output_key, str)
+        assert isinstance(keys, (str, list)), f"keys must be str of list[str], get: {type(keys)}"
         assert alpha >= 0, "alpha must be>=0"
         assert mode in ["add", "replace"], f"mode must be in 'add', 'replace', get: {mode}"
         super().__init__(order=CallbackOrder.Internal)
-        self.input_key = input_key
-        self.output_key = output_key
+        if isinstance(keys, str):
+            keys = [keys]
+        self.keys = keys
         self.on_train_only = on_train_only
         self.alpha = alpha
         self.mode = mode
@@ -142,38 +150,22 @@ class MixupCallback(Callback):
         Args:
             runner: runner for the experiment.
         """
-        features = runner.batch[self.input_key]
-        targets = runner.batch[self.output_key]
-
-        batch_size = features.shape[0]
-        assert batch_size % 2 == 0
-        beta = np.random.beta(self.alpha, self.alpha, batch_size // 2).astype(np.float32)
-
-        # It is need in order to word with different dimensions
-        features_shape = [batch_size // 2] + [1] * len(features.shape[1:])
-        targets_shape = [batch_size // 2] + [1] * len(targets.shape[1:])
-
-        features_beta = beta.reshape(features_shape)
-        beta = beta.reshape(targets_shape)
-
+        batch_size = runner.batch[self.keys[0]].shape[0]
+        beta = np.random.beta(self.alpha, self.alpha, batch_size).astype(np.float32)
         indexes = np.array(list(range(batch_size)))
-        np.random.shuffle(indexes)
+        # index shift by 1
+        indexes_2 = (indexes + 1) % batch_size
+        for key in self.keys:
+            targets = runner.batch[key]
+            targets_shape = [batch_size] + [1] * len(targets.shape[1:])
+            key_beta = beta.reshape(targets_shape)
+            targets = targets * key_beta + targets[indexes_2] * (1 - key_beta)
 
-        features = features[indexes[: batch_size // 2]] * features_beta + features[
-            indexes[batch_size // 2 :]
-        ] * (1 - features_beta)
-
-        targets = targets[indexes[: batch_size // 2]] * beta + targets[
-            indexes[batch_size // 2 :]
-        ] * (1 - beta)
-
-        if self.mode == "replace":
-            runner.batch[self.input_key] = features
-            runner.batch[self.output_key] = targets
-        else:
-            # self.mode == 'add':
-            runner.batch[self.input_key] = torch.cat([runner.batch[self.input_key], features])
-            runner.batch[self.output_key] = torch.cat([runner.batch[self.output_key], targets])
+            if self.mode == "replace":
+                runner.batch[key] = targets
+            else:
+                # self.mode == 'add'
+                runner.batch[key] = torch.cat([runner.batch[key], targets])
 
     def on_loader_start(self, runner: "IRunner") -> None:
         """
