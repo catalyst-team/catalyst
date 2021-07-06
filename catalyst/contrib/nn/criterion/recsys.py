b8792564ee4ea1330aeca6df835038aea16d2e00
Philipp <casyfill@gmail.com>
2021-07-06 13:50:29 -0400

    catalyst-make-codestyle

--- catalyst/contrib/nn/criterion/recsys.py
+++ catalyst/contrib/nn/criterion/recsys.py
@@ -12,15 +12,17 @@ class Pointwise(nn.Module):
     Input space: single document d1
     Output space: scores or relevant classes
     """
+
     def __init__(self) -> None:
         super().__init__()
-    
-    def forward(self, score:torch.Tensor):
+
+    def forward(self, score: torch.Tensor):
         pass
+
 
 class PairwiseLoss(nn.Module):
     """
-    Pairwise approached looks at a pair ofd documents at a time in the loss function.
+    Pairwise approached looks at a pair of documents at a time in the loss function.
     Given a pair of documents the algorithm try to come up with the optimal ordering 
     For that pair and compare it with the ground truth. The goal for the ranker is to 
     minimize the number of inversions in ranker. 
@@ -28,11 +30,13 @@ class PairwiseLoss(nn.Module):
     Input space: pairs of documents (d1, d2)
     Output space: preferences (yes/no) for a given doc.pair
     """
+
     def __init__(self) -> None:
         super().__init__()
-    
-    def forward(self, positive_score:torch.Tensor, negative_score:torch.Tensor):
+
+    def forward(self, positive_score: torch.Tensor, negative_score: torch.Tensor):
         pass
+
 
 class ListWiseLoss(nn.Module):
     """
@@ -42,10 +46,11 @@ class ListWiseLoss(nn.Module):
     Input space: document set
     Output space: permutations - ranking of documents 
     """
+
     def __init__(self) -> None:
         super().__init__()
-    
-    def forward(self, score_list:torch.Tensor):
+
+    def forward(self, score_list: torch.Tensor):
         pass
 
 
@@ -68,11 +73,12 @@ class BPRLoss(PairwiseLoss):
         output = recsys.BPRLoss()(pos_score, neg_score)
         output.backward()
     """
+
     def __init__(self, gamma=1e-10) -> None:
         super().__init__()
         self.gamma = gamma
-    
-    def forward(self, positive_score:torch.Tensor, negative_score:torch.Tensor)->torch.Tensor:
+
+    def forward(self, positive_score: torch.Tensor, negative_score: torch.Tensor) -> torch.Tensor:
         """
         Args
             positive_predictions: torch.LongTensor
@@ -92,10 +98,11 @@ class WARPLoss(PairwiseLoss):
      WARP loss randomly sample output labels of a model, until it finds a pair which it knows are wrongly labelled 
      and will then only apply an update to these two incorrectly labelled examples.
     """
+
     def __init__(self) -> None:
         super().__init__()
-    
-    def forward(self, positive_score:torch.Tensor, negative_score:torch.Tensor)->torch.Tensor:
+
+    def forward(self, positive_score: torch.Tensor, negative_score: torch.Tensor) -> torch.Tensor:
         """
         Args
             positive_predictions: torch.LongTensor
@@ -103,24 +110,22 @@ class WARPLoss(PairwiseLoss):
             negative_predictions: torch.LongTensor
                 Tensor containing predictions for sampled negative items.
         """
-        
+
         highest_negative_score, _ = torch.max(negative_score, 0).squezee()
 
-        loss = torch.clamp(highest_negative_score -
-                       positive_score +
-                       1.0, 0.0)
+        loss = torch.clamp(highest_negative_score - positive_score + 1.0, 0.0)
 
         return loss.mean()
 
 
-class LogisticLoss(Pointwise):
+class LogisticLoss(PairwiseLoss):
     """
     Logistic loss
     """
 
-    def __init__(self, ) -> None:
+    def __init__(self,) -> None:
         super().__init__()
-    
+
     def forward(self, positive_score, negative_score):
         """
         Args
@@ -129,10 +134,10 @@ class LogisticLoss(Pointwise):
             negative_predictions: torch.LongTensor
                 Tensor containing predictions for sampled negative items.
         """
-        
-        positives_loss = (1.0 - torch.sigmoid(positive_score))
+
+        positives_loss = 1.0 - torch.sigmoid(positive_score)
         negatives_loss = torch.sigmoid(negative_score)
-        
-        loss = (positives_loss + negatives_loss)
+
+        loss = positives_loss + negatives_loss
 
         return loss.mean()
--- tests/catalyst/contrib/nn/test_criterion.py
+++ tests/catalyst/contrib/nn/test_criterion.py
@@ -1,9 +1,11 @@
 # flake8: noqa
 import pytest
+import torch
+
 from catalyst.contrib.nn import criterion as module
 from catalyst.contrib.nn.criterion import CircleLoss, TripletMarginLossWithSampler
 from catalyst.data import AllTripletsSampler
-import torch
+
 
 def test_criterion_init():
     """@TODO: Docs. Contribution is welcome."""
@@ -26,18 +28,23 @@ def test_criterion_init():
 def test_BPRLoss():
     """Testing for Bayesian Personalized Ranking"""
     from catalyst.contrib.nn.criterion.recsys import BPRLoss
+
     loss = BPRLoss()
 
     rand = torch.rand(1000, dtype=torch.float)
-    assert float(loss.forward(rand, rand)) == pytest.approx(0.6931, 0.001) # log of 0.5
+    assert float(loss.forward(rand, rand)) == pytest.approx(0.6931, 0.001)  # log of 0.5
 
     pos, neg = torch.Tensor([1000, 1000, 1000, 1000,]), torch.Tensor([0, 0, 0, 0,])
-    assert float(loss.forward(pos, neg)) == pytest.approx(0, 0.001) # log of 1
+    assert float(loss.forward(pos, neg)) == pytest.approx(0, 0.001)  # log of 1
 
     neg, pos = torch.Tensor([1000, 1000, 1000, 1000,]), torch.Tensor([0, 0, 0, 0,])
     log_gamma = float(torch.log(torch.Tensor([loss.gamma,])))
-    assert float(loss.forward(pos, neg)) == pytest.approx(-log_gamma, 0.001) # log of 0 with gamma
+    assert float(loss.forward(pos, neg)) == pytest.approx(-log_gamma, 0.001)  # log of 0 with gamma
 
 
 def test_WARPLoss():
+    pass
+
+
+def testLogisticLoss():
     pass
