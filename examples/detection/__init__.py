52a7e0f34a338d6df1611a41a569cdb8e41de65e
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2021-07-29 22:00:33 +0300

    feature: ssd training with catalyst

--- examples/detection/__init__.py
+++ examples/detection/__init__.py
@@ -1,11 +1,18 @@
+import warnings
+
+warnings.filterwarnings("ignore", category=RuntimeWarning)
+
+
 from catalyst.registry import Registry
 
 from .callbacks import DetectionMeanAveragePrecision
 from .criterion import SSDCriterion
 from .custom_runner import SSDDetectionRunner
+from .dataset import DetectionDataset
 from .model import SingleShotDetector
 
 Registry(SSDDetectionRunner)
 Registry(SingleShotDetector)
 Registry(SSDCriterion)
 Registry(DetectionMeanAveragePrecision)
+Registry(DetectionDataset)
