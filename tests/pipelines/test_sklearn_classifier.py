4674f58860ca5fcda9f7b98ce40b72089b866768
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2021-08-16 20:26:21 -0500

    compare only best accuracy

--- tests/pipelines/test_sklearn_classifier.py
+++ tests/pipelines/test_sklearn_classifier.py
@@ -1,9 +1,12 @@
 # flake8: noqa
+import csv
 from functools import partial
+from logging import log
 import os
+from pathlib import Path
+from tempfile import TemporaryDirectory
 
 from pytest import mark
-import torch
 from torch.optim import Adam
 from torch.utils.data import DataLoader
 
@@ -15,58 +18,63 @@ from catalyst.settings import SETTINGS
 if SETTINGS.ml_required:
     from sklearn.ensemble import RandomForestClassifier
 
-TRAIN_EPOCH = 10
+TRAIN_EPOCH = 30
 LR = 0.01
 RANDOM_STATE = 42
 
 
+def read_csv(csv_path: str):
+    with open(csv_path, "r") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        for line_count, row in enumerate(csv_reader):
+            if line_count == 0:
+                colnames = row
+            else:
+                yield {colname: val for colname, val in zip(colnames, row)}
+
+
 def train_experiment(device, engine=None):
-    from catalyst import utils
+    with TemporaryDirectory() as logdir:
+        from catalyst import utils
 
-    utils.set_global_seed(RANDOM_STATE)
-    # 1. train, valid and test loaders
-    transforms = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
-    train_dataset = datasets.MNIST(
-        root=os.getcwd(), transform=transforms, train=False, download=True
-    )
-    train_loader = DataLoader(dataset=train_dataset, batch_size=512, shuffle=True)
+        utils.set_global_seed(RANDOM_STATE)
+        # 1. train, valid and test loaders
+        transforms = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
+        train_dataset = datasets.MNIST(
+            root=os.getcwd(), transform=transforms, train=False, download=True
+        )
+        train_loader = DataLoader(dataset=train_dataset, batch_size=512, shuffle=True)
 
-    valid_dataset = datasets.MNIST(
-        root=os.getcwd(), transform=transforms, train=False, download=True
-    )
-    valid_loader = DataLoader(dataset=valid_dataset, batch_size=128)
+        valid_dataset = datasets.MNIST(
+            root=os.getcwd(), transform=transforms, train=False, download=True
+        )
+        valid_loader = DataLoader(dataset=valid_dataset, batch_size=128)
 
-    # 2. model and optimizer
-    model = models.MnistSimpleNet(out_features=16, normalize=True)
-    optimizer = Adam(model.parameters(), lr=LR)
+        # 2. model and optimizer
+        model = models.MnistSimpleNet(out_features=16, normalize=True)
+        optimizer = Adam(model.parameters(), lr=LR)
 
-    # 3. criterion with triplets sampling
-    sampler_inbatch = data.HardTripletsSampler(norm_required=False)
-    criterion = nn.TripletMarginLossWithSampler(margin=0.5, sampler_inbatch=sampler_inbatch)
+        # 3. criterion with triplets sampling
+        sampler_inbatch = data.HardTripletsSampler(norm_required=False)
+        criterion = nn.TripletMarginLossWithSampler(margin=0.5, sampler_inbatch=sampler_inbatch)
 
-    # 4. training with catalyst Runner
-    class CustomRunner(dl.SupervisedRunner):
-        def handle_batch(self, batch) -> None:
-            images, targets = batch["features"].float(), batch["targets"].long()
-            features = self.model(images)
-            self.batch = {
-                "embeddings": features,
-                "targets": targets,
-            }
+        # 4. training with catalyst Runner
+        class CustomRunner(dl.SupervisedRunner):
+            def handle_batch(self, batch) -> None:
+                images, targets = batch["features"].float(), batch["targets"].long()
+                features = self.model(images)
+                self.batch = {
+                    "embeddings": features,
+                    "targets": targets,
+                }
 
-    callbacks = [
-        dl.PeriodicLoaderCallback(
-            valid_loader_key="train",
-            valid_metric_key="loss",
-            minimize=True,
-            valid=TRAIN_EPOCH + 1,
-            infer=TRAIN_EPOCH + 1,
-        ),
-        dl.ControlFlowCallback(
-            dl.CriterionCallback(input_key="embeddings", target_key="targets", metric_key="loss"),
-            loaders="train",
-        ),
-        dl.ControlFlowCallback(
+        callbacks = [
+            dl.ControlFlowCallback(
+                dl.CriterionCallback(
+                    input_key="embeddings", target_key="targets", metric_key="loss"
+                ),
+                loaders="train",
+            ),
             dl.SklearnModelCallback(
                 feature_key="embeddings",
                 target_key="targets",
@@ -76,34 +84,36 @@ def train_experiment(device, engine=None):
                 predict_method="predict_proba",
                 predict_key="sklearn_predict",
                 random_state=RANDOM_STATE,
-                n_estimators=200,
+                n_estimators=500,
             ),
-            filter_fn=lambda s, e, l: e > TRAIN_EPOCH,
-        ),
-        dl.ControlFlowCallback(
-            dl.AccuracyCallback(
-                target_key="targets", input_key="sklearn_predict", topk_args=(1, 3)
+            dl.ControlFlowCallback(
+                dl.AccuracyCallback(
+                    target_key="targets", input_key="sklearn_predict", topk_args=(1, 3)
+                ),
+                loaders="valid",
             ),
-            filter_fn=lambda s, e, l: l == "valid" and e > TRAIN_EPOCH,
-        ),
-    ]
+        ]
 
-    runner = CustomRunner(input_key="features", output_key="embeddings")
-    runner.train(
-        engine=engine or dl.DeviceEngine(device),
-        model=model,
-        criterion=criterion,
-        optimizer=optimizer,
-        callbacks=callbacks,
-        loaders={"train": train_loader, "valid": valid_loader},
-        verbose=False,
-        valid_loader="train",
-        valid_metric="loss",
-        minimize_valid_metric=True,
-        num_epochs=TRAIN_EPOCH + 1,
-    )
+        runner = CustomRunner(input_key="features", output_key="embeddings")
+        runner.train(
+            engine=engine or dl.DeviceEngine(device),
+            model=model,
+            criterion=criterion,
+            optimizer=optimizer,
+            callbacks=callbacks,
+            loaders={"train": train_loader, "valid": valid_loader},
+            verbose=False,
+            valid_loader="valid",
+            valid_metric="accuracy",
+            minimize_valid_metric=False,
+            num_epochs=TRAIN_EPOCH,
+            logdir=logdir,
+        )
 
-    assert runner.loader_metrics["accuracy"].item() > 0.5
+        valid_path = Path(logdir) / "logs/valid.csv"
+        best_accuracy = max(float(row["accuracy"]) for row in read_csv(valid_path))
+
+        assert best_accuracy > 0.5
 
 
 @mark.skipif(not SETTINGS.ml_required, reason="catalyst[ml] required")
