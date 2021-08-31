13245353da8949f5b4305073b7c77176c522ba10
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2021-08-30 22:40:43 -0500

    draft IContrastiveRunner

--- catalyst/runners/contrastive.py
+++ catalyst/runners/contrastive.py
@@ -1,4 +1,4 @@
-from typing import Any
+from typing import Any, Mapping
 
 from catalyst.core.runner import IRunner
 from catalyst.runners.runner import Runner
@@ -43,6 +43,7 @@ class IContrastiveRunner(IRunner):
         output_key: Any = "logits",
         target_key: str = "targets",
         loss_key: str = "loss",
+        augemention_key: str = "aug",
         projection_key: str = "projections",
         embedding_key: str = "embeddings",
     ):
@@ -54,7 +55,48 @@ class IContrastiveRunner(IRunner):
         self._target_key = target_key
         self._loss_key = loss_key
         self._projection_key = projection_key
+        self._augemention_key = augemention_key
         self._embedding_key = embedding_key
+
+    def _process_batch(self, batch):
+        if isinstance(batch, (tuple, list)):
+            assert len(batch) == 3
+            batch = {
+                f"{self._augemention_key}_1": batch[0],
+                f"{self._augemention_key}_2": batch[1],
+                self._target_key: batch[2],
+            }
+        return batch
+
+    def _process_input(self, batch: Mapping[str, Any], **kwargs):
+        embedding1, projection1 = self.model(batch[f"{self._augemention_key}_1"], **kwargs)
+        embedding2, projection2 = self.model(batch[f"{self._augemention_key}_2"], **kwargs)
+        
+        batch = {
+            **batch,
+            f"{self._projection_key}_1": projection1,
+            f"{self._projection_key}_2": projection2,
+            f"{self._embedding_key}_1": embedding1,
+            f"{self._embedding_key}_2": embedding2,
+        }
+        return batch
+    
+    def forward(self, batch: Mapping[str, Any], **kwargs) -> Mapping[str, Any]:
+        """
+        Forward method for your Runner.
+        Should not be called directly outside of runner.
+        If your model has specific interface, override this method to use it
+
+        Args:
+            batch (Mapping[str, Any]): dictionary with data batches
+                from DataLoaders.
+            **kwargs: additional parameters to pass to the model
+
+        Returns:
+            dict with model output batch
+        """
+        return self._process_input(batch, **kwargs)
+    
 
 
 class ContrastiveRunner(IContrastiveRunner, Runner):
@@ -67,10 +109,7 @@ class ContrastiveRunner(IContrastiveRunner, Runner):
         projection1 = self.model(embedding1)
         projection2 = self.model(embedding2)
         self.batch = {
-            f"{self._projection_key}_1": projection1,
-            f"{self._projection_key}_2": projection2,
-            f"{self._embedding_key}_1": embedding1,
-            f"{self._embedding_key}_2": embedding2,
+            
             self._target_key: target,
         }
 
