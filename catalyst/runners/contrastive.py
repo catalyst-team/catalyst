fe17f8d6fbac5c2ab6634c1001e95701fd11389c
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2021-08-30 22:59:00 -0500

    fix CriterionCallback keys

--- catalyst/runners/contrastive.py
+++ catalyst/runners/contrastive.py
@@ -1,12 +1,3 @@
-import torch
-from typing import Any, Mapping
-
-from catalyst.core.runner import IRunner
-from catalyst.runners.runner import Runner
-from catalyst.typing import RunnerModel
-from catalyst.core import Callback, IEngine
-
-
 from typing import Any, Dict, Generator, Iterable, List, Mapping, Optional, Union
 from collections import OrderedDict
 import os
@@ -18,10 +9,12 @@ from torch.optim.lr_scheduler import ReduceLROnPlateau
 from catalyst.callbacks.criterion import CriterionCallback, ICriterionCallback
 from catalyst.callbacks.optimizer import IOptimizerCallback, OptimizerCallback
 from catalyst.callbacks.scheduler import ISchedulerCallback, SchedulerCallback
+from catalyst.core import Callback, IEngine
 from catalyst.core.callback import Callback
 from catalyst.core.misc import callback_isinstance, sort_callbacks_by_order
 from catalyst.core.runner import IRunner, RunnerException
 from catalyst.engines import IEngine
+from catalyst.runners.runner import Runner
 from catalyst.typing import (
     Criterion,
     Model,
@@ -99,7 +92,7 @@ class IContrastiveRunner(IRunner):
     def _process_input(self, batch: Mapping[str, Any], **kwargs):
         embedding1, projection1 = self.model(batch[f"{self._augemention_key}_1"], **kwargs)
         embedding2, projection2 = self.model(batch[f"{self._augemention_key}_2"], **kwargs)
-        
+
         batch = {
             **batch,
             f"{self._projection_key}_1": projection1,
@@ -108,7 +101,7 @@ class IContrastiveRunner(IRunner):
             f"{self._embedding_key}_2": embedding2,
         }
         return batch
-    
+
     def forward(self, batch: Mapping[str, Any], **kwargs) -> Mapping[str, Any]:
         """
         Forward method for your Runner.
@@ -124,7 +117,6 @@ class IContrastiveRunner(IRunner):
             dict with model output batch
         """
         return self._process_input(batch, **kwargs)
-    
 
 
 class ContrastiveRunner(IContrastiveRunner, Runner):
@@ -143,11 +135,11 @@ class ContrastiveRunner(IContrastiveRunner, Runner):
         """Init."""
         IContrastiveRunner.__init__(
             self,
-            target_key = target_key,
-            loss_key  = loss_key,
-            augemention_key  = augemention_key,
-            projection_key  = projection_key,
-            embedding_key = embedding_key,
+            target_key=target_key,
+            loss_key=loss_key,
+            augemention_key=augemention_key,
+            projection_key=projection_key,
+            embedding_key=embedding_key,
         )
         Runner.__init__(self, model=model, engine=engine)
 
@@ -181,14 +173,16 @@ class ContrastiveRunner(IContrastiveRunner, Runner):
         Returns:
             dictionary with stage callbacks
         """
-        # I took it from supervised runner should be remade 
+        # I took it from supervised runner should be remade
         callbacks = super().get_callbacks(stage=stage)
         is_callback_exists = lambda callback_fn: any(
             callback_isinstance(x, callback_fn) for x in callbacks.values()
         )
         if isinstance(self._criterion, Criterion) and not is_callback_exists(ICriterionCallback):
             callbacks["_criterion"] = CriterionCallback(
-                input_key=self._output_key, target_key=self._target_key, metric_key=self._loss_key,
+                input_key=f"{self._projection_key}_1",
+                target_key=f"{self._projection_key}_2",
+                metric_key=self._loss_key,
             )
         if isinstance(self._optimizer, Optimizer) and not is_callback_exists(IOptimizerCallback):
             callbacks["_optimizer"] = OptimizerCallback(metric_key=self._loss_key)
