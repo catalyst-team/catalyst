7726a33bca08665b37dcb4f5136c73f924b9eb06
Nimrais <nikola.lukashuk@gmail.com>
2021-09-05 20:51:02 -0500

    fixed double imports

--- catalyst/runners/__init__.py
+++ catalyst/runners/__init__.py
@@ -4,7 +4,6 @@
 from catalyst.runners.supervised import ISupervisedRunner
 from catalyst.runners.contrastive import ContrastiveRunner
 from catalyst.runners.runner import Runner, SupervisedRunner
-from catalyst.runners.contrastive import ContrastiveRunner
 from catalyst.runners.config import ConfigRunner, SupervisedConfigRunner
 
 from catalyst.settings import SETTINGS
