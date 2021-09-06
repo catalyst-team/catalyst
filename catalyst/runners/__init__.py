10a36e42f19a12308423e75411cc38dc62f87beb
Nimrais <nikola.lukashuk@gmail.com>
2021-09-05 20:52:12 -0500

    added ContrastiveRunner if hydra_required

--- catalyst/runners/__init__.py
+++ catalyst/runners/__init__.py
@@ -16,6 +16,7 @@ if SETTINGS.hydra_required:
         "Runner",
         "ISupervisedRunner",
         "SupervisedRunner",
+        "ContrastiveRunner", 
         "ConfigRunner",
         "SupervisedConfigRunner",
         "HydraRunner",
