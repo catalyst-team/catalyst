5a23e63ce21a88ac90ae3be2f121fcb934b9ec2e
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2021-09-20 10:36:45 +0300

    ci: `UploadCommand` replaced with `build-pypi` workflow (#1299)

--- setup.py
+++ setup.py
@@ -2,15 +2,9 @@
 # flake8: noqa
 # -*- coding: utf-8 -*-
 
-# Note: To use the "upload" functionality of this file, you must:
-#   $ pip install twine
-
-import io
 import os
-from shutil import rmtree
-import sys
 
-from setuptools import Command, find_packages, setup
+from setuptools import find_packages, setup
 
 # Package meta-data.
 NAME = "catalyst"
@@ -32,7 +26,7 @@ def load_requirements(filename):
 def load_readme():
     """Docs? Contribution is welcome."""
     readme_path = os.path.join(PROJECT_ROOT, "README.md")
-    with io.open(readme_path, encoding="utf-8") as f:
+    with open(readme_path, encoding="utf-8") as f:
         return f"\n{f.read()}"
 
 
@@ -42,46 +36,6 @@ def load_version():
     with open(os.path.join(PROJECT_ROOT, "catalyst", "__version__.py")) as f:
         exec(f.read(), context)
     return context["__version__"]
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = "Build and publish the package."
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print("\033[1m{0}\033[0m".format(s))
-
-    def initialize_options(self):
-        """Docs? Contribution is welcome."""
-        pass
-
-    def finalize_options(self):
-        """Docs? Contribution is welcome."""
-        pass
-
-    def run(self):
-        """Docs? Contribution is welcome."""
-        try:
-            self.status("Removing previous builds…")
-            rmtree(os.path.join(PROJECT_ROOT, "dist"))
-        except OSError:
-            pass
-
-        self.status("Building Source and Wheel (universal) distribution…")
-        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
-
-        self.status("Uploading the package to PyPI via Twine…")
-        os.system("twine upload dist/*")
-
-        self.status("Pushing git tags…")
-        os.system("git tag v{0}".format(load_version()))
-        os.system("git push --tags")
-
-        sys.exit()
 
 
 # Specific dependencies.
@@ -173,6 +127,4 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
-    # $ setup.py publish support.
-    cmdclass={"upload": UploadCommand},
 )
