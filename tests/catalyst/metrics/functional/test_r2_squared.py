3dabd39a3c1b0be40394b18f058943cf3c1c3cdd
asteyo <aleksandrdanilov2077@gmail.com>
2021-09-20 11:31:09 +0300

    args order fix

--- tests/catalyst/metrics/functional/test_r2_squared.py
+++ tests/catalyst/metrics/functional/test_r2_squared.py
@@ -9,7 +9,7 @@ def test_r2_squared():
     """
   Tests for catalyst.metrics.r2_squared metric.
   """
-    y_true = torch.tensor([2.5, 0.0, 2, 8])
-    y_pred = torch.tensor([3, -0.5, 2, 7])
-    val = r2_squared(y_true, y_pred)
+    y_true = torch.tensor([3, -0.5, 2, 7])
+    y_pred = torch.tensor([2.5, 0.0, 2, 8])
+    val = r2_squared(y_pred, y_true)
     assert torch.isclose(val, torch.Tensor([0.9486]))
