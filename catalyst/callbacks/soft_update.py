7482207487fc7d3f09db77d860e93e931618ad9b
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2021-10-04 00:58:50 -0500

    feat: scope for SoftUpdateCallaback

--- catalyst/callbacks/soft_update.py
+++ catalyst/callbacks/soft_update.py
@@ -18,20 +18,31 @@ class SoftUpdateCallaback(Callback):
         target_model_key: key to the data inside `runner.model` to update
         source_model_key: key to the source data inside `runner.model`
         tau: smoothing parameter `target * (1.0 - tau) + source * tau`
+        scope (str): when the `target` should be updated
+                ``"on_batch_end"`` 
+                ``"on_batch_start"``
+                ``"on_epoch_end"``
+                ``"on_epoch_start"``
     """
 
-    def __init__(self, target_model_key: str, source_model_key: str, tau: float) -> None:
+    def __init__(self, target_model_key: str, source_model_key: str, tau: float, scope: str) -> None:
         """Init.
         
         Args:
-            target_model_key: key to the data inside `runner.model` to update
-            source_model_key: key to the source data inside `runner.model`
-            tau: smoothing parameter `target * (1.0 - tau) + source * tau`
+        target_model_key: key to the data inside `runner.model` to update
+        source_model_key: key to the source data inside `runner.model`
+        tau: smoothing parameter `target * (1.0 - tau) + source * tau`
+        scope (str): when the `target` should be updated
+                ``"on_batch_end"`` 
+                ``"on_batch_start"``
+                ``"on_epoch_end"``
+                ``"on_epoch_start"``
         """
         super().__init__(order=CallbackOrder.Metric)
         self.target_model_key = target_model_key
         self.source_model_key = source_model_key
         self.tau = tau
+        self.scope = scope
 
     def on_batch_end(self, runner: "IRunner") -> None:
         """On batch end action.
@@ -39,9 +50,43 @@ class SoftUpdateCallaback(Callback):
         Args:
             runner: runner for the experiment.
         """
-        soft_update(
-            runner.model[self.target_model_key], runner.model[self.source_model_key], self.tau
-        )
+        if runner.is_train_loader and self.scope == "on_batch_end":
+            soft_update(
+                runner.model[self.target_model_key], runner.model[self.source_model_key], self.tau
+            )
+    
+    def on_batch_start(self, runner: "IRunner") -> None:
+        """On batch start action.
+
+        Args:
+            runner: runner for the experiment.
+        """
+        if runner.is_train_loader and self.scope == "on_batch_start":
+            soft_update(
+                runner.model[self.target_model_key], runner.model[self.source_model_key], self.tau
+            )
+    
+    def on_epoch_end(self, runner: "IRunner") -> None:
+        """On epoch end action.
+
+        Args:
+            runner: runner for the experiment.
+        """
+        if runner.is_train_loader and self.scope == "on_epoch_end":
+            soft_update(
+                runner.model[self.target_model_key], runner.model[self.source_model_key], self.tau
+            )
+
+    def on_epoch_start(self, runner: "IRunner") -> None:
+        """On epoch start action.
+
+        Args:
+            runner: runner for the experiment.
+        """
+        if runner.is_train_loader and self.scope == "on_epoch_start":
+            soft_update(
+                runner.model[self.target_model_key], runner.model[self.source_model_key], self.tau
+            )
 
 
 __all__ = ["SoftUpdateCallaback"]
