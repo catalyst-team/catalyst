a2289e87980a437f5cd1025f22143bf9f0cbb24a
Mykola Lukashchuk <nikola.lukashuk@gmail.com>
2021-10-05 20:41:27 -0500

    refactor: simCLR

--- examples/self_supervised/simCLR.py
+++ examples/self_supervised/simCLR.py
@@ -1,7 +1,7 @@
 # flake8: noqa
 import argparse
 
-from common import add_arguments, ContrastiveModel, datasets
+from common import add_arguments, ContrastiveModel, get_loaders, get_contrastive_model
 
 import torch
 import torch.nn.functional as F
@@ -18,45 +18,21 @@ from catalyst.dl import SelfSupervisedRunner
 parser = argparse.ArgumentParser(description="Train SimCLR on cifar-10")
 add_arguments(parser)
 
-parser.add_argument("--aug-strength", default=1.0, type=float, help="Strength of augmentations")
-
 if __name__ == "__main__":
     args = parser.parse_args()
     batch_size = args.batch_size
     aug_strength = args.aug_strength
-
-    transforms = datasets[args.dataset]["train_transform"]
-    transform_original = datasets[args.dataset]["valid_transform"]
-
-    train_data = SelfSupervisedDatasetWrapper(
-        datasets[args.dataset]["dataset"](root="data", train=True, transform=None, download=True),
-        transforms=transforms,
-        transform_original=transform_original,
-    )
-    train_loader = torch.utils.data.DataLoader(
-        train_data, batch_size=batch_size, num_workers=args.num_workers
-    )
-
-    encoder = nn.Sequential(ResnetEncoder(arch="resnet50", frozen=False), nn.Flatten())
-    projection_head = nn.Sequential(
-        nn.Linear(2048, 512, bias=False),
-        nn.ReLU(inplace=True),
-        nn.Linear(512, args.feature_dim, bias=True),
-    )
-
-    model = ContrastiveModel(projection_head, encoder)
+    
     # 2. model and optimizer
+    model = get_contrastive_model(args)
     optimizer = Adam(model.parameters(), lr=args.learning_rate)
 
-    # 3. criterion with triplets sampling
+    # 3. criterion
     criterion = NTXentLoss(tau=args.temperature)
 
     callbacks = [
-        dl.ControlFlowCallback(
-            dl.CriterionCallback(
-                input_key="projection_left", target_key="projection_right", metric_key="loss"
-            ),
-            loaders="train",
+        dl.CriterionCallback(
+            input_key="projection_left", target_key="projection_right", metric_key="loss"
         )
     ]
 
@@ -67,10 +43,7 @@ if __name__ == "__main__":
         criterion=criterion,
         optimizer=optimizer,
         callbacks=callbacks,
-        loaders={
-            "train": train_loader,
-            # "valid": valid_loader
-        },
+        loaders=get_loaders(args),
         verbose=True,
         logdir=args.logdir,
         valid_loader="train",
