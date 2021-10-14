6ce3846e810ee4e0509d78d490213b3b23ae6969
Dmytro Doroshenko <dimdoroshenko@gmail.com>
2021-10-14 19:40:02 +0300

    refactoring: ignoring flake8 inside __init__

--- examples/detection/models/__init__.py
+++ examples/detection/models/__init__.py
@@ -1,3 +1,11 @@
-from .centernet import CenterNet  # noqa: F401
-from .ssd import SingleShotDetector  # noqa: F401
-from .yolo_x import yolo_x_tiny, yolo_x_small, yolo_x_medium, yolo_x_large, yolo_x_big  # noqa: F401
+# flake8: noqa
+
+from .centernet import CenterNet
+from .ssd import SingleShotDetector
+from .yolo_x import (
+    yolo_x_tiny,
+    yolo_x_small,
+    yolo_x_medium,
+    yolo_x_large,
+    yolo_x_big,
+)
