14ecc4e28fa3dd7b78a555bb13ee3a4aa255f4eb
Kseniia Iagafarova <xeniayagafarova@gmail.com>
2021-11-07 02:34:26 +0300

    fixed warning filter

--- catalyst/__init__.py
+++ catalyst/__init__.py
@@ -3,7 +3,7 @@ import warnings
 
 warnings.filterwarnings("ignore", message="numpy.dtype size changed", append=True)
 warnings.filterwarnings("ignore", module="tqdm", append=True)
-warnings.filterwarnings("ignore", module="tqdm", append=True)
+warnings.filterwarnings("once", append=True)
 warnings.filterwarnings("ignore", message="This overload of add_ is deprecated", append=True)
 
 from catalyst.__version__ import __version__
