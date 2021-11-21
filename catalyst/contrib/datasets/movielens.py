7aa388cecaff10350422b1db33d7b3a926b66ba7
Dany Chepenko <daniel.chepenko@yahoo.com>
2021-11-21 19:09:35 +0000

    codestyle fix

--- catalyst/contrib/datasets/movielens.py
+++ catalyst/contrib/datasets/movielens.py
@@ -282,18 +282,20 @@ class MovieLens20M(Dataset):
     This dataset was generated on October 17, 2016.
 
     Neither the University of Minnesota nor any of the researchers involved can guarantee the correctness of the data,
-    its suitability for any particular purpose, or the validity of results based on the use of the data set. 
-    
+    its suitability for any particular purpose, or the validity of results based on the use of the data set.
+
     The data set may be used for any research purposes under the following conditions:
-    * The user may not state or imply any endorsement from the University of Minnesota 
+    * The user may not state or imply any endorsement from the University of Minnesota
     or the GroupLens Research Group.
     * The user must acknowledge the use of the data set in publications resulting
     from the use of the data set (see below for citation information).
     * The user may not redistribute the data without separate permission.
     * The user may not use this information for any commercial or revenue-bearing purposes
-    without first obtaining permission from a faculty member of the GroupLens Research Project at the University of Minnesota.
+    without first obtaining permission from a faculty member of the GroupLens Research Project
+    at the University of Minnesota.
     * The executable software scripts are provided "as is" without warranty of any kind,
-    either expressed or implied, including, but not limited to, the implied warranties of merchantability and fitness for a particular purpose.
+    either expressed or implied, including, but not limited to,
+    the implied warranties of merchantability and fitness for a particular purpose.
     The entire risk as to the quality and performance of them is with you. Should the program prove defective,
     you assume the cost of all necessary servicing, repair or correction.
     * In no event shall the University of Minnesota, its affiliates or employees be
@@ -311,7 +313,7 @@ class MovieLens20M(Dataset):
 
     Ratings Data File Structure (ratings.csv)
     All ratings are contained in the file ratings.csv.
-    Each line of this file after the header row represents one rating of one movie by one user, 
+    Each line of this file after the header row represents one rating of one movie by one user,
     and has the following format:
     * userId,
     * movieId,
@@ -394,7 +396,7 @@ class MovieLens20M(Dataset):
                 If true the `n_rows` shold be provide
             n_rowns (int, optional): number of rows to retrieve.
                 Availbale only with `sample = True`
-        
+
         Raises:
             RuntimeError: If ``download = False`` and the dataset not found.
         """
