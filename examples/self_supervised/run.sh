b6b821538138812a8a56dc99faccb8d9fdad0f41
Sergey Kolesnikov <scitator@gmail.com>
2021-11-29 09:27:40 +0200

    SSL example update

--- examples/self_supervised/run.sh
+++ examples/self_supervised/run.sh
@@ -3,18 +3,20 @@
 # pip install catalyst[cv]==21.11
 # pip install catalyst[ml]==21.11
 
-export ARCH="resnet18"
 export NUM_EPOCHS=1
 export BATCH_SIZE=32
 export LEARNING_RATE=0.001
 
 for DATASET in "CIFAR-10" "CIFAR-100" "STL10"; do
 	for METHOD in "barlow_twins" "byol" "simCLR" "supervised_contrastive"; do
-		python $METHOD.py --dataset $DATASET --verbose \
-			--arch=$ARCH \
-			--logdir="./logs/$DATASET/$METHOD" \
-			--batch-size=$BATCH_SIZE \
-			--epochs=$NUM_EPOCHS \
-			--learning-rate=$LEARNING_RATE
+		for ARCH in "resnet18" "resnet34" "resnet50" "resnet101" "resnet152"; do
+			python $METHOD.py \
+				--dataset $DATASET \
+				--arch=$ARCH \
+				--logdir="./logs/$DATASET/$METHOD/$ARCH" \
+				--batch-size=$BATCH_SIZE \
+				--epochs=$NUM_EPOCHS \
+				--learning-rate=$LEARNING_RATE
+		done
 	done
 done
