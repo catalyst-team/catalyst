beb4fd256648823bc04b31ff47849e9bc7a675d4
GitHub Action <action@ithub.com>
2022-02-06 09:49:35 +0000

    v21.12

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '22.01rc0',
+    VERSION: '22.02rc0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
