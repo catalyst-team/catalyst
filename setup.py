6b3499291e2eb4232eac6681fb577dda4e11a548
Sergey Kolesnikov <scitator@gmail.com>
2022-04-29 00:17:00 +0300

    +

--- setup.py
+++ setup.py
@@ -129,6 +129,8 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: Implementation :: CPython",
     ],
 )
