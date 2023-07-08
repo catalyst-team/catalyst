65f290d94704eb01c8527cb94bd25f6bd8fb3128
Philmod <philmod@google.com>
2023-07-07 20:46:01 -0400

    make the arguments optional

--- catalyst/engines/torch.py
+++ catalyst/engines/torch.py
@@ -40,7 +40,7 @@ class DataParallelEngine(Engine):
         """Init."""
         super().__init__(*args, cpu=False, **kwargs)
 
-    def prepare_model(self, model, device_placement, evaluation_mode):
+    def prepare_model(self, model, device_placement = None, evaluation_mode = False):
         """Overrides."""
         model = torch.nn.DataParallel(model)
         model = super().prepare_model(model, device_placement, evaluation_mode)
